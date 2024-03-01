21665d6d3da80ec92f86cbd93f22d16487089703
wangyan21 <wangyan21@inspur.com>
2024-03-01 16:44:14 +0800

    巫山县-农资采购管理第一版

---
+++ src/views/wushan/agriculturalpurchase/index.vue
@@ -0,0 +1,240 @@
+<template>
+  <ContentWrap>
+    <!-- 搜索工作栏 -->
+    <el-form
+      class="-mb-15px"
+      :model="queryParams"
+      ref="queryFormRef"
+      :inline="true"
+      label-width="68px"
+    >
+      <el-form-item label="农资名称" prop="agriculturalName">
+        <el-input
+          v-model="queryParams.agriculturalName"
+          placeholder="请输入农资名称"
+          clearable
+          @keyup.enter="handleQuery"
+          class="!w-240px"
+        />
+      </el-form-item>
+      <el-form-item label="采购单号" prop="purchaseId">
+        <el-input
+          v-model="queryParams.purchaseId"
+          placeholder="请输入采购单号"
+          clearable
+          @keyup.enter="handleQuery"
+          class="!w-240px"
+        />
+      </el-form-item>
+      <el-form-item label="采购日期" prop="purchaseTime">
+        <el-date-picker
+          v-model="queryParams.purchaseTime"
+          value-format="YYYY-MM-DD HH:mm:ss"
+          type="daterange"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          :default-time="[new Date('1 00:00:00'), new Date('1 23:59:59')]"
+          class="!w-240px"
+        />
+      </el-form-item>
+      <el-form-item label="供应商" prop="supplierName">
+        <el-input
+          v-model="queryParams.supplierName"
+          placeholder="请输入供应商"
+          clearable
+          @keyup.enter="handleQuery"
+          class="!w-240px"
+        />
+      </el-form-item>
+      <el-form-item label="订单状态" prop="orderStatus">
+        <el-select
+          v-model="queryParams.orderStatus"
+          placeholder="请选择订单状态"
+          clearable
+          class="!w-240px"
+        >
+          <el-option
+            v-for="dict in getStrDictOptions(DICT_TYPE.KAIZHOU_AGRICULTURAL_ORDER_STATUS)"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="handleQuery"><Icon icon="ep:search" class="mr-5px" /> 搜索</el-button>
+        <el-button @click="resetQuery"><Icon icon="ep:refresh" class="mr-5px" /> 重置</el-button>
+        <el-button
+          type="primary"
+          plain
+          @click="openForm('create')"
+          v-hasPermi="['kaizhou:agricultural-purchase:create']"
+        >
+          <Icon icon="ep:plus" class="mr-5px" /> 新增
+        </el-button>
+        <el-button
+          type="success"
+          plain
+          @click="handleExport"
+          :loading="exportLoading"
+          v-hasPermi="['kaizhou:agricultural-purchase:export']"
+        >
+          <Icon icon="ep:download" class="mr-5px" /> 导出
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </ContentWrap>
+
+  <!-- 列表 -->
+  <ContentWrap>
+    <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
+      <el-table-column label="主键" align="center" prop="id" />
+      <el-table-column label="农资主键" align="center" prop="agriculturalId" />
+      <el-table-column label="农资名称" align="center" prop="agriculturalName" />
+      <el-table-column label="农资规格" align="center" prop="agriculturalSize" />
+      <el-table-column label="采购单号" align="center" prop="purchaseId" />
+      <el-table-column label="采购日期" align="center" prop="purchaseTime" />
+      <el-table-column label="供应商" align="center" prop="supplierName" />
+      <el-table-column label="采购单价" align="center" prop="purchasePrice" />
+      <el-table-column label="采购数量" align="center" prop="purchaseNum" />
+      <el-table-column label="采购金额" align="center" prop="totalPrice" />
+      <el-table-column label="订单状态" align="center" prop="orderStatus">
+        <template #default="scope">
+          <dict-tag :type="DICT_TYPE.KAIZHOU_AGRICULTURAL_ORDER_STATUS" :value="scope.row.orderStatus" />
+        </template>
+      </el-table-column>
+      <el-table-column label="生产日期" align="center" prop="productionDate" />
+      <el-table-column label="有效日期" align="center" prop="expirationDate" />
+      <el-table-column label="备注" align="center" prop="remark" />
+      <el-table-column
+        label="创建时间"
+        align="center"
+        prop="createTime"
+        :formatter="dateFormatter"
+        width="180px"
+      />
+      <el-table-column label="操作" align="center">
+        <template #default="scope">
+          <el-button
+            link
+            type="primary"
+            @click="openForm('update', scope.row.id)"
+            v-hasPermi="['kaizhou:agricultural-purchase:update']"
+          >
+            编辑
+          </el-button>
+          <el-button
+            link
+            type="danger"
+            @click="handleDelete(scope.row.id)"
+            v-hasPermi="['kaizhou:agricultural-purchase:delete']"
+          >
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <Pagination
+      :total="total"
+      v-model:page="queryParams.pageNo"
+      v-model:limit="queryParams.pageSize"
+      @pagination="getList"
+    />
+  </ContentWrap>
+
+  <!-- 表单弹窗：添加/修改 -->
+  <AgriculturalPurchaseForm ref="formRef" @success="getList" />
+</template>
+
+<script setup lang="ts">
+import { getStrDictOptions, DICT_TYPE } from '@/utils/dict'
+import { dateFormatter } from '@/utils/formatTime'
+import download from '@/utils/download'
+import { AgriculturalPurchaseApi, AgriculturalPurchaseVO } from '@/api/kaizhou/agriculturalpurchase'
+import AgriculturalPurchaseForm from './AgriculturalPurchaseForm.vue'
+
+/** 农资采购信息 列表 */
+defineOptions({ name: 'WushanAgriculturalPurchase' })
+
+const message = useMessage() // 消息弹窗
+const { t } = useI18n() // 国际化
+
+const loading = ref(true) // 列表的加载中
+const list = ref<AgriculturalPurchaseVO[]>([]) // 列表的数据
+const total = ref(0) // 列表的总页数
+const queryParams = reactive({
+  pageNo: 1,
+  pageSize: 10,
+  agriculturalName: undefined,
+  purchaseId: undefined,
+  purchaseTime: [],
+  supplierName: undefined,
+  orderStatus: undefined,
+})
+const queryFormRef = ref() // 搜索的表单
+const exportLoading = ref(false) // 导出的加载中
+
+/** 查询列表 */
+const getList = async () => {
+  loading.value = true
+  try {
+    const data = await AgriculturalPurchaseApi.getAgriculturalPurchasePage(queryParams)
+    list.value = data.list
+    total.value = data.total
+  } finally {
+    loading.value = false
+  }
+}
+
+/** 搜索按钮操作 */
+const handleQuery = () => {
+  queryParams.pageNo = 1
+  getList()
+}
+
+/** 重置按钮操作 */
+const resetQuery = () => {
+  queryFormRef.value.resetFields()
+  handleQuery()
+}
+
+/** 添加/修改操作 */
+const formRef = ref()
+const openForm = (type: string, id?: number) => {
+  formRef.value.open(type, id)
+}
+
+/** 删除按钮操作 */
+const handleDelete = async (id: number) => {
+  try {
+    // 删除的二次确认
+    await message.delConfirm()
+    // 发起删除
+    await AgriculturalPurchaseApi.deleteAgriculturalPurchase(id)
+    message.success(t('common.delSuccess'))
+    // 刷新列表
+    await getList()
+  } catch {}
+}
+
+/** 导出按钮操作 */
+const handleExport = async () => {
+  try {
+    // 导出的二次确认
+    await message.exportConfirm()
+    // 发起导出
+    exportLoading.value = true
+    const data = await AgriculturalPurchaseApi.exportAgriculturalPurchase(queryParams)
+    download.excel(data, '农资采购信息.xls')
+  } catch {
+  } finally {
+    exportLoading.value = false
+  }
+}
+
+/** 初始化 **/
+onMounted(() => {
+  getList()
+})
+</script>
