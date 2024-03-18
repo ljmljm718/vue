04e4c21ea08404e02d45704d31977aa3499a9166
zhoumingxiu <zhoumingxiu@inspur.com>
2024-03-18 17:22:16 +0800

    feat: 武隆基地信息

---
+++ src/views/agriculture/parkinfo/components/ParkDetailForm.vue
@@ -0,0 +1,174 @@
+<template>
+  <el-form
+    ref="formRef"
+    :model="formData"
+    :rules="formRules"
+    v-loading="formLoading"
+    label-width="0px"
+    :inline-message="true"
+  >
+    <el-table :data="formData" class="-mt-10px">
+      <el-table-column label="序号" type="index" width="100" />
+       <el-table-column label="编号" min-width="150">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`${$index}.code`" :rules="formRules.code" class="mb-0px!">
+            <el-input v-model="row.code" placeholder="不填后台自动生成" />
+          </el-form-item>
+        </template>
+      </el-table-column>
+      <el-table-column label="名称" min-width="150">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`${$index}.name`" :rules="formRules.name" class="mb-0px!">
+            <el-input v-model="row.name" placeholder="请输入名称" />
+          </el-form-item>
+        </template>
+      </el-table-column>
+<!--      <el-table-column label="类型" min-width="150">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`${$index}.type`" :rules="formRules.type" class="mb-0px!">
+            <el-select v-model="row.type" placeholder="请选择类型">
+                <el-option label="请选择字典生成" value="" />
+            </el-select>
+          </el-form-item>
+        </template>
+      </el-table-column>-->
+      <el-table-column label="海拔" min-width="150">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`${$index}.altitude`" :rules="formRules.altitude" class="mb-0px!">
+            <el-input v-model="row.altitude" placeholder="请输入海拔" />
+          </el-form-item>
+        </template>
+      </el-table-column>
+      <el-table-column label="纬度" min-width="150">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`${$index}.latitude`" :rules="formRules.latitude" class="mb-0px!">
+            <el-input v-model="row.latitude" placeholder="请输入纬度" />
+          </el-form-item>
+        </template>
+      </el-table-column>
+      <el-table-column label="经度" min-width="150">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`${$index}.longitude`" :rules="formRules.longitude" class="mb-0px!">
+            <el-input v-model="row.longitude" placeholder="请输入经度" />
+          </el-form-item>
+        </template>
+      </el-table-column>
+      <el-table-column label="通讯地址" min-width="150">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`${$index}.address`" :rules="formRules.address" class="mb-0px!">
+            <el-input v-model="row.address" placeholder="请输入通讯地址" />
+          </el-form-item>
+        </template>
+      </el-table-column>
+      <el-table-column label="联系人" min-width="150">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`${$index}.contact`" :rules="formRules.contact" class="mb-0px!">
+            <el-input v-model="row.contact" placeholder="请输入联系人" />
+          </el-form-item>
+        </template>
+      </el-table-column>
+      <el-table-column label="联系电话" min-width="150">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`${$index}.tel`" :rules="formRules.tel" class="mb-0px!">
+            <el-input v-model="row.tel" placeholder="请输入联系电话" />
+          </el-form-item>
+        </template>
+      </el-table-column>
+      <el-table-column label="面积" min-width="150">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`${$index}.area`" :rules="formRules.area" class="mb-0px!">
+            <el-input v-model="row.area" placeholder="请输入面积" />
+          </el-form-item>
+        </template>
+      </el-table-column>
+      <el-table-column label="备注" min-width="150">
+        <template #default="{ row, $index }">
+          <el-form-item :prop="`${$index}.remark`" :rules="formRules.remark" class="mb-0px!">
+            <el-input v-model="row.remark" placeholder="请输入备注" />
+          </el-form-item>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" fixed="right" label="操作" width="60">
+        <template #default="{ $index }">
+          <el-button @click="handleDelete($index)" link>—</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-form>
+  <el-row justify="center" class="mt-3">
+    <el-button @click="handleAdd" round>+ 添加地块基本信息</el-button>
+  </el-row>
+</template>
+<script setup lang="ts">
+import { ParkInfoApi } from '@/api/agriculture/parkinfo'
+
+const props = defineProps<{
+  parkId: undefined // 主表（主表的关联字段）
+}>()
+const formLoading = ref(false) // 表单的加载中
+const formData = ref([])
+const formRules = reactive({
+  name: [{ required: true, message: '名称不能为空', trigger: 'blur' }]
+})
+const formRef = ref() // 表单 Ref
+
+/** 监听主表的关联字段的变化，加载对应的子表数据 */
+watch(
+  () => props.parkId,
+  async (val) => {
+    // 1. 重置表单
+    formData.value = []
+    // 2. val 非空，则加载数据
+    if (!val) {
+      return;
+    }
+    try {
+      formLoading.value = true
+      formData.value = await ParkInfoApi.getParkDetailListByParkId(val)
+    } finally {
+      formLoading.value = false
+    }
+  },
+  { immediate: true }
+)
+
+/** 新增按钮操作 */
+const handleAdd = () => {
+  const row = {
+    id: undefined,
+    parkId: undefined,
+    code: undefined,
+    name: undefined,
+    type: undefined,
+    altitude: undefined,
+    latitude: undefined,
+    longitude: undefined,
+    address: undefined,
+    contact: undefined,
+    tel: undefined,
+    area: undefined,
+    remark: undefined,
+    deptId: undefined,
+    userId: undefined
+  }
+  row.parkId = props.parkId
+  formData.value.push(row)
+}
+
+/** 删除按钮操作 */
+const handleDelete = (index) => {
+  formData.value.splice(index, 1)
+}
+
+/** 表单校验 */
+const validate = () => {
+  return formRef.value.validate()
+}
+
+/** 表单值 */
+const getData = () => {
+  return formData.value
+}
+
+defineExpose({ validate, getData })
+</script>
---
+++ src/views/agriculture/parkinfo/index.vue
@@ -0,0 +1,222 @@
+<template>
+  <ContentWrap>
+    <!-- 搜索工作栏 -->
+    <el-form
+      class="-mb-15px"
+      :model="queryParams"
+      ref="queryFormRef"
+      :inline="true"
+      label-width="68px"
+    >
+      <el-form-item label="编号" prop="code">
+        <el-input
+          v-model="queryParams.code"
+          placeholder="请输入编号"
+          clearable
+          @keyup.enter="handleQuery"
+          class="!w-240px"
+        />
+      </el-form-item>
+      <el-form-item label="名称" prop="name">
+        <el-input
+          v-model="queryParams.name"
+          placeholder="请输入名称"
+          clearable
+          @keyup.enter="handleQuery"
+          class="!w-240px"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="handleQuery"><Icon icon="ep:search" class="mr-5px" /> 搜索</el-button>
+        <el-button @click="resetQuery"><Icon icon="ep:refresh" class="mr-5px" /> 重置</el-button>
+        <el-button
+          type="primary"
+          plain
+          @click="openForm('create')"
+          v-hasPermi="['agriculture:park-info:create']"
+        >
+          <Icon icon="ep:plus" class="mr-5px" /> 新增
+        </el-button>
+        <el-button
+          type="success"
+          plain
+          @click="handleExport"
+          :loading="exportLoading"
+          v-hasPermi="['agriculture:park-info:export']"
+        >
+          <Icon icon="ep:download" class="mr-5px" /> 导出
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </ContentWrap>
+
+  <!-- 列表 -->
+  <ContentWrap>
+    <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
+      <!-- 子表的列表 -->
+      <el-table-column type="expand">
+        <template #default="scope">
+          <el-tabs model-value="parkDetail">
+            <el-tab-pane label="地块基本信息" name="parkDetail">
+              <ParkDetailList :park-id="scope.row.id" />
+            </el-tab-pane>
+          </el-tabs>
+        </template>
+      </el-table-column>
+      <el-table-column label="编号" align="center" prop="code" width="200"/>
+      <el-table-column label="名称" align="center" prop="name" width="150"/>
+      <el-table-column label="类型" align="center" prop="type" >
+        <template #default="scope">
+          <dict-tag :type="DICT_TYPE.AGRI_PARK_TYPE" :value="scope.row.type" />
+        </template>
+      </el-table-column>
+      <el-table-column label="海拔（米）" align="center" prop="altitude" width="100"/>
+      <el-table-column label="纬度" align="center" prop="latitude" />
+      <el-table-column label="经度" align="center" prop="longitude" />
+      <el-table-column label="通讯地址" align="center" prop="address" />
+      <el-table-column label="联系人" align="center" prop="contact" />
+      <el-table-column label="联系电话" align="center" prop="tel" />
+      <el-table-column label="面积（亩）" align="center" prop="area" width="100"/>
+      <el-table-column label="备注" align="center" prop="remark" />
+      <el-table-column
+        label="创建时间"
+        align="center"
+        prop="createTime"
+        :formatter="dateFormatter"
+        width="180px"
+      />
+      <el-table-column label="操作" align="center" width="150" fixed="right">
+        <template #default="scope">
+          <el-button
+            link
+            type="primary"
+            @click="openForm('update', scope.row.id)"
+            v-hasPermi="['agriculture:park-info:update']"
+          >
+            编辑
+          </el-button>
+          <el-button
+            link
+            type="danger"
+            @click="handleDelete(scope.row.id)"
+            v-hasPermi="['agriculture:park-info:delete']"
+          >
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <Pagination
+      :total="total"
+      v-model:page="queryParams.pageNo"
+      v-model:limit="queryParams.pageSize"
+      @pagination="getList"
+    />
+  </ContentWrap>
+
+  <!-- 表单弹窗：添加/修改 -->
+  <ParkInfoForm ref="formRef" @success="getList" />
+</template>
+
+<script setup lang="ts">
+import { dateFormatter } from '@/utils/formatTime'
+import download from '@/utils/download'
+import { ParkInfoApi, ParkInfoVO } from '@/api/agriculture/parkinfo'
+import ParkInfoForm from './ParkInfoForm.vue'
+import ParkDetailList from './components/ParkDetailList.vue'
+import {DICT_TYPE} from "@/utils/dict";
+
+/** 基地基本信息 列表 */
+defineOptions({ name: 'ParkInfo' })
+
+const message = useMessage() // 消息弹窗
+const { t } = useI18n() // 国际化
+
+const loading = ref(true) // 列表的加载中
+const list = ref<ParkInfoVO[]>([]) // 列表的数据
+const total = ref(0) // 列表的总页数
+const queryParams = reactive({
+  pageNo: 1,
+  pageSize: 10,
+  code: undefined,
+  name: undefined,
+  type: undefined,
+  altitude: undefined,
+  latitude: undefined,
+  longitude: undefined,
+  address: undefined,
+  contact: undefined,
+  tel: undefined,
+  area: undefined,
+  remark: undefined,
+  createTime: [],
+  deptId: undefined,
+  userId: undefined
+})
+const queryFormRef = ref() // 搜索的表单
+const exportLoading = ref(false) // 导出的加载中
+
+/** 查询列表 */
+const getList = async () => {
+  loading.value = true
+  try {
+    const data = await ParkInfoApi.getParkInfoPage(queryParams)
+    list.value = data.list
+    total.value = data.total
+  } finally {
+    loading.value = false
+  }
+}
+
+/** 搜索按钮操作 */
+const handleQuery = () => {
+  queryParams.pageNo = 1
+  getList()
+}
+
+/** 重置按钮操作 */
+const resetQuery = () => {
+  queryFormRef.value.resetFields()
+  handleQuery()
+}
+
+/** 添加/修改操作 */
+const formRef = ref()
+const openForm = (type: string, id?: number) => {
+  formRef.value.open(type, id)
+}
+
+/** 删除按钮操作 */
+const handleDelete = async (id: number) => {
+  try {
+    // 删除的二次确认
+    await message.delConfirm()
+    // 发起删除
+    await ParkInfoApi.deleteParkInfo(id)
+    message.success(t('common.delSuccess'))
+    // 刷新列表
+    await getList()
+  } catch {}
+}
+
+/** 导出按钮操作 */
+const handleExport = async () => {
+  try {
+    // 导出的二次确认
+    await message.exportConfirm()
+    // 发起导出
+    exportLoading.value = true
+    const data = await ParkInfoApi.exportParkInfo(queryParams)
+    download.excel(data, '基地基本信息.xls')
+  } catch {
+  } finally {
+    exportLoading.value = false
+  }
+}
+
+/** 初始化 **/
+onMounted(() => {
+  getList()
+})
+</script>
