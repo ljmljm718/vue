c180d3883634dffe899ea5069c726de2911457c8
LiWeiJie <3491909454@qq.com>
2024-03-19 17:33:45 +0800

    酉阳鲁渝协作示范村数字化赋能————物联网设备->监控设备 & 水质监测 前端

--- src/utils/dict.ts
+++ src/utils/dict.ts
@@ -248,6 +248,8 @@ export enum DICT_TYPE {
 
   // ========== AGRI - 武隆  ==========
   AGRI_PARK_TYPE = 'agri_park_type', //基地种植类型
+
   // =========== YYANG -  酉阳 ==========
   YYANG_WATER_DETECTION = 'yyang_water_detection', 
-  AGRI_FARMER_SEX = 'agri_farmer_sex', //性别1男2女}
+  AGRI_FARMER_SEX = 'agri_farmer_sex', //性别1男2女
+}
