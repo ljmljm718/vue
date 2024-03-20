37105bc90cc71698032634e4953853337e23ac5d
likehai <likehai@inspur.com>
2024-03-20 11:22:51 +0800

    代码回退

--- src/views/agriculture/cropbase/CropBaseForm.vue
+++ src/views/agriculture/cropbase/CropBaseForm.vue
@@ -1,49 +1,49 @@
 <template>
-  <Dialog :title="dialogTitle" v-model="dialogVisible">
-    <el-form
-      ref="formRef"
-      :model="formData"
-      :rules="formRules"
-      label-width="100px"
-      v-loading="formLoading"
-    >
-      <el-form-item label="编号" prop="cropCode">
-        <el-input v-model="formData.cropCode" disabled placeholder="系统自动生成...." />
-      </el-form-item>
-      <el-form-item label="名称" prop="cropName">
-        <el-input v-model="formData.cropName" placeholder="请输入名称" />
-      </el-form-item>
-      <el-form-item label="品种" prop="cropType">
-        <el-select v-model="formData.cropType" placeholder="请选择品种">
-          <el-option
-            v-for="dict in getStrDictOptions(DICT_TYPE.KAIZHOU_CROP_CULTIVARS)"
-            :key="dict.value"
-            :label="dict.label"
-            :value="dict.value"
-          />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="所属基地" prop="belongPlot">
-        <el-input v-model="formData.belongPlot" placeholder="请输入所属园区" />
-      </el-form-item>
-      <el-form-item label="所属地块" prop="belongPark">
-        <el-input v-model="formData.belongPark" placeholder="请输入所属地块" />
-      </el-form-item>
-      <el-form-item label="图片" prop="imgId">
-        <UploadImg v-model="formData.imgId" />
-      </el-form-item>
-      <el-form-item label="描述" prop="cropDesc">
-        <el-input v-model="formData.cropDesc" placeholder="请输入描述" />
-      </el-form-item>
-      <el-form-item label="备注" prop="remark">
-        <el-input v-model="formData.remark" placeholder="请输入备注" />
-      </el-form-item>
-    </el-form>
-    <template #footer>
-      <el-button @click="submitForm" type="primary" :disabled="formLoading">确 定</el-button>
-      <el-button @click="dialogVisible = false">取 消</el-button>
-    </template>
-  </Dialog>
+    <Dialog :title="dialogTitle" v-model="dialogVisible">
+        <el-form
+                ref="formRef"
+                :model="formData"
+                :rules="formRules"
+                label-width="100px"
+                v-loading="formLoading"
+        >
+            <el-form-item label="编号" prop="cropCode">
+                <el-input v-model="formData.cropCode" disabled placeholder="系统自动生成...." />
+            </el-form-item>
+            <el-form-item label="名称" prop="cropName">
+                <el-input v-model="formData.cropName" placeholder="请输入名称" />
+            </el-form-item>
+            <el-form-item label="品种" prop="cropType">
+                <el-select v-model="formData.cropType" placeholder="请选择品种">
+                    <el-option
+                            v-for="dict in getStrDictOptions(DICT_TYPE.KAIZHOU_CROP_CULTIVARS)"
+                            :key="dict.value"
+                            :label="dict.label"
+                            :value="dict.value"
+                    />
+                </el-select>
+            </el-form-item>
+            <el-form-item label="所属基地" prop="belongPlot">
+                <el-input v-model="formData.belongPlot" placeholder="请输入所属园区" />
+            </el-form-item>
+            <el-form-item label="所属地块" prop="belongPark">
+                <el-input v-model="formData.belongPark" placeholder="请输入所属地块" />
+            </el-form-item>
+            <el-form-item label="图片" prop="imgId">
+                <UploadImg v-model="formData.imgId" />
+            </el-form-item>
+            <el-form-item label="描述" prop="cropDesc">
+                <el-input v-model="formData.cropDesc" placeholder="请输入描述" />
+            </el-form-item>
+            <el-form-item label="备注" prop="remark">
+                <el-input v-model="formData.remark" placeholder="请输入备注" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <el-button @click="submitForm" type="primary" :disabled="formLoading">确 定</el-button>
+            <el-button @click="dialogVisible = false">取 消</el-button>
+        </template>
+    </Dialog>
 </template>
 <script setup lang="ts">
 import { getStrDictOptions, DICT_TYPE } from '@/utils/dict'
@@ -60,72 +60,6 @@ const dialogTitle = ref('') // 弹窗的标题
 const formLoading = ref(false) // 表单的加载中：1）修改时的数据加载；2）提交的按钮禁用
 const formType = ref('') // 表单的类型：create - 新增；update - 修改
 const formData = ref({
-  id: undefined,
-  cropCode: undefined,
-  cropName: undefined,
-  cropType: undefined,
-  imgId: undefined,
-  cropDesc: undefined,
-  remark: undefined,
-  belongPark: undefined,
-  belongPlot: undefined,
-  deptId: undefined,
-  userId: undefined,
-})
-const formRules = reactive({
-  cropName: [{ required: true, message: '名称不能为空', trigger: 'blur' }],
-  cropType: [{ required: true, message: '品种不能为空', trigger: 'change' }],
-  imgId: [{ required: true, message: '图片不能为空', trigger: 'blur' }],
-  belongPlot: [{ required: true, message: '所属基地不能为空', trigger: 'blur' }],
-  belongPark: [{ required: true, message: '所属地块不能为空', trigger: 'blur' }],
-})
-const formRef = ref() // 表单 Ref
-
-/** 打开弹窗 */
-const open = async (type: string, id?: number) => {
-  dialogVisible.value = true
-  dialogTitle.value = t('action.' + type)
-  formType.value = type
-  resetForm()
-  // 修改时，设置数据
-  if (id) {
-    formLoading.value = true
-    try {
-      formData.value = await CropBaseApi.getCropBase(id)
-    } finally {
-      formLoading.value = false
-    }
-  }
-}
-defineExpose({ open }) // 提供 open 方法，用于打开弹窗
-
-/** 提交表单 */
-const emit = defineEmits(['success']) // 定义 success 事件，用于操作成功后的回调
-const submitForm = async () => {
-  // 校验表单
-  await formRef.value.validate()
-  // 提交请求
-  formLoading.value = true
-  try {
-    const data = formData.value as unknown as CropBaseVO
-    if (formType.value === 'create') {
-      await CropBaseApi.createCropBase(data)
-      message.success(t('common.createSuccess'))
-    } else {
-      await CropBaseApi.updateCropBase(data)
-      message.success(t('common.updateSuccess'))
-    }
-    dialogVisible.value = false
-    // 发送操作成功的事件
-    emit('success')
-  } finally {
-    formLoading.value = false
-  }
-}
-
-/** 重置表单 */
-const resetForm = () => {
-  formData.value = {
     id: undefined,
     cropCode: undefined,
     cropName: undefined,
@@ -137,7 +71,73 @@ const resetForm = () => {
     belongPlot: undefined,
     deptId: undefined,
     userId: undefined,
-  }
-  formRef.value?.resetFields()
+})
+const formRules = reactive({
+    cropName: [{ required: true, message: '名称不能为空', trigger: 'blur' }],
+    cropType: [{ required: true, message: '品种不能为空', trigger: 'change' }],
+    imgId: [{ required: true, message: '图片不能为空', trigger: 'blur' }],
+    belongPlot: [{ required: true, message: '所属基地不能为空', trigger: 'blur' }],
+    belongPark: [{ required: true, message: '所属地块不能为空', trigger: 'blur' }],
+})
+const formRef = ref() // 表单 Ref
+
+/** 打开弹窗 */
+const open = async (type: string, id?: number) => {
+    dialogVisible.value = true
+    dialogTitle.value = t('action.' + type)
+    formType.value = type
+    resetForm()
+    // 修改时，设置数据
+    if (id) {
+        formLoading.value = true
+        try {
+            formData.value = await CropBaseApi.getCropBase(id)
+        } finally {
+            formLoading.value = false
+        }
+    }
+}
+defineExpose({ open }) // 提供 open 方法，用于打开弹窗
+
+/** 提交表单 */
+const emit = defineEmits(['success']) // 定义 success 事件，用于操作成功后的回调
+const submitForm = async () => {
+    // 校验表单
+    await formRef.value.validate()
+    // 提交请求
+    formLoading.value = true
+    try {
+        const data = formData.value as unknown as CropBaseVO
+        if (formType.value === 'create') {
+            await CropBaseApi.createCropBase(data)
+            message.success(t('common.createSuccess'))
+        } else {
+            await CropBaseApi.updateCropBase(data)
+            message.success(t('common.updateSuccess'))
+        }
+        dialogVisible.value = false
+        // 发送操作成功的事件
+        emit('success')
+    } finally {
+        formLoading.value = false
+    }
+}
+
+/** 重置表单 */
+const resetForm = () => {
+    formData.value = {
+        id: undefined,
+        cropCode: undefined,
+        cropName: undefined,
+        cropType: undefined,
+        imgId: undefined,
+        cropDesc: undefined,
+        remark: undefined,
+        belongPark: undefined,
+        belongPlot: undefined,
+        deptId: undefined,
+        userId: undefined,
+    }
+    formRef.value?.resetFields()
 }
 </script>
--- src/views/bigscreen5/index.vue
+++ src/views/bigscreen5/index.vue
@@ -55,9 +55,51 @@
                   </div>
               </div>
             </div>
+            <div class="left4">
+              <div class="box-title">种植分布</div>
+              <div class="left4-item">
+                <div id="chart1"></div>
+              </div>
+            </div>
           </div>
           <div class="main"></div>
-          <div class="right"></div>
+          <div class="right">
+            <div class="right1">
+              <div class="box-title">气象监测</div>
+              <div class="right1-item">
+                <div class="right1-top">
+                  <div :class="qxzIndex==1?'active':'actived'" @click="qxzIndex=1" style="margin-right:10px;">气象站1</div>
+                  <div :class="qxzIndex==2?'active':'actived'" @click="qxzIndex=2">气象站2</div>
+                </div>
+                <div class="right1-warper">
+                  <div v-for="item,index in right1List" :key="index" class="right1Bg">
+                    <div :class="['right1-icon','right1bg-'+(index+1).toString()]"></div>
+                    <div style="margin-left: 20px;display: flex;flex-direction: column;align-items: center;height: 40%;justify-content: space-between;;">
+                      <div style="color:#16bee2;font-size: 18px;">{{ item.val }}</div>
+                      <div style="font-size: 13px;">{{ item.title }}</div>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="right2">
+              <div class="box-title">棚内环境监测</div>
+              <div class="right2-item">
+                <div class="right2-select" style="margin-top: 15px;">
+                  <select name="" id="" class="select" style="margin-right: 20px;">
+                    <option value="12">基地</option>
+                  </select>
+                  <select name="" id="" class="select">
+                    <option value="12">1号大鹏</option>
+                  </select>
+                </div>
+                <div class="right2-warper">
+
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
 </div>
 </template>
@@ -69,10 +111,58 @@ generateBaseOptions,
 generatePieOptions,
 } from "../../utils/bigscreenTool/index";
 
-import * as echarts from "echarts"
 import {ref,reactive,onMounted} from 'vue'
-import { object } from 'vue-types';
 import { resetSize } from '@/components/Verifition/src/utils/util';
+const initChart1= ()=> {
+      initChartStatic(
+        "chart1",
+        generatePieOptions({
+          legend: {
+            show: true,
+            top: "center",
+            left: "right",
+            bottom:'0',
+            orient:'vertical',
+            itemWidth: 12,
+            itemHeight: 12,
+          },
+          color: ["#0097f4", "#17968b", "#b39946",'#c1764a' ,'#dfdfe0'],
+          series: [
+            {
+              nam: "种植分布",
+              type: "pie",
+              radius: ["30%", "50%"],
+              center: "center",
+              data: [
+                {value: "32",name:'喜力'},
+                {value: "26",name:'金红九'},
+                {value: "21",name:'红满园'},
+                {value: "6",name:'格雷'},
+                {value: "7",name:'百利'},
+              ],
+              label: {
+                // formatter: "{c|{c}},{d|{d}%}",
+                formatter: "\n{c}\n {d}%",
+                rich: {
+                  c: {
+                    color: "#c1c1c1",
+                    fontSize: 10,
+                  },
+                  d: {
+                    color: "#c1c1c1",
+                    fontSize: 10,
+                  },
+                },
+              },
+            },
+          ],
+        })
+      );
+}
+onMounted(()=>{
+  initChart1()
+})
+let qxzIndex=ref(1)
 let leftTop1List=ref<any>([
   {
     title:'基地',
@@ -107,6 +197,32 @@ let leftTop3List=ref<any>([
   {
     title:'虫情监测',
     val:'21台'
+  },
+])
+let right1List=ref([
+  {
+    val:'17.3℃',
+    title:'大气温度'
+  },
+  {
+    val:'67.4%/RH',
+    title:'大气温度'
+  },
+  {
+    val:'0.01mm',
+    title:'降雨量'
+  },
+  {
+    val:'90.17kPa',
+    title:'气压'
+  },
+  {
+    val:'20890Lux',
+    title:'光照'
+  },
+  {
+    val:'0.75m/s',
+    title:'风速'
   },
 ])
 </script>
@@ -219,12 +335,97 @@ gap: 10px;
       }
     }
   }
+  .left4{
+    width: 100%;
+    height: 100%;
+  .left4-item{
+    width: 100%;
+    height: calc(100% - 2rem);
+    #chart1{
+      width: 100%;
+      height: 100%;
+    }
+  }
+  }
 }
 .right{
   display: grid;
-  grid-template-rows: 40% calc(60% - 10px);
+  grid-template-rows: 35% calc(65% - 10px);
   gap: 10px;
   grid-auto-columns: 100%;
+  .right1{
+    width: 100%;
+    height: 100%;
+    .right1-item{
+      width: 100%;
+      height: calc(100% - 2rem);
+      .right1-top{
+        height: 33px;
+        margin-left: 280px;
+        display: flex;
+        .active{
+          color: #fff;
+          font-size: 14px;
+          padding: 7px 15px;
+          
+          background-size: 100% 100%;
+          background-image: url(./assets/active.png);
+        }
+        .actived{
+          color: #c1c1c1;
+          padding: 7px 15px;
+          font-size: 14px;
+          background-size: 100% 100%;
+          background-image: url(./assets/actived.png);
+        }
+      }
+      .right1-warper{
+        width: 100%;
+        height: calc(100% - 33px);
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr ;
+        gap: 8px;
+        grid-template-rows: 1fr 1fr;
+        .right1Bg{
+          width: 100%;
+          height: 100%;
+          display: flex;
+          align-items: center;
+
+          .right1-icon{
+            width: 3rem;
+            height: 3rem;
+            background-size: 100% 100%;
+          }
+        }
+      }
+    }
+  }
+  .right2{
+    width: 100%;
+    height: 100%;
+    .right2-item{
+      width: 100%;
+      height: calc(100% - 2rem);
+      .right2-select{
+        height: 33px;
+        margin-left: 230px;
+        display: flex;
+        .select{
+          background: none;
+          padding: 0 15px;
+          color: #fff;
+          background-size: 100% 100%;
+          background-image: url(./assets/select.png);
+        }
+      }
+      .right2-warper{
+        width: 100%;
+        height: calc(100% - 48px);
+        background-color: red;
+      }
+    }
+  }
 }
 }
 
@@ -236,6 +437,11 @@ gap: 10px;
 @for $i from 1 through 4 {
 .left3Top-#{$i} {
   background-image: url(./assets/left3Top#{$i}.png);
+}
+}
+@for $i from 1 through 6 {
+.right1bg-#{$i} {
+  background-image: url(./assets/right1Bg#{$i}.png);
 }
 }
 
