b4b19ca682b6731709fe2ea244d119e47e473450
zhaofei06 <glpat-HH5C7EZ61Em5ubcGAWyb>
2024-03-20 15:14:41 +0800

    fix:土壤墒情、虫情信息新增修改时关联选择对应的设备

--- src/views/agriculture/device/soil/SoilMoistureForm.vue
+++ src/views/agriculture/device/soil/SoilMoistureForm.vue
@@ -8,7 +8,18 @@
       v-loading="formLoading"
     >
       <el-form-item label="设备编号" prop="deviceCode">
-        <el-input v-model="deviceData.deviceCode" disabled/>
+<!--        <el-input v-model="deviceData.deviceCode" disabled/>-->
+        <el-select v-model="deviceData.deviceCode" placeholder="请选择设备" @change="changeDevice" style="width: 100%;">
+          <el-option
+            v-for="item in deviceList"
+            :key="item.id"
+            :label="item.deviceCode"
+            :value="item.id"
+          >
+            <span style="float: left">{{item.deviceCode}}</span>
+            <span style="float: right">{{item.deviceName}}</span>
+          </el-option>
+        </el-select>
       </el-form-item>
       <el-form-item label="设备名称" prop="deviceName">
         <el-input v-model="deviceData.deviceName" disabled/>
@@ -37,6 +48,7 @@
 </template>
 <script setup lang="ts">
 import { SoilMoistureApi, SoilMoistureVO } from '@/api/agriculture/soilmoisture'
+import {DeviceInfoApi, DeviceInfoVO} from '@/api/agriculture/deviceinfo'
 
 /** 土壤墒情 表单 */
 defineOptions({ name: 'SoilMoistureForm' })
@@ -78,6 +90,7 @@ const deviceData = ref({
   deviceCode: '',
   deviceName: ''
 })
+const deviceList = ref<DeviceInfoVO[]>([]) // 设备列表的数据
 
 /** 打开弹窗 */
 const open = async (type: string, id?: number, deviceCode?: string, deviceName?: string) => {
@@ -85,6 +98,7 @@ const open = async (type: string, id?: number, deviceCode?: string, deviceName?:
   dialogTitle.value = t('action.' + type)
   formType.value = type
   resetForm()
+  getDeviceInfo()
   // 修改时，设置数据
   if (id) {
     formLoading.value = true
@@ -102,6 +116,15 @@ const open = async (type: string, id?: number, deviceCode?: string, deviceName?:
   }
 }
 defineExpose({ open }) // 提供 open 方法，用于打开弹窗
+
+const getDeviceInfo = async () => {
+  const data = await DeviceInfoApi.getDeviceInfoPage({
+    deviceType: '14,16',
+    pageNo: 1,
+    pageSize: 100
+  })
+  deviceList.value = data.list
+}
 
 /** 提交表单 */
 const emit = defineEmits(['success']) // 定义 success 事件，用于操作成功后的回调
@@ -127,6 +150,19 @@ const submitForm = async () => {
   }
 }
 
+// 选择设备时赋值
+const changeDevice = async (value) => {
+  const select = deviceList.value.filter( item => {
+    return item.id === value
+  })
+  if(select.length > 0) {
+    //赋值
+    formData.value.equId = select[0].id
+    deviceData.value.deviceCode = select[0].deviceCode
+    deviceData.value.deviceName = select[0].deviceName
+  }
+}
+
 /** 重置表单 */
 const resetForm = () => {
   formData.value = {
@@ -140,5 +176,11 @@ const resetForm = () => {
     soilDepth: undefined,
   }
   formRef.value?.resetFields()
+  if (formType.value === 'create') {
+    deviceData.value = {
+      deviceCode: undefined,
+      deviceName: undefined
+    }
+  }
 }
 </script>
