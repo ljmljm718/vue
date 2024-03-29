1805e5ef2559c984477c48470d96c3f515802df2
Tony <weijialin@inspur.com>
2024-03-29 11:03:32 +0800

    feat: 武隆县高山番茄数字化赋能三个大屏跳转

--- src/views/bigscreen5/index.vue
+++ src/views/bigscreen5/index.vue
@@ -57,11 +57,22 @@
             </div>
           </div>
           <div class="content-main-item middle-main-wrapper">
-            <div class="main-top">
+            <div class="top-card-wrapper">
+              <div class="top-card-item card-selected">
+                <div class="value-card">产业一张图</div>
+              </div>
+              <div class="top-card-item" @click="$router.push('/bigscreen8')">
+                <div class="value-card">产业一张图</div>
+              </div>
+              <div class="top-card-item" @click="$router.push('/bigscreen7')">
+                <div class="value-card">设备监控</div>
+              </div>
+            </div>
+            <!-- <div class="main-top">
               <div :class="mainIndex==1?'active':'actived'" @click="mainIndex=1">农业一张图</div>
               <div :class="mainIndex==2?'active':'actived'" @click="mainIndex=2">产业一张图</div>
               <div :class="mainIndex==3?'active':'actived'" @click="$router.push('/bigscreen7')">设备监控</div>
-            </div>
+            </div> -->
             <div class="middle-main-item">
                 <div class="sxt" style="left: 30%;top: 50%;">
                   <div class="message">
@@ -484,6 +495,35 @@ gap: 10px;
     position: relative;
     width: 100%;
     height: 100%;
+    .top-card-wrapper {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      z-index: 10;
+      padding: 0.8rem 0;
+      .top-card-item {
+        margin: 0 1.3rem;
+        width: 10rem;
+        aspect-ratio: 3.7;
+        background-image: url(./assets/actived2.png);
+        background-size: 100% 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        .value-card {
+          font-size: 1.1rem;
+          font-family: 'TitleFont';
+        }
+        .label-card {
+          font-size: 0.9rem;
+        }
+      }
+      .card-selected {
+        background-image: url(./assets/active2.png) !important;
+      }
+    }
     .main-top{
       z-index: 9999;
       position: absolute;
