5d4ee5c9c934b436cef6807edf074d000c002f96
Tony <weijialin@inspur.com>
2024-05-16 11:26:24 +0800

    feat: 标签页显示表单与本地保存表单

---
+++ src/components/EditFrame/index.ts
@@ -0,0 +1,21 @@
+import EditFrame from './src/EditFrame.vue'
+import {
+    addFormStorage,
+    getFormStorage,
+    editFormStorage,
+    deleteFormStorage,
+    addOrUpdateFormStorage,
+    getAllFormStorage,
+    deleteFormById
+} from './src/storage'
+
+export {
+    EditFrame,
+    addFormStorage,
+    getFormStorage,
+    editFormStorage,
+    deleteFormStorage,
+    addOrUpdateFormStorage,
+    getAllFormStorage,
+    deleteFormById
+}
---
+++ src/components/EditFrame/src/EditFrame.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const { back } = useRouter() 
+defineOptions({ name: 'EditFrame' })
+</script>
+<template>
+    <ContentWrap>
+        <div class="flex justify-between w-full">
+            <slot name="header">
+                <div></div>
+            </slot>
+            <el-button type="primary" plain @click="back()">返回</el-button>
+        </div>
+    </ContentWrap>
+    <ContentWrap>
+        <el-scrollbar height="700px">
+            <slot name="content"></slot>
+        </el-scrollbar>
+    </ContentWrap>
+</template>
