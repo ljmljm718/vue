f06e34592c447afa4f9cf375ee61a00d0efc7e1b
Tony <weijialin@inspur.com>
2024-05-16 14:20:56 +0800

    fix: BUG

--- src/layout/components/ToolHeader.vue
+++ src/layout/components/ToolHeader.vue
@@ -65,14 +65,13 @@ export default defineComponent({
     }
     setTimeout(() => {
       MatchRouteMap()
-    },2000)
-
+    }, 2000)
 
     const drawer = ref(false)
-    const storageList = ref<Array<any>>([])
+    const storageList = ref <Array<any>>([])
     const getAllStorageList = async () => {
       const { list = [] } = await getAllFormStorage()
-      console.log('getAllStorageList', list);
+      console.log('getAllStorageList', list)
       storageList.value = list
     }
     getAllStorageList()
@@ -99,68 +98,95 @@ export default defineComponent({
           </div>
         ) : undefined}
         <div class="h-full flex items-center">
-          <div
-            class="custom-hover"
-            color="var(--top-header-text-color)"
-          >
+          <div class="custom-hover" color="var(--top-header-text-color)">
             <a
               href={bigscreenRoute.value}
-              style="color: var(--top-header-text-color);transform:scale(.8);margin-bottom: .3rem;"
+              style="color: var(--top-header-text-color);transform:scale(.8);margin-bottom: -1px;margin-right: .4rem;"
             >
-              <el-icon><Platform /></el-icon>
+              <el-icon>
+                <Platform />
+              </el-icon>
             </a>
-          <span
-            onClick={() => {
-              drawer.value = true
-              getAllStorageList()
-            }}
-            style="color: var(--top-header-text-color);transform:scale(1.2);margin: .5rem .8rem 0 .8rem;"
-          >
-            <el-icon><Management /></el-icon>
-          </span>
-          <el-drawer
-            v-model={drawer.value}
-            title="暂存列表"
-            direction="rtl"
-          >
-            <el-table data={storageList.value} size="small" stripe class="shadow-lg rounded-lg py-1" style="border: 1px solid #25252525;">
-              <el-table-column label="表单名称" prop="name" />
-              <el-table-column label="表单Id" prop="formId" />
-              <el-table-column label="修改时间" formatter={(row) => {
-                return <span>{row.timeStamp ? formatTime(row.timeStamp, 'yyyy-MM-dd HH:mm:ss') : '----'}</span>
-              }} />
-              <el-table-column label="操作" formatter={(row) => {
-                return (
-                  <>
-                    <router-link
-                      to={row.url + (row.formId === 'new_form' ? '' : `?id=${row.formId}`)}
-                      onClick={() => { drawer.value = false }}
-                    >
-                      <el-button link type={'primary'}>编辑</el-button>
-                    </router-link>
-                    <el-button link type={'danger'} onClick={() => handleDeleteStorageForm(row)}>删除</el-button>
-                  </>
-                )
-              }} />
-            </el-table>
-          </el-drawer>
-          {screenfull.value ? (
-            <Screenfull class="custom-hover" color="var(--top-header-text-color)"></Screenfull>
-          ) : undefined}
-          {search.value ? <RouterSearch isModal={false}/> : undefined}
-          {size.value ? (
-            <SizeDropdown class="custom-hover" color="var(--top-header-text-color)"></SizeDropdown>
-          ) : undefined}
-          {locale.value ? (
-            <LocaleDropdown
-              class="custom-hover"
-              color="var(--top-header-text-color)"
-            ></LocaleDropdown>
-          ) : undefined}
-          {message.value ? (
-            <Message class="custom-hover" color="var(--top-header-text-color)"></Message>
-          ) : undefined}
-          <UserInfo></UserInfo>
+            <span
+              onClick={() => {
+                drawer.value = true
+                getAllStorageList()
+              }}
+              style="color: var(--top-header-text-color);transform:scale(1.2);margin: .5rem .8rem 0 .8rem;"
+            >
+              <el-icon>
+                <Management />
+              </el-icon>
+            </span>
+            <el-drawer v-model={drawer.value} title="暂存列表" direction="rtl">
+              <el-table
+                data={storageList.value}
+                size="small"
+                stripe
+                class="shadow-lg rounded-lg py-1"
+                style="border: 1px solid #25252525;"
+              >
+                <el-table-column label="表单名称" prop="name" />
+                <el-table-column label="表单Id" prop="formId" />
+                <el-table-column
+                  label="修改时间"
+                  formatter={(row) => {
+                    return (
+                      <span>
+                        {row.timeStamp ? formatTime(row.timeStamp, 'yyyy-MM-dd HH:mm:ss') : '----'}
+                      </span>
+                    )
+                  }}
+                />
+                <el-table-column
+                  label="操作"
+                  formatter={(row) => {
+                    return (
+                      <>
+                        <router-link
+                          to={row.url + (row.formId === 'new_form' ? '' : `?id=${row.formId}`)}
+                          onClick={() => {
+                            drawer.value = false
+                          }}
+                        >
+                          <el-button link type={'primary'}>
+                            编辑
+                          </el-button>
+                        </router-link>
+                        <el-button
+                          link
+                          type={'danger'}
+                          onClick={() => handleDeleteStorageForm(row)}
+                        >
+                          删除
+                        </el-button>
+                      </>
+                    )
+                  }}
+                />
+              </el-table>
+            </el-drawer>
+            {screenfull.value ? (
+              <Screenfull class="custom-hover" color="var(--top-header-text-color)"></Screenfull>
+            ) : undefined}
+            {search.value ? <RouterSearch isModal={false} /> : undefined}
+            {size.value ? (
+              <SizeDropdown
+                class="custom-hover"
+                color="var(--top-header-text-color)"
+              ></SizeDropdown>
+            ) : undefined}
+            {locale.value ? (
+              <LocaleDropdown
+                class="custom-hover"
+                color="var(--top-header-text-color)"
+              ></LocaleDropdown>
+            ) : undefined}
+            {message.value ? (
+              <Message class="custom-hover" color="var(--top-header-text-color)"></Message>
+            ) : undefined}
+            <UserInfo />
+          </div>
         </div>
       </div>
     )
@@ -176,7 +202,7 @@ $prefix-cls: #{$namespace}-tool-header;
 }
 
 a {
-  color:black;
+  color: black;
   font-size: 25px;
   margin-top: 8px;
 }
