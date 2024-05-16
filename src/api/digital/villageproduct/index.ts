adbfdcb974805602d90860acbf30e8aaca896bbe
卢序振 <luxuzhen@inspur.com>
2024-05-16 17:37:47 +0800

    数字乡村产业管理

--- src/api/digital/villageproduct/index.ts
+++ src/api/digital/villageproduct/index.ts
@@ -21,31 +21,31 @@ export interface VillageProductVO {
 export const VillageProductApi = {
   // 查询特色产品分页
   getVillageProductPage: async (params: any) => {
-    return await request.get({ url: `/digital/village-product/page`, params })
+    return await request.get({url: `/digital/village-product/page`, params})
   },
 
   // 查询特色产品详情
   getVillageProduct: async (id: number) => {
-    return await request.get({ url: `/digital/village-product/get?id=` + id })
+    return await request.get({url: `/digital/village-product/get?id=` + id})
   },
 
   // 新增特色产品
   createVillageProduct: async (data: VillageProductVO) => {
-    return await request.post({ url: `/digital/village-product/create`, data })
+    return await request.post({url: `/digital/village-product/create`, data})
   },
 
   // 修改特色产品
   updateVillageProduct: async (data: VillageProductVO) => {
-    return await request.put({ url: `/digital/village-product/update`, data })
+    return await request.put({url: `/digital/village-product/update`, data})
   },
 
   // 删除特色产品
   deleteVillageProduct: async (id: number) => {
-    return await request.delete({ url: `/digital/village-product/delete?id=` + id })
+    return await request.delete({url: `/digital/village-product/delete?id=` + id})
   },
 
   // 导出特色产品 Excel
   exportVillageProduct: async (params) => {
-    return await request.download({ url: `/digital/village-product/export-excel`, params })
+    return await request.download({url: `/digital/village-product/export-excel`, params})
   },
 }
---
+++ src/views/digital/villageprocessingrecords/VillageProcessingRecordsForm.vue
@@ -0,0 +1,116 @@
+<template>
+  <Dialog :title="dialogTitle" v-model="dialogVisible">
+    <el-form
+      ref="formRef"
+      :model="formData"
+      :rules="formRules"
+      label-width="100px"
+      v-loading="formLoading"
+    >
+      <el-form-item label="采收编号" prop="recoveryNum">
+        <el-input v-model="formData.recoveryNum" placeholder="请输入采收编号" />
+      </el-form-item>
+      <el-form-item label="加工时间" prop="machiningTime">
+        <el-date-picker
+          v-model="formData.machiningTime"
+          type="date"
+          value-format="x"
+          placeholder="选择加工时间"
+        />
+      </el-form-item>
+      <el-form-item label="品种名称" prop="product">
+        <el-input v-model="formData.product" placeholder="请输入品种名称" />
+      </el-form-item>
+      <el-form-item label="批次号" prop="batchCode">
+        <el-input v-model="formData.batchCode" placeholder="请输入批次号" />
+      </el-form-item>
+      <el-form-item label="消耗量" prop="remark">
+        <el-input v-model="formData.remark" placeholder="请输入消耗量" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <el-button @click="submitForm" type="primary" :disabled="formLoading">确 定</el-button>
+      <el-button @click="dialogVisible = false">取 消</el-button>
+    </template>
+  </Dialog>
+</template>
+<script setup lang="ts">
+import { VillageProcessingRecordsApi, VillageProcessingRecordsVO } from '@/api/digital/villageprocessingrecords'
+
+/** 加工记录 表单 */
+defineOptions({ name: 'VillageProcessingRecordsForm' })
+
+const { t } = useI18n() // 国际化
+const message = useMessage() // 消息弹窗
+
+const dialogVisible = ref(false) // 弹窗的是否展示
+const dialogTitle = ref('') // 弹窗的标题
+const formLoading = ref(false) // 表单的加载中：1）修改时的数据加载；2）提交的按钮禁用
+const formType = ref('') // 表单的类型：create - 新增；update - 修改
+const formData = ref({
+  id: undefined,
+  recoveryNum: undefined,
+  machiningTime: undefined,
+  product: undefined,
+  batchCode: undefined,
+  remark: undefined,
+})
+const formRules = reactive({
+})
+const formRef = ref() // 表单 Ref
+
+/** 打开弹窗 */
+const open = async (type: string, id?: number) => {
+  dialogVisible.value = true
+  dialogTitle.value = t('action.' + type)
+  formType.value = type
+  resetForm()
+  // 修改时，设置数据
+  if (id) {
+    formLoading.value = true
+    try {
+      formData.value = await VillageProcessingRecordsApi.getVillageProcessingRecords(id)
+    } finally {
+      formLoading.value = false
+    }
+  }
+}
+defineExpose({ open }) // 提供 open 方法，用于打开弹窗
+
+/** 提交表单 */
+const emit = defineEmits(['success']) // 定义 success 事件，用于操作成功后的回调
+const submitForm = async () => {
+  // 校验表单
+  await formRef.value.validate()
+  // 提交请求
+  formLoading.value = true
+  try {
+    const data = formData.value as unknown as VillageProcessingRecordsVO
+    if (formType.value === 'create') {
+      await VillageProcessingRecordsApi.createVillageProcessingRecords(data)
+      message.success(t('common.createSuccess'))
+    } else {
+      await VillageProcessingRecordsApi.updateVillageProcessingRecords(data)
+      message.success(t('common.updateSuccess'))
+    }
+    dialogVisible.value = false
+    // 发送操作成功的事件
+    emit('success')
+  } finally {
+    formLoading.value = false
+  }
+}
+
+/** 重置表单 */
+const resetForm = () => {
+  formData.value = {
+    id: undefined,
+    recoveryNum: undefined,
+    machiningTime: undefined,
+    product: undefined,
+    batchCode: undefined,
+    remark: undefined,
+  }
+  formRef.value?.resetFields()
+}
+</script>
