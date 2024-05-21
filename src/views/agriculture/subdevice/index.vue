9f95eeb35b3b37865496bb4357589ea46e522589
liujinming <1582741993@qq.com>
2024-05-21 16:04:42 +0800

    fix:bug

--- src/views/agriculture/subdevice/index.vue
+++ src/views/agriculture/subdevice/index.vue
@@ -17,15 +17,15 @@
           class="!w-240px"
         />
       </el-form-item>
-<!--      <el-form-item label="所属设备" prop="devicesId">-->
-<!--        <el-input-->
-<!--          v-model="queryParams.devicesId"-->
-<!--          placeholder="请输入所属设备"-->
-<!--          clearable-->
-<!--          @keyup.enter="handleQuery"-->
-<!--          class="!w-240px"-->
-<!--        />-->
-<!--      </el-form-item>-->
+      <!--      <el-form-item label="所属设备" prop="devicesId">-->
+      <!--        <el-input-->
+      <!--          v-model="queryParams.devicesId"-->
+      <!--          placeholder="请输入所属设备"-->
+      <!--          clearable-->
+      <!--          @keyup.enter="handleQuery"-->
+      <!--          class="!w-240px"-->
+      <!--        />-->
+      <!--      </el-form-item>-->
       <el-form-item label="子设备名称" prop="subDevicesName">
         <el-input
           v-model="queryParams.subDevicesName"
@@ -105,13 +105,9 @@
   <!-- 列表 -->
   <ContentWrap>
     <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
-<<<<<<< HEAD
-      <!-- <el-table-column label="主键" align="center" prop="id" /> -->
-      <el-table-column label="子设备编号" align="center" prop="subDevicesNum"/>
-=======
       <!--      <el-table-column label="主键" align="center" prop="id" />-->
-<!--      <el-table-column label="子设备编号" align="center" prop="subDevicesNum"/>-->
->>>>>>> 97bab8a254cb8ca8eb3086f94816009969d24b6f
+      <el-table-column label="子设备编号" align="center" prop="subDevicesNum"/>
+      <!--      <el-table-column label="子设备编号" align="center" prop="subDevicesNum"/>-->
       <el-table-column label="子设备名称" align="center" prop="subDevicesName"/>
       <el-table-column label="所属设备" align="center" prop="devicesId"/>
       <el-table-column label="所属设备名称" align="center" prop="remark"/>
@@ -242,6 +238,7 @@ const props = defineProps({
 watch(() => props.currCategory,
   () => {
     if (props.currCategory) {
+      console.log(props.currCategory.id,2222222)
       if (props.currCategory.deviceName==undefined){
         queryParams.devicesId = undefined
       }else {
@@ -320,3 +317,4 @@ onMounted(() => {
   getList()
 })
 </script>
+
