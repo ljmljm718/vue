5fd173dc5719819bb8d579cf1deeada57a95c987
LiWeiJie <3491909454@qq.com>
2024-05-24 10:40:06 +0800

    fix:模版修改

---
+++ src/views/agriculture/productmanual/createOrUpdate.vue
@@ -0,0 +1,238 @@
+//templateCreateOrUpdate
+//class="grid 2xl:grid-cols-2 gap-3 p-4"
+//class="col-span-2"
+<template>
+  <div>
+    <EditFrame>
+      <template #header>
+        <div class="flex">
+           <el-button
+            type="primary"
+            :icon="FolderChecked"
+            plain
+            @click="localSave()"
+          >
+            保存
+          </el-button>
+          <el-button
+            type="success"
+            :icon="TopRight"
+            plain
+            @click="submitForm"
+          >提交</el-button>
+          <el-button
+            type="danger"
+            :icon="Refresh"
+            plain
+            @click="resetForm()"
+          >清空
+          </el-button>
+        </div>
+      </template>
+       
+
+      <template #content>
+        <el-scrollbar class="croll-bar-template">
+        <!-- 原内容 -->
+          <el-form
+            ref="formRef"
+            :model="formData"
+            :rules="formRules"
+            label-width="100px"
+            v-loading="formLoading"
+          >
+            <el-form-item label="方案名称" prop="schemeName">
+              <el-input v-model="formData.schemeName" placeholder="请输入方案名称" />
+            </el-form-item>
+            <el-form-item label="简介" prop="briefIntroduction">
+              <el-input v-model="formData.briefIntroduction" type="textarea" placeholder="请输入简介" />
+            </el-form-item>
+            <el-form-item label="创作人" prop="marketingCreator">
+              <el-input v-model="formData.marketingCreator" placeholder="请输入创作人" />
+            </el-form-item>
+            <el-form-item label="上传时间" prop="marketingUploadTime">
+              <el-date-picker
+                v-model="formData.marketingUploadTime"
+                type="datetime"
+                value-format="x"
+                placeholder="选择上传时间"
+              />
+            </el-form-item>
+            <el-form-item label="分类" prop="marketingCategory">
+              <el-input v-model="formData.marketingCategory" placeholder="请输入分类" />
+            </el-form-item>
+            <el-form-item label="标签" prop="marketingTags">
+              <el-input v-model="formData.marketingTags" placeholder="请输入标签" />
+            </el-form-item>
+            <el-form-item label="文件管理" prop="fileManagement">
+              <UploadFile v-model="formData.fileManagement" :limit="1" />
+            </el-form-item>
+            <el-form-item label="封面图片" prop="coverImage">
+              <UploadImg v-model="formData.coverImage" />
+            </el-form-item>
+            <!-- <el-form-item label="营销推广类型" prop="marketingType">
+              <el-input v-model="formData.marketingType" placeholder="请输入营销推广类型" />
+            </el-form-item>
+            <el-form-item label="备用一" prop="reserveOne">
+              <el-input v-model="formData.reserveOne" placeholder="请输入备用一" />
+            </el-form-item>
+            <el-form-item label="备用二" prop="reserveTwo">
+              <el-input v-model="formData.reserveTwo" placeholder="请输入备用二" />
+            </el-form-item>
+            <el-form-item label="备用三" prop="reserveThree">
+              <el-input v-model="formData.reserveThree" placeholder="请输入备用三" />
+            </el-form-item> -->
+          </el-form>
+        <!-- 截至 -->
+        </el-scrollbar>
+      </template>
+    </EditFrame>
+
+  </div>
+</template>
+<script setup lang="ts">
+//自己的js内容
+import { MarketingProgramApi, MarketingProgramVO } from '@/api/agriculture/marketingprogram'
+
+/** 营销方案 表单 */
+defineOptions({ name: 'MarketingProgramForm' })
+
+const { t } = useI18n() // 国际化
+const message = useMessage() // 消息弹窗
+
+const dialogVisible = ref(false) // 弹窗的是否展示
+const dialogTitle = ref('') // 弹窗的标题
+const formLoading = ref(false) // 表单的加载中：1）修改时的数据加载；2）提交的按钮禁用
+const formType = ref('') // 表单的类型：create - 新增；update - 修改
+const formData = ref({
+  id: undefined,
+  schemeName: undefined,
+  briefIntroduction: undefined,
+  marketingCreator: undefined,
+  marketingUploadTime: undefined,
+  marketingCategory: undefined,
+  marketingTags: undefined,
+  fileManagement: undefined,
+  coverImage: undefined,
+  marketingType: 'productmanual',
+  reserveOne: undefined,
+  reserveTwo: undefined,
+  reserveThree: undefined,
+})
+const formRules = reactive({
+})
+const formRef = ref() // 表单 Ref
+
+/** 打开弹窗 */
+const open = async (type: string, id?: number) => {
+  dialogVisible.value = true
+  dialogTitle.value = t('action.' + type)
+  formType.value = type
+  resetForm()
+  // 修改时，设置数据
+  if (id) {
+    formLoading.value = true
+    try {
+      formData.value = await MarketingProgramApi.getMarketingProgram(id)
+    } finally {
+      formLoading.value = false
+    }
+  }
+}
+defineExpose({ open }) // 提供 open 方法，用于打开弹窗
+
+/** 提交表单 */
+const emit = defineEmits(['success']) // 定义 success 事件，用于操作成功后的回调
+const submitForm = async () => {
+  // 校验表单
+  await formRef.value.validate()
+  // 提交请求
+  formLoading.value = true
+  try {
+    const data = formData.value as unknown as MarketingProgramVO
+    if (!formData.value.id) {
+      await MarketingProgramApi.createMarketingProgram(data)
+      message.success(t('common.createSuccess'))
+    } else {
+      await MarketingProgramApi.updateMarketingProgram(data)
+      message.success(t('common.updateSuccess'))
+    }
+    dialogVisible.value = false
+    // 发送操作成功的事件
+    emit('success')
+
+    deleteFormStorage(
+      ROUTE_PATH,
+      formData.value.id ? formData.value.id : 'new_form'
+    )
+    // 关闭当前页面
+    useTagsViewStore().delView(router.currentRoute.value);
+    // 发送操作成功的事件
+    router.push(ORIGIN_PATH)
+  } finally {
+    formLoading.value = false
+  }
+}
+
+/** 重置表单 */
+const resetForm = () => {
+  formData.value = {
+    id: undefined,
+    schemeName: undefined,
+    briefIntroduction: undefined,
+    marketingCreator: undefined,
+    marketingUploadTime: undefined,
+    marketingCategory: undefined,
+    marketingTags: undefined,
+    fileManagement: undefined,
+    coverImage: undefined,
+    marketingType: 'productmanual',
+    reserveOne: undefined,
+    reserveTwo: undefined,
+    reserveThree: undefined,
+  }
+  formRef.value?.resetFields()
+}
+
+import {EditFrame,addFormStorage,addOrUpdateFormStorage,getFormStorage,deleteFormStorage} from '@/components/EditFrame/index'
+import { useTagsViewStore } from "@/store/modules/tagsView";
+
+// 本地保存表单
+const route = useRoute()
+const router = useRouter()
+// 下面是抽象出的基本配置
+const ROUTE_PATH = route.path
+const FORMPAGE_NAME = '产品手册'
+const ORIGIN_PATH = '/pcg/marketingCenter/productManual' // 关闭表单时跳转的路由
+
+//保存到浏览器缓存
+const localSave = () => {
+  addOrUpdateFormStorage(
+    ROUTE_PATH,
+    FORMPAGE_NAME + (formData.value.id ? '编辑' : '新增'), // TODO: 前面的表单名称写成当前页面名称
+    formData.value.id ? formData.value.id : 'new_form',
+    formData.value
+  )
+  ElMessage.success('保存成功！')
+}
+//获取浏览器缓存
+const loadData = async (id = 'new_form') => {
+  const _form = await getFormStorage(ROUTE_PATH, id)
+  if (_form) formData.value = _form.formContent
+}
+if (!formData.value.id) loadData()
+
+//起步函数
+const getFrom = async () =>{
+  resetForm();
+  if(route.query.id)  {
+    formData.value = await MarketingProgramApi.getMarketingProgram (route.query.id as any);
+    loadData(route.query.id);
+  }
+}
+
+// 方式二 调用立即执行函数
+onMounted(async () => {
+    getFrom();
+});
+</script>
--- src/views/agriculture/productmanual/templateCreate.vue
+++ src/views/agriculture/productmanual/templateCreate.vue
@@ -43,6 +43,24 @@
   </div>
 </template>
 <script setup lang="ts">
+// 需要修改的方法
+// 1.submitForm 中 if判断条件改为 !formData.value.id
+// 2.submitForm方法中 "emit('success')" 行后面添加
+// 开始
+//     deleteFormStorage(
+//       ROUTE_PATH,
+//       formData.value.id ? formData.value.id : 'new_form'
+//     )
+//     // 关闭当前页面
+//     useTagsViewStore().delView(router.currentRoute.value);
+//     // 发送操作成功的事件
+//     router.push(ORIGIN_PATH)
+// 截至
+// 3.getFrom方法中  formData.value = await "后端函数" 
+// 改为open方法中 try 下方的第一行 即"fromData.value = await MarkeryingProgramApi.getMakertingProgram(id)" 
+// 注意其中的id要改为 route.query.id as any
+// 4.const FORMPAGE_NAME = '产品手册' 将它改为菜单名称
+// 5.const ORIGIN_PATH = '/pcg/marketingCenter/productManual' // 将它改为保存之后的路由
 import {EditFrame,addFormStorage,addOrUpdateFormStorage,getFormStorage,deleteFormStorage} from '@/components/EditFrame/index'
 import { useTagsViewStore } from "@/store/modules/tagsView";
 
@@ -52,7 +70,7 @@ const router = useRouter()
 // 下面是抽象出的基本配置
 const ROUTE_PATH = route.path
 const FORMPAGE_NAME = '菜单名称'
-const ORIGIN_PATH = '/pcg/marketingCenter/productManual' // 关闭表单时跳转的路径 路由
+const ORIGIN_PATH = '/pcg/marketingCenter/productManual' // 关闭表单时跳转的路由
 
 //保存到浏览器缓存
 const localSave = () => {
@@ -75,7 +93,7 @@ if (!formData.value.id) loadData()
 const getFrom = async () =>{
   resetForm();
   if(route.query.id)  {
-    formData.value = await 查询单挑方法 (route.query.id as any);
+    formData.value = await open中的根据id查询的方法 (route.query.id as any);
     loadData(route.query.id);
   }
 }
@@ -84,7 +102,5 @@ const getFrom = async () =>{
 onMounted(async () => {
     getFrom();
 });
-// 注意需要在submit最后一行,即faill前面加--router.push(ORIGIN_PATH),即跳转回原地址
-// submit后面加入删除缓存
-// 将 !formData.value.id
+
 </script>
