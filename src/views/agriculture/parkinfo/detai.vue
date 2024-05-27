7db6275b264a0eda22a21789444d76c7f01d6da0
zhoulx <zhoulx-s@inspur.com>
2024-05-27 16:26:24 +0800

    fix:基地信息查询修改为一行3个字段

--- src/views/agriculture/parkinfo/detai.vue
+++ src/views/agriculture/parkinfo/detai.vue
@@ -43,58 +43,56 @@
       :rules="formRules"
       label-width="100px"
       v-loading="formLoading"
-    >
-<!--      <el-form-item label="编号" prop="code">-->
-<!--        <el-input v-model="formData.code" placeholder="不填后台自动生成" />-->
-<!--      </el-form-item>-->
-      <el-form-item label="名称" prop="name">
-        <el-input v-model="formData.name" placeholder="请输入名称" disabled/>
-      </el-form-item>
-      <el-form-item label="类型" prop="type">
-        <el-select v-model="formData.type" placeholder="请选择类型" style="width: 100%" disabled>
-          <el-option
-            v-for="dict in parkCategoryOptions"
-            :key="dict.value"
-            :label="dict.categoryLabel"
-            :value="dict.id"
-          />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="海拔" prop="altitude">
-        <el-input v-model="formData.altitude" placeholder="请输入海拔" disabled>
-          <template #append>米</template>
-        </el-input>
-      </el-form-item>
-      <el-form-item label="纬度" prop="latitude">
-        <el-input v-model="formData.latitude" placeholder="请输入纬度" disabled/>
-      </el-form-item>
-      <el-form-item label="经度" prop="longitude">
-        <el-input v-model="formData.longitude" placeholder="请输入经度" disabled/>
-      </el-form-item>
-      <el-form-item label="通讯地址" prop="address">
-        <el-input v-model="formData.address" placeholder="请输入通讯地址" disabled/>
-      </el-form-item>
-      <el-form-item label="联系人" prop="contact">
-        <el-input v-model="formData.contact" placeholder="请输入联系人" disabled/>
-      </el-form-item>
-      <el-form-item label="联系电话" prop="tel">
-        <el-input v-model="formData.tel" placeholder="请输入联系电话" disabled/>
-      </el-form-item>
-      <el-form-item label="面积" prop="area">
-        <el-input v-model="formData.area" placeholder="请输入面积" disabled>
-          <template #append>亩</template>
-        </el-input>
-      </el-form-item>
-      <el-form-item label="数量" prop="quantity">
-        <el-input v-model="formData.quantity" placeholder="请输入数量" disabled/>
-      </el-form-item>
-      <el-form-item label="图片" prop="img">
-        <UploadImg v-model="formData.img"  disabled/>
-      </el-form-item>
-      <el-form-item label="简介" prop="remark">
-        <el-input v-model="formData.remark" type="textarea" placeholder="请输入备注" disabled/>
-      </el-form-item>
-    </el-form>
+      class="grid 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-2 p-4"
+          >
+            <el-form-item label="名称" prop="name">
+              <el-input v-model="formData.name" placeholder="请输入名称" disabled/>
+            </el-form-item>
+            <el-form-item label="类型" prop="type">
+              <el-select v-model="formData.type" placeholder="请选择类型" style="width: 100%" disabled>
+                <el-option
+                  v-for="dict in parkCategoryOptions"
+                  :key="dict.value"
+                  :label="dict.categoryLabel"
+                  :value="dict.id"
+                />
+              </el-select>
+            </el-form-item>
+            <el-form-item label="经度" prop="longitude">
+              <el-input v-model="formData.longitude" placeholder="请输入经度" disabled/>
+            </el-form-item>
+            <el-form-item label="纬度" prop="latitude">
+              <el-input v-model="formData.latitude" placeholder="请输入纬度" disabled/>
+            </el-form-item>
+            <el-form-item label="海拔" prop="altitude">
+              <el-input v-model="formData.altitude" placeholder="请输入海拔" disabled>
+                <template #append>米</template>
+              </el-input>
+            </el-form-item>
+            <el-form-item label="面积" prop="area">
+              <el-input v-model="formData.area" placeholder="请输入面积" disabled>
+                <template #append>亩</template>
+              </el-input>
+            </el-form-item>
+            <el-form-item label="通讯地址" prop="address">
+              <el-input v-model="formData.address" placeholder="请输入通讯地址" disabled/>
+            </el-form-item>
+            <el-form-item label="数量" prop="quantity">
+              <el-input v-model="formData.quantity" placeholder="请输入数量" disabled/>
+            </el-form-item>
+            <el-form-item label="联系人" prop="contact">
+              <el-input v-model="formData.contact" placeholder="请输入联系人" disabled/>
+            </el-form-item>
+            <el-form-item label="联系电话" prop="tel">
+              <el-input v-model="formData.tel" placeholder="请输入联系电话" disabled/>
+            </el-form-item>
+            <el-form-item label="简介" prop="remark" class="col-span-2">
+              <el-input v-model="formData.remark" type="textarea" placeholder="请输入备注" disabled/>
+            </el-form-item>
+            <el-form-item label="图片" prop="img">
+              <UploadImg v-model="formData.img" disabled/>
+            </el-form-item>
+          </el-form>
     <!-- 子表的表单 -->
     <el-tabs v-model="subTabsName">
       <el-tab-pane label="地块基本信息" name="parkDetail">
