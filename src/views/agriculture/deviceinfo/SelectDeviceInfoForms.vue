4fe078f17ac978a4a488bd6f288f0d3d342d0553
LiWeiJie <3491909454@qq.com>
2024-06-12 16:04:31 +0800

    fix:设备点位

--- src/views/agriculture/deviceinfo/SelectDeviceInfoForms.vue
+++ src/views/agriculture/deviceinfo/SelectDeviceInfoForms.vue
@@ -22,10 +22,10 @@
           class="!w-240px"
         />
       </el-form-item>
-      <el-form-item label="设备名称" prop="deviceName">
+      <el-form-item label="设备点位" prop="deviceName">
         <el-input
           v-model="queryParams.deviceName"
-          placeholder="请输入设备名称"
+          placeholder="请输入设备点位"
           clearable
           @keyup.enter="handleQuery"
           class="!w-240px"
@@ -101,7 +101,7 @@
     <el-table v-loading="loading" :data="list" :stripe="true" ref="suibian" :show-overflow-tooltip="true" @select="fangfa"  @selection-change="handleSelectionChange">
       <el-table-column width="30" label="选择" type="selection"/>
       <el-table-column label="设备编号" align="center" prop="deviceCode" width="200"/>
-      <el-table-column label="设备名称" align="center" prop="deviceName" width="150"/>
+      <el-table-column label="设备点位" align="center" prop="deviceName" width="150"/>
       <el-table-column label="设备类型" align="center" prop="deviceType" width="200">
         <template #default="scope">
           <el-cascader
