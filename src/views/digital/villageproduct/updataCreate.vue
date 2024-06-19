ee2abe84f79d88158390a6a82f7d5a7b02c638f3
LiWeiJie <3491909454@qq.com>
2024-06-19 11:23:41 +0800

    fix：智慧产销，新增页样式修改 /模版修改

--- src/views/digital/villageproduct/updataCreate.vue
+++ src/views/digital/villageproduct/updataCreate.vue
@@ -4,29 +4,38 @@
 
     <EditFrame>
       <template #header v-if="show1">
-        <div class="flex">
-          <!-- <el-button
-           type="primary"
-           :icon="FolderChecked"
-           plain
-           @click="localSave()"
-         >
-           保存
-         </el-button> -->
+        <div class="flex  ">
           <el-button
-            type="success"
+            class = '!bg-[#67c23a] !color-[#fff]'
             :icon="TopRight"
             plain
             @click="submitForm"
           >提交
           </el-button>
           <el-button
-            type="danger"
+           class = '!bg-[#f56c6c] !color-[#fff]'
             :icon="Refresh"
             plain
             @click="resetForm()"
           >清空
           </el-button>
+        </div>
+        <div class="flex">
+          <el-button 
+          type="primary" 
+          plain
+          @click="router.back"
+          >
+            返回
+          </el-button>
+          <el-button
+           type="primary"
+           :icon="FolderChecked"
+           plain
+           @click="localSave()"
+         >
+           暂存
+         </el-button>
         </div>
       </template>
 
@@ -200,9 +209,10 @@ const route = useRoute()
 const router = useRouter()
 // 下面是抽象出的基本配置
 const ROUTE_PATH = route.path
-const FORMPAGE_NAME = ''
-const ORIGIN_PATH = '/pcg/production/village-product' // 关闭表单时跳转的路径
+const FORMPAGE_NAME = '特色产品'
+const ORIGIN_PATH = '/pcg/production/village-product' // 关闭表单时跳转的路由
 
+//保存到浏览器缓存
 const localSave = () => {
   addOrUpdateFormStorage(
     ROUTE_PATH,
@@ -212,6 +222,27 @@ const localSave = () => {
   )
   ElMessage.success('保存成功！')
 }
+//获取浏览器缓存
+const loadData = async (id = 'new_form') => {
+  const _form = await getFormStorage(ROUTE_PATH, id)
+  if (_form) formData.value = _form.formContent
+}
+if (!formData.value.id) loadData()
+
+//起步函数
+const getFrom = async () =>{
+  resetForm();
+  if(route.query.id)  {
+    formData.value = await VillageProductApi.getVillageProduct (route.query.id as any);
+    loadData(route.query.id);
+  }
+}
+
+// 方式二 调用立即执行函数
+onMounted(async () => {
+    getFrom();
+});
+
 //方式一:调用
 // if(route.query.id){
 //     替换成自己的
