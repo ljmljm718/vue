5f86730c25b8c47a3b56fc3c2991ba38d7d9ee9a
Tony <weijialin@inspur.com>
2024-07-01 11:19:05 +0800

    feat: 门户首页

---
+++ src/views/homePages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full h-[100vh] overflow-auto home-page-bg flex justify-center items-center">
+    <div class="grid grid-cols-3 grid-rows-3 gap-[1rem]">
+      <div class="home-item-bg"></div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style lang="scss" scoped>
+.home-page-bg {
+  background-image: url(./assets/bg.png);
+  background-size: 100% 100%;
+}
+
+.home-item-bg {
+  background-image: url(./assets/itemBg.png);
+  background-size: 100% 100%;
+}
+</style>
