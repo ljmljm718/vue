2435ea374920201abf16769c10f6365bb9d696b7
zhanglch <zhanglch-s@inspur.com>
2024-07-22 15:10:29 +0800

    fix:设备信息查看监控按钮判断条件修正

--- src/views/agriculture/deviceinfo/index.vue
+++ src/views/agriculture/deviceinfo/index.vue
@@ -246,7 +246,7 @@
           <el-button
             link
             type="primary"
-            v-if="scope.row.deviceKind=='101'&& scope.row.deviceStatus=='online'&&scope.row.url.indexOf('mp4')>0"
+            v-if="scope.row.deviceMonitorType=='视频监控'&& scope.row.deviceStatus=='online'&&scope.row.url.indexOf('mp4')>0"
             @click="openExternalLink(scope)">查看监控
           </el-button>
           <el-button
