923ab9d7d2cdb0bbf5c977382c4f639b4544b239
changxu02 <changxu02@inspur.com>
2024-07-31 10:24:08 +0800

    style: 鲁渝协作乡村振兴数商兴农大数据平台 门户首页第2版 颜色调整

--- src/views/homePages/home.vue
+++ src/views/homePages/home.vue
@@ -1,56 +1,74 @@
 <template>
-  <div class="bg-[#0e2c46] w-[100vw] h-[100vh]">
-    <ScaleBox>
+  <div class="bg-[#0E1925] w-[100vw] h-[100vh]">
+    <scale-box>
       <div class="w-full h-full overflow-hidden home-bg">
-        <div class="h-[8%] bg-[#000] flex justify-center items-center header-title mb-[1%]">
-          鲁渝协作乡村振兴数商兴农大数据平台
+        <!-- 标题 -->
+        <div class="h-[8%]">
         </div>
-        <div class="h-[91%] flex justify-center items-center relative bottom-bg bg-[#00000030]">
+        <div class="h-[91%] flex justify-center items-center relative bottom-bg bg-transparent">
           <div class="relative top-[-10em] z-20 w-[32em] h-[32em]">
-            <div class="up-down-frame" @click="routerHandler('可视化驾驶舱')">
+            <!-- 中心 -->
+            <div class="up-down-frame">
               <div class="absolute left-0 top-0 circle-bg w-[32em] h-[32em]"></div>
               <div class="absolute center-bg w-[31em] h-[31em] left-[1em] top-[1em]">
                 <div :class="['icon-frame', 'icon-' + curIcon]"></div>
+                <div class="light-bg"></div>
+                <div class="pedestal1"></div>
+                <div class="pedestal2"></div>
+                <div class="pedestal3"></div>
+                <div class="pedestal4"></div>
               </div>
             </div>
-            <div class="up-line">
-              <div class="sensor-bg relative left-[-7em] top-[-10em]" @click="routerHandler('智能感知')"></div>
-              <div class="resource-bg absolute right-[-7em] top-[-10em]" @click="routerHandler('农业资源')"></div>
+            <!-- 分支 -->
+            <div class="up-left-line cursor-pointer">
+              <div class="sensor-bg relative left-[-11em] top-[-14em]" @click="routerHandler('智能感知')"></div>
             </div>
-            <div class="left-line">
-              <div class="agri-bg relative left-[-9em] top-[1em]" @click="routerHandler('智慧农事')"></div>
+            <div class="up-right-line cursor-pointer">
+              <div class="resource-bg relative left-[4em] top-[-14em]" @click="routerHandler('农业资源')"></div>
             </div>
-            <div class="right-line">
-              <div class="digital-bg relative right-[-9em] top-[1em]" @click="routerHandler('数字产销')"></div>
+            <div class="left-line cursor-pointer">
+              <div class="agri-bg relative left-[-13em] top-[-4em]" @click="routerHandler('智慧农事')"></div>
             </div>
-            <div class="down-line">
-              <div class="monitor-bg relative left-[0em] top-[8em]" @click="routerHandler('视频监控')"></div>
+            <div class="right-line cursor-pointer">
+              <div class="digital-bg relative right-[-13em] top-[-4em]" @click="routerHandler('数字产销')"></div>
             </div>
-            <div class="rect-bg text-[#fff] text-[1.2em] relative">
-              <div class="lt-label-1 w-[15em] h-[4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-12.7em] top-[2em]" @click="routerHandler('GIS一张图')">GIS一张图</div>
-              <div class="lt-label-2 w-[15em] h-[4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-14.5em] top-[6.8em]" @click="routerHandler('数字监测')">数字监测</div>
-              <div class="lt-label-3 w-[15em] h-[4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-15.9em] top-[11.6em]" @click="routerHandler('智能预警')">智能预警</div>
-              <div class="lt-label-4 w-[15em] h-[4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-16.9em] top-[16.4em]" @click="routerHandler('设备巡检')">设备巡检</div>
+            <div class="down-left-line cursor-pointer">
+              <div class="monitor-bg relative left-[-13em] top-[3em]" @click="routerHandler('视频监控')"></div>
+            </div>
+            <div class="down-right-line cursor-pointer">
+              <div class="drive-bg relative left-[3em] top-[3em]" @click="routerHandler('可视化驾驶舱')"></div>
+            </div>
+            <!-- 两侧菜单 -->
+            <div class="rect-bg text-[#fff] text-[1.2em] relative cursor-default">
+              <!-- 左右圆弧上的翠绿色短弧和箭头 -->
+              <div class="left-decorator"></div>
+              <div class="left-arrow"></div>
+              <div class="right-decorator"></div>
+              <div class="right-arrow"></div>
+              <!-- 选项菜单 -->
+              <div class="lt-label-1 w-[10em] h-[2.4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-10.1em] top-[2em] text-[1.4em]" @click="routerHandler('GIS一张图')">GIS一张图</div>
+              <div class="lt-label-2 w-[10em] h-[2.4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-11.4em] top-[5em] text-[1.4em]" @click="routerHandler('数字监测')">数字监测</div>
+              <div class="lt-label-3 w-[10em] h-[2.4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-12.4em] top-[8em] text-[1.4em]" @click="routerHandler('智能预警')">智能预警</div>
+              <div class="lt-label-4 w-[10em] h-[2.4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-13.1em] top-[11em] text-[1.4em]" @click="routerHandler('设备巡检')">设备巡检</div>
 
-              <div class="lb-label-1 w-[15em] h-[4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-13.2em] top-[38.6em]" @click="routerHandler('农事活动')">农事活动</div>
-              <div class="lb-label-2 w-[15em] h-[4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-15em] top-[29.9em]" @click="routerHandler('物侯管理')">物侯管理</div>
-              <div class="lb-label-3 w-[15em] h-[4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-16.3em] top-[21.2em]" @click="routerHandler('种植管理')">种植管理</div>
-              <div class="lb-label-4 w-[15em] h-[4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-17.2em] top-[12.5em]" @click="routerHandler('农事概览')">农事概览</div>
+              <div class="lb-label-4 w-[10em] h-[2.4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-13.1em] top-[19em] text-[1.4em]" @click="routerHandler('农事活动')">农事活动</div>
+              <div class="lb-label-3 w-[10em] h-[2.4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-12.5em] top-[19.5em] text-[1.4em]" @click="routerHandler('物侯管理')">物侯管理</div>
+              <div class="lb-label-2 w-[10em] h-[2.4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-11.6em] top-[20em] text-[1.4em]" @click="routerHandler('种植管理')">种植管理</div>
+              <div class="lb-label-1 w-[10em] h-[2.4em] flex flex-row-reverse items-center pr-[2em] absolute left-[-10.4em] top-[20.5em] text-[1.4em]" @click="routerHandler('农事概览')">农事概览</div>
 
-              <div class="rt-label-1 w-[15em] h-[4em] flex items-center pl-[2em] absolute left-[88.4em] top-[-13.2em]" @click="routerHandler('基地总览')">基地总览</div>
-              <div class="rt-label-2 w-[15em] h-[4em] flex items-center pl-[2em] absolute left-[90.2em] top-[-12.6em]" @click="routerHandler('农资管理')">农资管理</div>
-              <div class="rt-label-3 w-[15em] h-[4em] flex items-center pl-[2em] absolute left-[91.7em] top-[-11.9em]" @click="routerHandler('仓储管理')">仓储管理</div>
-              <div class="relative w-[15em] h-[4em] flex items-center pl-[2em] absolute left-[92.6em] top-[-11.2em]"></div>
+              <div class="rt-label-1 w-[10em] h-[2.4em] flex items-center pl-[2em] absolute left-[64.5em] top-[-6em] text-[1.4em]" @click="routerHandler('基地总览')">基地总览</div>
+              <div class="rt-label-2 w-[10em] h-[2.4em] flex items-center pl-[2em] absolute left-[65.7em] top-[-5.5em] text-[1.4em]" @click="routerHandler('农资管理')">农资管理</div>
+              <div class="rt-label-3 w-[10em] h-[2.4em] flex items-center pl-[2em] absolute left-[66.6em] top-[-5em] text-[1.4em]" @click="routerHandler('仓储管理')">仓储管理</div>
 
-              <div class="rb-label-1 w-[15em] h-[4em] flex items-center pl-[2em] absolute left-[88.5em] top-[7.2em]" @click="routerHandler('特色产品')">特色产品</div>
-              <div class="rb-label-2 w-[15em] h-[4em] flex items-center pl-[2em] absolute left-[90.4em] top-[-1.5em]" @click="routerHandler('营销管理')">营销管理</div>
-              <div class="rb-label-3 w-[15em] h-[4em] flex items-center pl-[2em] absolute left-[91.7em] top-[-10.1em]" @click="routerHandler('销售管理')">销售管理</div>
-              <div class="rb-label-4 w-[15em] h-[4em] flex items-center pl-[2em] absolute left-[92.6em] top-[-18.8em]" @click="routerHandler('生产管理')">生产管理</div>
+              <div class="rb-label-4 w-[10em] h-[2.4em] flex items-center pl-[2em] absolute left-[67.1em] top-[2em] text-[1.4em]" @click="routerHandler('生产管理')">生产管理</div>
+              <div class="rb-label-3 w-[10em] h-[2.4em] flex items-center pl-[2em] absolute left-[66.5em] top-[2.5em] text-[1.4em]" @click="routerHandler('销售管理')">销售管理</div>
+              <div class="rb-label-2 w-[10em] h-[2.4em] flex items-center pl-[2em] absolute left-[65.6em] top-[3em] text-[1.4em]" @click="routerHandler('营销管理')">营销管理</div>
+              <div class="rb-label-1 w-[10em] h-[2.4em] flex items-center pl-[2em] absolute left-[64.4em] top-[3.5em] text-[1.4em]" @click="routerHandler('特色产品')">特色产品</div>
             </div>
           </div>
         </div>
       </div>
-    </ScaleBox>
+    </scale-box>
   </div>
 </template>
 <script setup lang="ts">
@@ -110,7 +128,7 @@ const routerHandler = (item:string) => {
 </script>
 <style lang="scss" scoped>
 .home-bg {
-  background-image: url(./assets/new_home/bg.png);
+  background-image: url(./assets/homeV2/bg.png);
   background-size: 100% 100%;
   font-size: 12px;
 }
@@ -128,33 +146,72 @@ const routerHandler = (item:string) => {
   }
 }
 
-.header-title {
-  background-image: linear-gradient(to top, #6EEBFF, #FFFFFF);
-  -webkit-background-clip: text;
-  color: transparent;
-  font-size: 3em;
-  font-family: "ArtFont";
-}
-
 @keyframes spin {
   0% { transform: rotate(0deg); } /* 开始时旋转0度 */
   100% { transform: rotate(360deg); } /* 结束时旋转360度，即一圈 */
 }
 .circle-bg {
-  background-image: url(./assets/new_home/circle.png);
+  background-image: url(./assets/homeV2/circle.png);
   background-size: 100% 100%;
   animation: spin 2s linear infinite;
 }
+.light-bg {
+  background-image: url(./assets/homeV2/light-bg.png);
+  background-size: 100% 100%;
+  position: absolute;
+  top: 14em;
+  left: 5.6em;
+  width: 20em;
+  height: 10em;
+  z-index: 5;
+}
+.pedestal1 {
+  background-image: url(./assets/homeV2/pedestal1.png);
+  background-size: 100% 100%;
+  position: absolute;
+  top: 21.5em;
+  left: 12.3em;
+  width: 6.7em;
+  height: 1.7em;
+  z-index: 4;
+}
+.pedestal2 {
+  background-image: url(./assets/homeV2/pedestal2.png);
+  background-size: 100% 100%;
+  position: absolute;
+  top: 20.8em;
+  left: 9.4em;
+  width: 12.4em;
+  height: 3.4em;
+  z-index: 3;
+}
+.pedestal3 {
+  background-image: url(./assets/homeV2/pedestal3.png);
+  background-size: 100% 100%;
+  position: absolute;
+  top: 22.5em;
+  left: 9.4em;
+  width: 12.4em;
+  height: 2.5em;
+  z-index: 2;
+}
+.pedestal4 {
+  background-image: url(./assets/homeV2/pedestal4.png);
+  background-size: 100% 100%;
+  position: absolute;
+  top: 22em;
+  left: 6.5em;
+  width: 18em;
+  height: 4.6em;
+  z-index: 1;
+}
 
 .bottom-bg {
-  background-image: url(./assets/new_home/bottomBg.gif);
-  background-size: 100% auto;
-  background-repeat: no-repeat;
-  background-position: center center;
+  background-color: transparent;
 }
 
 .center-bg {
-  background-image: url(./assets/new_home/centerBg.png);
+  background-image: url(./assets/homeV2/centerBg2.png);
   background-size: 100% 100%;
 }
 
@@ -168,52 +225,68 @@ const routerHandler = (item:string) => {
   animation: bounce 2s ease-in-out infinite alternate;
 }
 
-.up-line, .down-line, .left-line, .right-line {
+.up-left-line, .up-right-line, .down-left-line, .down-right-line, .left-line, .right-line {
   position: relative;
   background-size: 100% 100%;
   z-index: 999;
 }
-.up-line {
-  background-image: url(./assets/new_home/upLine.png);
-  width: 70em;
+.up-left-line {
+  background-image: url(./assets/homeV2/up-left-line.png);
+  width: 9em;
   height: 8em;
-  left: -19em;
-  top: 9em;
+  left: -7em;
+  top: 12em;
 }
-
-.down-line {
-  background-image: url(./assets/new_home/downLine.png);
-  width: 10em;
-  height: 18em;
-  left: 8em;
+.up-right-line {
+  background-image: url(./assets/homeV2/up-right-line.png);
+  width: 9em;
+  height: 8em;
+  left: 30em;
   top: 4em;
 }
 
+.down-left-line {
+  background-image: url(./assets/homeV2/down-left-line.png);
+  width: 6em;
+  height: 11em;
+  left: 4em;
+  top: 8em;
+}
+
+.down-right-line {
+  background-image: url(./assets/homeV2/down-right-line.png);
+  width: 6em;
+  height: 11em;
+  left: 22em;
+  top: -3em;
+}
+
 .left-line {
-  background-image: url(./assets/new_home/leftLine.png);
-  width: 15em;
-  height: 10em;
-  left: -11em;
-  top: 19em;
+  background-image: url(./assets/homeV2/left-line.png);
+  width: 16em;
+  height: 3em;
+  left: -13em;
+  top: 8em;
 }
 
 .right-line {
-  background-image: url(./assets/new_home/rightLine.png);
-  width: 14em;
-  height: 10em;
+  background-image: url(./assets/homeV2/right-line.png);
+  width: 16em;
+  height: 3em;
   left: 30em;
-  top: 10em;
+  top: 5em;
 }
 
-.sensor-bg { background-image: url(./assets/new_home/sensor.png); }
-.resource-bg { background-image: url(./assets/new_home/resource.png); }
-.monitor-bg { background-image: url(./assets/new_home/monitor.png); }
-.digital-bg { background-image: url(./assets/new_home/digital.png); }
-.agri-bg { background-image: url(./assets/new_home/agri.png); }
+.sensor-bg { background-image: url(./assets/homeV2/sensor.png); }
+.resource-bg { background-image: url(./assets/homeV2/resource.png); }
+.monitor-bg { background-image: url(./assets/homeV2/monitor.png); }
+.digital-bg { background-image: url(./assets/homeV2/digital.png); }
+.agri-bg { background-image: url(./assets/homeV2/agri.png); }
+.drive-bg { background-image: url(./assets/homeV2/drive.png); }
 
-.sensor-bg, .resource-bg, .monitor-bg, .digital-bg, .agri-bg {
+.sensor-bg, .resource-bg, .monitor-bg, .digital-bg, .agri-bg, .drive-bg {
   width: 16em;
-  height: 15em;
+  height: 17em;
   background-size: 100% 100%;
   &:hover {
     transform: scale(1.1);
@@ -222,18 +295,59 @@ const routerHandler = (item:string) => {
 }
 
 .rect-bg {
-  background-image: url(./assets/new_home/rect.png);
+  background-image: url(./assets/homeV2/rect.png);
   background-size: 100% 100%;
   width: 92.5em;
+  height: 46em;
   aspect-ratio: 2.07;
   position: absolute;
   left: -33em;
   top: 0em;
 }
 
+.left-decorator {
+  background-image: url(./assets/homeV2/left-decorator.png);
+  background-size: 100% 100%;
+  width: .8em;
+  height: 10em;
+  position: absolute;
+  top: 18em;
+  left: -1.3em;
+}
+
+.left-arrow {
+  background-image: url(./assets/homeV2/left-arrow.png);
+  background-size: 100% 100%;
+  width: 0.8em;
+  height: 1.6em;
+  position: absolute;
+  top: 22em;
+  left: -2.6em;
+}
+
+.right-decorator {
+  background-image: url(./assets/homeV2/right-decorator.png);
+  background-size: 100% 100%;
+  width: .8em;
+  height: 10em;
+  position: absolute;
+  top: 18em;
+  right: -1.3em;
+}
+
+.right-arrow {
+  background-image: url(./assets/homeV2/right-arrow.png);
+  background-size: 100% 100%;
+  width: 0.8em;
+  height: 1.6em;
+  position: absolute;
+  top: 22em;
+  right: -2.6em;
+}
+
 @for $i from 1 through 7 {
   .icon-#{$i} {
-    background-image: url(./assets/new_home/icon#{$i}.png);
+    background-image: url(./assets/homeV2/icon#{$i}.png);
   }
 }
 
@@ -246,16 +360,20 @@ const routerHandler = (item:string) => {
   position: relative;
   left: 8em;
   top: 9em;
+  z-index: 6;
 }
 
+// lb-label-1的背景图是lt-label-4的上下翻转，依次类推
+$j: 4;
 @for $i from 1 through 4 {
   .lt-label-#{$i} {
-    background-image: url(./assets/new_home/label#{$i}.png);
+    background-image: url(./assets/homeV2/label#{$i}.png);
     background-size: 100% 100%;
   }
   .lt-label-#{$i}:hover {
-    background-image: url(./assets/new_home/slabel#{$i}.png);
+    background-image: url(./assets/homeV2/slabel#{$i}.png);
     background-size: 100% 100%;
+    color: #11F47F;
   }
 
   .lb-label-#{$i} {
@@ -269,7 +387,7 @@ const routerHandler = (item:string) => {
     top: 0;
     left: 0;
     z-index: -1;
-    background: url(./assets/new_home/label#{$i}.png) no-repeat center center;
+    background: url(./assets/homeV2/label#{$i}.png) no-repeat center center;
     background-size: 100% 100%;
     transform:scaleY(-1);
   }
@@ -281,9 +399,12 @@ const routerHandler = (item:string) => {
     top: 0;
     left: 0;
     z-index: -1;
-    background: url(./assets/new_home/slabel#{$i}.png) no-repeat center center;
+    background: url(./assets/homeV2/slabel#{$i}.png) no-repeat center center;
     background-size: 100% 100%;
     transform:scaleY(-1);
+  }
+  .lb-label-#{$i}:hover {
+    color: #11F47F;
   }
 
   .rt-label-#{$i} {
@@ -297,7 +418,7 @@ const routerHandler = (item:string) => {
     top: 0;
     left: 0;
     z-index: -1;
-    background: url(./assets/new_home/label#{$i}.png) no-repeat center center;
+    background: url(./assets/homeV2/label#{$i}.png) no-repeat center center;
     background-size: 100% 100%;
     transform: scaleX(-1);
   }
@@ -309,9 +430,12 @@ const routerHandler = (item:string) => {
     top: 0;
     left: 0;
     z-index: -1;
-    background: url(./assets/new_home/slabel#{$i}.png) no-repeat center center;
+    background: url(./assets/homeV2/slabel#{$i}.png) no-repeat center center;
     background-size: 100% 100%;
     transform: scaleX(-1);
+  }
+  .rt-label-#{$i}:hover {
+    color: #11F47F;
   }
 
   .rb-label-#{$i} {
@@ -325,7 +449,7 @@ const routerHandler = (item:string) => {
     top: 0;
     left: 0;
     z-index: -1;
-    background: url(./assets/new_home/label#{$i}.png) no-repeat center center;
+    background: url(./assets/homeV2/label#{$i}.png) no-repeat center center;
     background-size: 100% 100%;
     transform: scaleY(-1) scaleX(-1);
   }
@@ -337,9 +461,12 @@ const routerHandler = (item:string) => {
     top: 0;
     left: 0;
     z-index: -1;
-    background: url(./assets/new_home/slabel#{$i}.png) no-repeat center center;
+    background: url(./assets/homeV2/slabel#{$i}.png) no-repeat center center;
     background-size: 100% 100%;
     transform: scaleY(-1) scaleX(-1);
+  }
+  .rb-label-#{$i}:hover {
+    color: #11F47F;
   }
 }
 </style>
