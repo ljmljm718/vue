c893a9b6b5f9c2c217c857108fbf376604660f9c
Zxuyoubin <xuyoubin@inspur.com>
2024-07-31 11:50:33 +0800

    fix：登录页样式优化

--- src/views/Login/components/LoginForm.vue
+++ src/views/Login/components/LoginForm.vue
@@ -397,4 +397,19 @@ onMounted(() => {
   color: #46696c;
   transition: background-color 50000s ease-in-out 0s; 
 }
+
+
+::v-deep .el-checkbox__input.is-checked .el-checkbox__inner,.el-checkbox__input.is-indeterminate .el-checkbox__inner {
+  background-color: #46696c !important;
+  border: 1px solid #46696c !important;
+}
+::v-deep .el-checkbox__inner:hover {
+  border-color: #46696c;
+}
+::v-deep .el-checkbox__input.is-checked + .el-checkbox__label {
+  color: #46696c;
+}
+::v-deep .el-checkbox__input.is-focus .el-checkbox__inner {
+  border-color: #46696c!important;
+}
 </style>
