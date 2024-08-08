d940295fc4ec8ece77b806c979dd23c158bd504d
liujinming <1582741993@qq.com>
2024-08-08 15:33:49 +0800

    fix:种植管理查询优化

--- src/views/agriculture/cropbase/index.vue
+++ src/views/agriculture/cropbase/index.vue
@@ -8,44 +8,50 @@
       :inline="true"
       label-width="68px"
     >
-      <!--      <el-form-item label="编号" prop="cropCode">-->
-      <!--        <el-input-->
-      <!--            v-model="queryParams.cropCode"-->
-      <!--            placeholder="请输入编号"-->
-      <!--            clearable-->
-      <!--            @keyup.enter="handleQuery"-->
-      <!--            class="!w-240px"-->
-      <!--        />-->
-      <!--      </el-form-item>-->
-      <el-form-item label="种植品种" prop="cropName">
+<!--            <el-form-item label="编号" prop="cropCode">-->
+<!--              <el-input-->
+<!--                  v-model="queryParams.cropCode"-->
+<!--                  placeholder="请输入编号"-->
+<!--                  clearable-->
+<!--                  @keyup.enter="handleQuery"-->
+<!--                  class="!w-240px"-->
+<!--              />-->
+<!--            </el-form-item>-->
+      <el-form-item label="基地名称" prop="parkName">
+        <el-input v-model="queryParams.parkName" placeholder="请选择">
+          <template #append>
+            <el-button @click="openParkPopup('0')">
+              <Icon icon="ep:search" />
+              选择
+            </el-button>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item label="地块名称" prop="plotName">
+        <el-input v-model="queryParams.plotName" placeholder="请选择">
+          <template #append>
+            <el-button @click="openPlotPopup(queryParams.belongPark)">
+              <Icon icon="ep:search" />
+              选择
+            </el-button>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item label="批次号" prop="batchCode">
         <el-input
-          v-model="queryParams.cropName"
-          placeholder="请输入种植品种"
+          v-model="queryParams.batchCode"
+          placeholder="请输入批次号"
           clearable
           @keyup.enter="handleQuery"
           class="!w-240px"
         />
       </el-form-item>
-      <el-form-item label="品类" prop="cropType">
-        <el-select v-model="queryParams.cropType" clearable placeholder="请选择品类"
-                   class="!w-240px">
-          <el-option
-            v-for="item in listCategoryManagement"
-            :key="item.id"
-            :label="item.categoryName"
-            :value="item.id"/>
+      <el-form-item label="采收状态" prop="recoveryNo">
+        <el-select v-model="queryParams.recoveryNo" placeholder="请选择采收状态" clearable   class="!w-240px">
+          <el-option label="全部" value="" />
+          <el-option label="已采收" value="true" />
+          <el-option label="未采收" value="false" />
         </el-select>
-      </el-form-item>
-      <el-form-item label="创建时间" prop="createTime">
-        <el-date-picker
-          v-model="queryParams.createTime"
-          value-format="YYYY-MM-DD HH:mm:ss"
-          type="daterange"
-          start-placeholder="开始日期"
-          end-placeholder="结束日期"
-          :default-time="[new Date('1 00:00:00'), new Date('1 23:59:59')]"
-          class="!w-240px"
-        />
       </el-form-item>
       <el-form-item>
         <el-button @click="handleQuery" type="primary">
@@ -137,7 +143,7 @@
           />
         </template>
       </el-table-column>
-      <el-table-column label="采收状态" align="center" prop="recoveryNo">
+      <el-table-column label="采收状态" align="center" prop="recoveryNo" width="120">
         <template #default="scope">
           <el-tag type="success" v-if="scope.row.recoveryNo">已采收</el-tag>
           <el-tag v-if="!scope.row.recoveryNo">未采收</el-tag>
@@ -153,7 +159,7 @@
         :formatter="dateFormatter"
         width="180px"
       />
-      <el-table-column label="操作" align="center" width="220" fixed="right">
+      <el-table-column label="操作" align="center" width="250" fixed="right">
         <template #default="scope">
           <el-button
             link
@@ -253,6 +259,10 @@
       </div>
     </template>
   </el-drawer>
+  <!--  选择基地-->
+  <ParkInfoPopup ref="parkPopupRef" @success="handleParkPopupChange" />
+  <!--  选择地块-->
+  <ParkDetailPopup ref="plotPopupRef" @success="handlePlotPopupChange" />
 </template>
 
 <script setup lang="ts">
@@ -272,6 +282,10 @@ import {useUserStore} from "@/store/modules/user";
 import avatarImg from "@/assets/imgs/avatar.gif";
 import {CategoryManagementVO, allDataCacheManager} from "@/api/agriculture/categorymanagement";
 import {getUserProfile} from "@/api/system/user/profile";
+import ParkDetailPopup from "@/views/agriculture/parkdetail/components/ParkDetailPopup.vue";
+import ParkInfoPopup from "@/views/agriculture/parkinfo/components/ParkInfoPopup.vue";
+import {ParkInfoVO} from "@/api/agriculture/parkinfo";
+import {ParkDetailVO} from "@/api/agriculture/parkdetail";
 
 /** 鲁渝协作品种管理 列表 */
 defineOptions({name: 'AgriCropBase'})
@@ -290,7 +304,11 @@ const queryParams = reactive({
   cropName: undefined,
   cropType: undefined,
   createTime: [],
+  batchCode: undefined,
+  recoveryNo: undefined,
   belongPark: undefined,
+  parkName: undefined,
+  plotName: undefined,
   belongPlot: undefined,
   deptId: undefined,
   userId: undefined,
@@ -351,6 +369,38 @@ const getList = async () => {
   } finally {
     loading.value = false
   }
+}
+
+
+//基地的选择
+const parkPopupRef = ref()
+const openType = ref('')
+const openParkPopup = (id: string) => {
+  openType.value = id
+  if (openType.value === undefined || openType.value === '') {
+    message.error('请选择基地')
+  } else parkPopupRef.value.open(id)
+}
+const handleParkPopupChange = (order: ParkInfoVO) => {
+  if (openType.value === '0') {
+    queryParams.belongPark = String(order[0].id)
+    queryParams.parkName = String(order[0].name)
+  } else queryParams.parkName = String(order[0].name)
+}
+
+//地块的选择
+const plotPopupRef = ref()
+const openType1 = ref('')
+const openPlotPopup = (id: string) => {
+  openType1.value = id
+  if (!openType1.value) {
+    message.error('请选择基地')
+  } else plotPopupRef.value.open(id)
+}
+const handlePlotPopupChange = (order: ParkDetailVO) => {
+  console.log('--->>查看选择的地块信息：', order[0])
+  queryParams.belongPlot = String(order[0].id)
+  queryParams.plotName = String(order[0].name)
 }
 
 /** 搜索按钮操作 */
