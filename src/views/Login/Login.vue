f237417022cbdc9bf0d5446b0b12bebe150468f7
Tony <weijialin@inspur.com>
2024-08-12 11:34:45 +0800

    style: 登录页样式微调

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -4,56 +4,52 @@
     class="relative h-[100%] lt-md:px-10px lt-sm:px-10px lt-xl:px-10px lt-xl:px-10px login"
   >
     <div class="logo"></div>
-    <scale-box>
-      <div class="relative mx-auto h-full flex">
-        <div class="relative relative2 flex-1 p-30px dark:bg-[var(--login-bg-color)] lt-sm:p-10px">
-          <!-- 右上角的主题、语言选择 -->
-          <!-- <div
-          class="flex items-center justify-between text-white at-2xl:justify-end at-xl:justify-end"
-        >
-          <div class="flex items-center at-2xl:hidden at-xl:hidden">
-            <img alt="" class="mr-10px h-48px w-48px" src="@/assets/imgs/logo.png" />
-            <span class="text-20px font-bold">{{ underlineToHump(appStore.getTitle) }}</span>
-          </div>
-          <div class="flex items-center justify-end space-x-10px">
-            <ThemeSwitch />
-            <LocaleDropdown class="dark:text-white lt-xl:text-white" />
-          </div>
-        </div>  -->
-          <!-- 右边的登录界面 -->
-          <!-- <Transition appear enter-active-class="animate__animated animate__bounceInRight" style="color:#fff">
-          
-          <div 
-            class="m-auto h-full w-[100%] flex items-center at-2xl:max-w-800px at-lg:max-w-500px at-md:max-w-500px at-xl:max-w-500px;"
-          > -->
-          <!-- 账号登录 -->
-          <!-- <LoginForm class="m-auto h-auto p-20px lt-xl:(rounded-3xl light:bg-white)"/> -->
-          <!-- 手机登录 -->
-          <!-- <MobileForm class="m-auto h-auto p-20px lt-xl:(rounded-3xl light:bg-white)"/> -->
-          <!-- 二维码登录 -->
-          <!-- <QrCodeForm class="m-auto h-auto p-20px lt-xl:(rounded-3xl light:bg-white)" /> -->
-          <!-- 注册 -->
-          <!-- <RegisterForm class="m-auto h-auto p-20px lt-xl:(rounded-3xl light:bg-white)" /> -->
-          <!-- 三方登录 -->
-          <!-- <SSOLoginVue class="m-auto h-auto p-20px lt-xl:(rounded-3xl light:bg-white)" /> -->
-          <!-- </div> -->
-          <!-- </Transition> -->
-          <div class="cont">
-            <div class="left w-[68rem] aspect-[1.56]">
-            </div>
-            <div class="w-[36rem] ml-[8rem] ">
-              <LoginForm />
-            </div>
+    <div class="absolute left-[13vw] left-img w-[42vw] top-[25vh] aspect-[1.56]">
+    </div>
+    <div class="relative mx-auto h-full flex">
+      <div class="relative relative2 flex-1 p-30px dark:bg-[var(--login-bg-color)] lt-sm:p-10px overflow-hidden">
+        <!-- 右上角的主题、语言选择 -->
+        <!-- <div
+        class="flex items-center justify-between text-white at-2xl:justify-end at-xl:justify-end"
+      >
+        <div class="flex items-center at-2xl:hidden at-xl:hidden">
+          <img alt="" class="mr-10px h-48px w-48px" src="@/assets/imgs/logo.png" />
+          <span class="text-20px font-bold">{{ underlineToHump(appStore.getTitle) }}</span>
+        </div>
+        <div class="flex items-center justify-end space-x-10px">
+          <ThemeSwitch />
+          <LocaleDropdown class="dark:text-white lt-xl:text-white" />
+        </div>
+      </div>  -->
+        <!-- 右边的登录界面 -->
+        <!-- <Transition appear enter-active-class="animate__animated animate__bounceInRight" style="color:#fff">
+        
+        <div 
+          class="m-auto h-full w-[100%] flex items-center at-2xl:max-w-800px at-lg:max-w-500px at-md:max-w-500px at-xl:max-w-500px;"
+        > -->
+        <!-- 账号登录 -->
+        <!-- <LoginForm class="m-auto h-auto p-20px lt-xl:(rounded-3xl light:bg-white)"/> -->
+        <!-- 手机登录 -->
+        <!-- <MobileForm class="m-auto h-auto p-20px lt-xl:(rounded-3xl light:bg-white)"/> -->
+        <!-- 二维码登录 -->
+        <!-- <QrCodeForm class="m-auto h-auto p-20px lt-xl:(rounded-3xl light:bg-white)" /> -->
+        <!-- 注册 -->
+        <!-- <RegisterForm class="m-auto h-auto p-20px lt-xl:(rounded-3xl light:bg-white)" /> -->
+        <!-- 三方登录 -->
+        <!-- <SSOLoginVue class="m-auto h-auto p-20px lt-xl:(rounded-3xl light:bg-white)" /> -->
+        <!-- </div> -->
+        <!-- </Transition> -->
+        <div class="cont">
+          <div class="w-[36rem] ml-[54vw]">
+            <LoginForm />
           </div>
         </div>
       </div>
-    </scale-box>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
 import { underlineToHump } from '@/utils'
-import ScaleBox from 'vue3-scale-box'
-
 import { useDesign } from '@/hooks/web/useDesign'
 import { useAppStore } from '@/store/modules/app'
 import { ThemeSwitch } from '@/layout/components/ThemeSwitch'
@@ -107,10 +103,6 @@ $prefix-cls: #{$namespace}-login;
     overflow: hidden;
     display: flex;
     align-items: center;
-    .left {
-      background-size: 100% 100%;
-      background-image: url('./assets/leftContent.png');
-    }
   }
 }
 
@@ -124,10 +116,15 @@ $prefix-cls: #{$namespace}-login;
     z-index: 999;
     left: 2vw;
     width: 40vw;
-    aspect-ratio: 8;
+    aspect-ratio: 8.6;
     background-size: 100% 100%;
     object-fit: container;
     background-image: url('./assets/leftTitle.png');
   }
+}
+
+.left-img {
+  background-size: 100% 100%;
+  background-image: url('./assets/leftContent.png');
 }
 </style>
