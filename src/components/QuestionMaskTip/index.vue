d855044cc2109775b42be6bc358c5992438fef0c
zhuqing <zhuqing02@inspur.com>
2024-08-21 18:51:17 +0800

    feat 问号提示组件

---
+++ src/components/QuestionMaskTip/index.vue
@@ -0,0 +1,31 @@
+<template>
+ 
+  <el-tooltip :content="prop.content" :placement="prop.placement" effect="light" popper-class="tooltip-width">
+   
+  <el-icon size="15px" color="#b8b9b9" style="margin-right: 10px">
+    <QuestionFilled />
+    </el-icon>
+  </el-tooltip>
+
+</template>
+
+<script setup lang="ts">
+import { defineOptions } from 'vue'
+import { defineProps } from 'vue'
+
+defineOptions({ name: 'QuestionMaskTip' })
+const prop = defineProps({
+  content: String,
+  placement:{
+    type: String as PropType<'top' | 'bottom' | 'left' | 'right'>,
+    default : 'top'
+  }
+})
+</script>
+
+<style lang="less">
+.tooltip-width {
+  max-width: 150px;
+
+}
+</style>
