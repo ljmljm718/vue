d7269363a6f5fc1ce7bc3813b2b261c634187040
xuweimin <xuweimin@inspur.com>
2024-09-02 15:57:36 +0800

    fix:优化健康等级管理页面

--- src/views/agriculture/healthlevel/HealthLevelForm.vue
+++ src/views/agriculture/healthlevel/HealthLevelForm.vue
@@ -25,13 +25,12 @@
           clearable
         >
           <el-option
-            v-for="dict in getIntDictOptions(DICT_TYPE.AGRI_HEALTH_LEVEL)"
+            v-for="dict in getStrDictOptions(DICT_TYPE.AGRI_HEALTH_LEVEL)"
             :key="dict.value"
             :label="dict.label"
             :value="dict.value"
           />
         </el-select>
-
       </el-form-item>
       <el-form-item label="健康等级上限分" prop="max">
         <el-input v-model="formData.max" placeholder="请输入健康等级上限分" />
@@ -68,7 +67,7 @@ import { HealthLevelApi, HealthLevelVO } from '@/api/agriculture/healthlevel'
 import BreedFrom from "@/views/agriculture/varietymanagement/SelectVarirtManagement.vue";
 //模型管理页面
 import ModelFrom from "@/views/agriculture/modelmanagement/ModelSelectPopup.vue";
-import { getIntDictOptions, DICT_TYPE } from '@/utils/dict'
+import { getStrDictOptions, DICT_TYPE } from '@/utils/dict'
 
 /** 健康等级 表单 */
 defineOptions({ name: 'HealthLevelForm' })
