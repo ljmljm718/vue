c81cd65b3ae198c986c11c2950646c624f15e075
Tony <weijialin@inspur.com>
2024-09-09 15:21:27 +0800

    feat:: 登录页加前缀

--- src/router/modules/remaining.ts
+++ src/router/modules/remaining.ts
@@ -201,6 +201,17 @@ const remainingRouter: AppRouteRecordRaw[] = [
       }
     ]
   },
+  ...[
+    '/mingyue',
+    '/shuangqiao',
+    '/erdu',
+    '/tianyin'
+  ].map(subItem => ({
+    path: subItem + '/login',
+    component: () => import('@/views/Login/Login.vue'),
+    name: subItem,
+    meta: { hidden: true }
+  })),
   {
     path: '/login',
     component: () => import('@/views/Login/Login.vue'),
