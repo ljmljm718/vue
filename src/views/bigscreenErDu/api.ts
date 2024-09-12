3d409548cbdf38aee15e14071f64a0d492c24432
zhuqing <zhuqing02@inspur.com>
2024-09-12 18:07:55 +0800

    feat 二渡村种养信息

--- src/views/bigscreenErDu/api.ts
+++ src/views/bigscreenErDu/api.ts
@@ -3,3 +3,14 @@ import request from '@/config/axios'
 export const exam = async () => {
   return request.get({ url: '/exam'})
 }
+export const cropBase = async ( params) => {
+  return request.get({ 
+    url: '/agriculture/crop-base/page',
+    params
+  })
+}
+export const warnRecordInfo = async() =>{
+  return request.get({
+    url:'/agriculture/warning-record-big-screen/qianjiangWarnRecordInfo'
+  })
+}
