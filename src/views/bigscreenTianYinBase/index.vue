637643ef87f0c7b8c203c1ad8fff339fd4d90e4c
wangyilan <3159653750@qq.com>
2024-09-27 10:11:55 +0800

    fix

--- src/views/bigscreenTianYinBase/index.vue
+++ src/views/bigscreenTianYinBase/index.vue
@@ -74,7 +74,7 @@ export default defineComponent({
           <BigscreenMain>
             <div class="bg-[#0d1724] w-full h-full relative overflow-hidden">
               <div class="absolute z-2 w-full h-full">
-                {/* <CesiumMap /> */}
+                <CesiumMap />
                 <div class="meng-ban z-0"></div>
               </div>
               <div
