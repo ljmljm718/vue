a7faf140809a113bdb94a4c9acff7f246ef9ade0
Tony <weijialin@inspur.com>
2024-10-15 16:06:30 +0800

    fix: BUG

--- src/views/Home/panelTangBa.vue
+++ src/views/Home/panelTangBa.vue
@@ -68,7 +68,7 @@
               </div>
             </div>
           </div>
-          <div class="w-full box-border p-5 bg-#00000020" v-show="curDeviceKind === '101' || curVideoLink">
+          <div class="w-full box-border p-5 bg-#00000020" v-show="curDeviceKind === '101'">
             <video :src="curVideoLink" autoplay controls muted loop class="w-full aspect-video"></video>
           </div>
           <div v-show="curDeviceKind === '102'" class="py-3 pb-[42px]">
