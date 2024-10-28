e4ffe8d4c26493b52ec58e5dbdedf15d8000b5c0
wangyilan <3159653750@qq.com>
2024-10-28 09:44:42 +0800

    feat:茶竹村首页

--- src/views/Home/base.vue
+++ src/views/Home/base.vue
@@ -13,6 +13,7 @@
     <homeShuangQiao v-else-if="deptId === 156" />
     <HomeErdu v-else-if="deptId === 161" />
     <HomeTaiYuan v-else-if="deptId === 166 || deptId === 165" />
+    <HomeChaZhu v-else-if="deptId === 168" />
     <div v-else v-loading class="w-full h-[30rem]"></div>
   </div>
 </template>
@@ -31,6 +32,7 @@ import HomeTangBa from './homeTangBa.vue'
 import homeShuangQiao from './homeShuangQiao.vue'
 import HomeErdu from './homeErDu.vue'
 import HomeTaiYuan from './homeTaiYuan.vue'
+import HomeChaZhu from './homeChaZhu.vue'
 import { ref } from 'vue'
 const deptId = ref(0)
 const judgeHomePage = async () => {
