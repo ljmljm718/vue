c0bb94996fc7fc16afc3a26b34df2bf3c8ed5a6b
lvjiahao <lvjiahao@inspur.com>
2024-11-11 14:28:28 +0800

    fix:查询条件一行三列对齐及展开样式调整

--- src/views/agriculture/diseasepestsurveillance/index.vue
+++ src/views/agriculture/diseasepestsurveillance/index.vue
@@ -6,7 +6,7 @@
       :model="queryParams"
       ref="queryFormRef"
       :inline="true"
-      label-width="68px"
+      label-width="88px"
     >
       <el-row>
         <el-form-item label="设备" prop="deviceName">
--- src/views/agriculture/feedingstrategy/index.vue
+++ src/views/agriculture/feedingstrategy/index.vue
@@ -6,7 +6,7 @@
       :model="queryParams"
       ref="queryFormRef"
       :inline="true"
-      label-width="68px"
+      label-width="88px"
     >
       <el-form-item label="生长期" prop="growthPeriod">
         <el-input
