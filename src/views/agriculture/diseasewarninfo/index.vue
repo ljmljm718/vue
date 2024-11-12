f145fb69fd6ff93844dcc4f30591c8023f37042d
wangyilan <3159653750@qq.com>
2024-11-12 09:38:23 +0800

    fix:病虫害预警记录数据显示问题

--- src/views/agriculture/diseasewarninfo/index.vue
+++ src/views/agriculture/diseasewarninfo/index.vue
@@ -530,6 +530,7 @@ const formatTimestamp = (timestamp) => {
 }
 //根据预警等级切换tag颜色
 const getTagType = (warnType: string) => {
+  if (!warnType) return 'info';
   if (warnType.includes('一级') || warnType.includes('1级') || warnType.includes('严重')) {
     return 'danger'; // 红色
   } else if (warnType.includes('二级') || warnType.includes('2级') || warnType.includes('轻微')) {
