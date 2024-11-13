9f14468a95061e6710a9265888512bfea4c41f9a
Tony <weijialin@inspur.com>
2024-11-13 09:09:52 +0800

    feat: 删除宇视云所有报错提示

--- src/components/EzPlayer/src/EzPlayer.vue
+++ src/components/EzPlayer/src/EzPlayer.vue
@@ -1,23 +1,25 @@
 <!-- 宇视云 播放器 -->
 <script setup lang="ts">
-import { DeviceNvrApi } from '@/api/agriculture/devicenvr/index'
-import Dplayer from 'dplayer'
-import Hls from "hls.js";
+import { DeviceNvrApi } from '@/api/agriculture/devicenvr/index';
+import Dplayer from 'dplayer';
+import Hls from 'hls.js';
 import axios from 'axios';
 import { uniqueId } from 'lodash-es';
-import request from '@/config/axios'
-import { ElMessage } from 'element-plus'
+import request from '@/config/axios';
 
-const liveToken = localStorage.getItem("LIVE_TOKEN");
-if (liveToken === 'error') localStorage.removeItem("LIVE_TOKEN")
+const liveToken = localStorage.getItem('LIVE_TOKEN');
+if (liveToken === 'error') localStorage.removeItem('LIVE_TOKEN');
 
 const getAddressInfoRecordPage = async (params: any) => {
-  return await request.get({ url: `/agriculture/device-nvr/getToken`, params })
-}
+  return await request.get({
+    url: `/agriculture/device-nvr/getToken`,
+    params
+  });
+};
 
-defineOptions({ name: 'EzPlayer' })
+defineOptions({ name: 'EzPlayer' });
 
-const domID = uniqueId()
+const domID = uniqueId();
 
 const props = defineProps({
   modelValue: {
@@ -28,75 +30,99 @@ const props = defineProps({
     type: [String, Number],
     default: () => 1
   }
-})
+});
 
 watch(
   () => [props.modelValue, props.channelNo],
   (_deviceSerial, _channelNo) => {
     if (!_deviceSerial || !_channelNo) return;
-    initPlayer(_deviceSerial, _channelNo)
+    initPlayer(_deviceSerial, _channelNo);
   }
-)
+);
 
 onMounted(() => {
   const _deviceSerial = props.modelValue;
-  const _channelNo = props.channelNo
+  const _channelNo = props.channelNo;
   if (!_deviceSerial || !_channelNo) return;
-  initPlayer(_deviceSerial, _channelNo)
-})
+  initPlayer(_deviceSerial, _channelNo);
+});
 
 const sleep = (delaytime = 1000) => {
-  return new Promise(resolve => setTimeout(resolve, delaytime))
-}
+  return new Promise((resolve) => setTimeout(resolve, delaytime));
+};
 
-const checkAuth = async (deviceSerial, channelNo = 1, leftTimes = 2): Promise<string> => {
+const checkAuth = async (deviceSerial, channelNo = 1, leftTimes = 4): Promise<string> => {
   if (leftTimes <= 0) {
-    ElMessage.error("获取视频流失败，请联系管理员!");
+    // ElMessage.error("获取视频流失败，请联系管理员!");
   }
   if (!deviceSerial || !channelNo || leftTimes <= 0) return deviceSerial;
-  const liveToken = localStorage.getItem("LIVE_TOKEN");
+  const liveToken = localStorage.getItem('LIVE_TOKEN');
   if (liveToken) {
     // 获取视频流
-    const { data: liveDataRes } = await axios.post(
-      "https://ezcloud.uniview.com/openapi/live/video/get",
-      { deviceSerial, channelNo, protocol: 2, quality: 1 },
-      { headers: { Authorization: liveToken } }
-    )
+    const { data: liveDataRes } = await axios
+      .post(
+        'https://ezcloud.uniview.com/openapi/live/video/get',
+        {
+          deviceSerial,
+          channelNo,
+          protocol: 2,
+          quality: 1
+        },
+        {
+          headers: {
+            Authorization: liveToken
+          }
+        }
+      )
+      .catch((e) => {
+        console.error(e);
+      });
     const { code, data: UrlData } = liveDataRes;
-    console.log("🚀 ~ checkAuth ~ UrlData:", UrlData);
-    if (!UrlData) return ElMessage.error(liveDataRes)
+    console.log('🚀 ~ checkAuth ~ UrlData:', UrlData);
+    if (!UrlData) {
+      localStorage.removeItem('LIVE_TOKEN');
+      return await checkAuth(deviceSerial, channelNo, leftTimes);
+    }
     const { status = -1, url } = UrlData;
     if (code === 200) {
       if (status !== 0) {
         await await axios.post(
           'https://ezcloud.uniview.com/openapi/live/video/start',
-          { url }, { headers: { Authorization: liveToken } }
-        )
-        await sleep(3000)
+          { url },
+          {
+            headers: {
+              Authorization: liveToken
+            }
+          }
+        );
+        await sleep(3000);
       }
       return url;
     } else {
-      localStorage.setItem("LIVE_TOKEN", undefined)
-      return ''
+      localStorage.removeItem('LIVE_TOKEN');
+      return '';
     }
   }
 
-  const { list } = await DeviceNvrApi.getDeviceNvrPage({ pageNo: 1, pageSize: 10 }).catch(() => {})
-  let appId = "626194353357848583", secretKey = "ca06cd14935e031bd7a394ee7eca154d";
-  let _deviceSerial_ = ''
+  const { list } = await DeviceNvrApi.getDeviceNvrPage({ pageNo: 1, pageSize: 10 }).catch(() => {});
+  let appId = '626194353357848583',
+    secretKey = 'ca06cd14935e031bd7a394ee7eca154d';
+  let _deviceSerial_ = '';
   if (Array.isArray(list) && list.length > 0) {
     const firstItem = list[0];
     const { deviceSerial: __deviceSerial } = firstItem;
-    _deviceSerial_ = __deviceSerial
+    _deviceSerial_ = __deviceSerial;
   }
-  const data = await getAddressInfoRecordPage({ deviceSerial: _deviceSerial_ })
-  console.log("🚀 ~ checkAuth ~ data:", data)
-  if (data && data !== 'error') localStorage.setItem("LIVE_TOKEN", data)
-  return await checkAuth(deviceSerial, channelNo, leftTimes - 1)
-}
+  const data = await getAddressInfoRecordPage({
+    deviceSerial: _deviceSerial_
+  });
+  console.log('🚀 ~ checkAuth ~ data:', data);
+  if (data && data !== 'error') localStorage.setItem('LIVE_TOKEN', data);
+  return await checkAuth(deviceSerial, channelNo, leftTimes - 1);
+};
 
-let hls:any = null;
-let playerIns:any = null;
+let hls: any = null;
+let playerIns: any = null;
 const initPlayer = async (dtu, channelId = '') => {
   if (!dtu) return;
   const resUrl = await checkAuth(dtu, channelId);
@@ -109,37 +135,38 @@ const initPlayer = async (dtu, channelId = '') => {
       volume: 0,
       video: {
         url: resUrl,
-        type: "customHls",
+        type: 'customHls',
         customType: {
           customHls: (video) => {
             hls.loadSource(video.src);
             hls.attachMedia(video);
-          },
-        },
+          }
+        }
       },
       mutex: false
-    })
-  })
-}
+    });
+  });
+};
 
 onUnmounted(() => {
   if (playerIns) playerIns.destroy();
   if (hls) hls.destroy();
-})
+});
 
 onActivated(() => {
-  initPlayer(props.modelValue, props.channelNo)
-})
+  initPlayer(props.modelValue, props.channelNo);
+});
 onDeactivated(() => {
   if (playerIns) playerIns.destroy();
   if (hls) hls.destroy();
-})
+});
 
-defineExpose({ initPlayer })
+defineExpose({ initPlayer });
 </script>
 <template>
   <!-- 宇视云 -->
-  <div class="w-full h-full" :id="domID"></div>
+  <div class="w-full h-full bg-[#000] text-[#fff] flex justify-center items-center" :id="domID">
+    视频加载中...
+  </div>
 </template>
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
