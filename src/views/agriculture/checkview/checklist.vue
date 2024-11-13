5153af44dc9023c162dc08ab4fdedd724bddcb1a
zhoumingxiu <zhoumingxiu@inspur.com>
2024-11-13 10:55:36 +0800

    fix: 巡检总览，左侧树样式

--- src/views/agriculture/checkview/checklist.vue
+++ src/views/agriculture/checkview/checklist.vue
@@ -1,52 +1,53 @@
 <template>
-  <ContentWrap>
-    <!-- 搜索工作栏 -->
-    <el-form
-      class="-mb-15px"
-      :model="queryParamsCountPageByBaseId"
-      ref="queryFormRef"
-      :inline="true"
-      label-width="68px"
-    >
-      <el-form-item label="时间" prop="createTime">
-        <el-date-picker
-          v-model="queryParamsCountPageByBaseId.createTime"
-          value-format="YYYY-MM-DD"
-          type="date"
-          placeholder="请选择时间"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item>
-        <el-button @click="handleQuery" type="primary">
-          <Icon icon="ep:search" class="mr-5px"/>
-          搜索
-        </el-button>
-        <el-button @click="resetQuery">
-          <Icon icon="ep:refresh" class="mr-5px"/>
-          重置
-        </el-button>
-      </el-form-item>
-    </el-form>
-  </ContentWrap>
-  <ContentWrap>
-    <!--    待修改-->
-    <el-table v-loading="loading" :data="listDevice" :show-overflow-tooltip="true" :stripe="true">
-      <!-- 子表的列表 -->
-      <el-table-column type="expand">
-        <template #default="scope">
-          <el-tabs model-value="checkDetail">
-            <el-tab-pane label="巡检信息" name="checkDetail">
-              <CheckDetailList :check-row="scope.row" :base-id="queryParamsCountPageByBaseId"/>
-            </el-tab-pane>
-          </el-tabs>
-        </template>
-      </el-table-column>
-      <el-table-column label="设备类型" align="center" prop="categoryName" width="450"/>
-      <el-table-column label="巡检设备总数" align="center" prop="sumNum" width="100"/>
-      <el-table-column label="未巡检/个" align="center" prop="unFinishCheckNum"/>
-      <el-table-column label="已巡检/个" align="center" prop="finishCheckNum" />
-<!--      <el-table-column label="操作" align="center">
+  <div ref="containerDom">
+    <ContentWrap>
+      <!-- 搜索工作栏 -->
+      <el-form
+        class="-mb-15px"
+        :model="queryParamsCountPageByBaseId"
+        ref="queryFormRef"
+        :inline="true"
+        label-width="68px"
+      >
+        <el-form-item label="时间" prop="createTime">
+          <el-date-picker
+            v-model="queryParamsCountPageByBaseId.createTime"
+            value-format="YYYY-MM-DD"
+            type="date"
+            placeholder="请选择时间"
+            class="!w-240px"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="handleQuery" type="primary">
+            <Icon icon="ep:search" class="mr-5px" />
+            搜索
+          </el-button>
+          <el-button @click="resetQuery">
+            <Icon icon="ep:refresh" class="mr-5px" />
+            重置
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </ContentWrap>
+    <ContentWrap>
+      <!--    待修改-->
+      <el-table v-loading="loading" :data="listDevice" :show-overflow-tooltip="true" :stripe="true">
+        <!-- 子表的列表 -->
+        <el-table-column type="expand">
+          <template #default="scope">
+            <el-tabs model-value="checkDetail">
+              <el-tab-pane label="巡检信息" name="checkDetail">
+                <CheckDetailList :check-row="scope.row" :base-id="queryParamsCountPageByBaseId" />
+              </el-tab-pane>
+            </el-tabs>
+          </template>
+        </el-table-column>
+        <el-table-column label="设备类型" align="center" prop="categoryName" width="450" />
+        <el-table-column label="巡检设备总数" align="center" prop="sumNum" width="100" />
+        <el-table-column label="未巡检/个" align="center" prop="unFinishCheckNum" />
+        <el-table-column label="已巡检/个" align="center" prop="finishCheckNum" />
+        <!--      <el-table-column label="操作" align="center">
         <template #default="scope">
           <el-button
             link
@@ -57,75 +58,83 @@
           </el-button>
         </template>
       </el-table-column>-->
-    </el-table>
-    <!-- 分页 -->
-    <Pagination
-      v-model:limit="queryParamsCountPageByBaseId.pageSize"
-      v-model:page="queryParamsCountPageByBaseId.pageNo"
-      :total="totalDevice"
-      @pagination="getDeviceCountList"
-    />
-  </ContentWrap>
+      </el-table>
+      <!-- 分页 -->
+      <Pagination
+        v-model:limit="queryParamsCountPageByBaseId.pageSize"
+        v-model:page="queryParamsCountPageByBaseId.pageNo"
+        :total="totalDevice"
+        @pagination="getDeviceCountList"
+      />
+    </ContentWrap>
+  </div>
 </template>
-<script setup lang="ts">/** 巡检记录 列表 */
-import CheckDetailList from "@/views/agriculture/checkview/components/checkDetailList.vue";
+<script setup lang="ts">
+/** 巡检记录 列表 */
+import CheckDetailList from '@/views/agriculture/checkview/components/checkDetailList.vue';
 
-defineOptions({name: 'CheckList'})
+defineOptions({ name: 'CheckList' });
 
-import {ParkInfoVO} from "@/api/agriculture/parkinfo";
-import {
-  CheckLogsApi,
-} from "@/api/agriculture/checklogs";
+import { ParkInfoVO } from '@/api/agriculture/parkinfo';
+import { CheckLogsApi } from '@/api/agriculture/checklogs';
 // 设备统计信息列表的数据
-const listDevice = ref<ParkInfoVO[]>([])
+const listDevice = ref<ParkInfoVO[]>([]);
 // 设备统计信息列表的总页数
-const totalDevice = ref(0)
-const loading = ref(false) // 列表的加载中
+const totalDevice = ref(0);
+const loading = ref(false); // 列表的加载中
+const containerDom = ref();
+const emit = defineEmits(['clearTree', 'heightChange']);
 
 const queryParamsCountPageByBaseId = reactive({
   pageNo: 1,
   pageSize: 10,
   baseId: '',
-  createTime: '',
-})
+  createTime: ''
+});
 
 /** 查询设备分类统计列表 */
 const getDeviceCountList = async () => {
-  loading.value = true
+  loading.value = true;
   try {
-    const data = await CheckLogsApi.getCountPageByBaseId(queryParamsCountPageByBaseId)
-    listDevice.value = data.list
-    totalDevice.value = data.total
+    const data = await CheckLogsApi.getCountPageByBaseId(queryParamsCountPageByBaseId);
+    listDevice.value = data.list;
+    totalDevice.value = data.total;
   } finally {
-    loading.value = false
+    loading.value = false;
   }
-}
+  emit('heightChange', containerDom.value.clientHeight);
+};
 /** 搜索按钮操作 */
 const handleQuery = () => {
-  queryParamsCountPageByBaseId.pageNo = 1
-  getDeviceCountList()
-}
+  queryParamsCountPageByBaseId.pageNo = 1;
+  getDeviceCountList();
+};
 /** 重置按钮操作 */
 const resetQuery = () => {
-  queryParamsCountPageByBaseId.pageNo = 1
-  queryParamsCountPageByBaseId.pageSize = 10
-  queryParamsCountPageByBaseId.createTime = formatCurrentDate()
-  handleQuery()
-}
+  queryParamsCountPageByBaseId.pageNo = 1;
+  queryParamsCountPageByBaseId.pageSize = 10;
+  queryParamsCountPageByBaseId.createTime = formatCurrentDate();
+  emit('clearTree');
+  handleQuery();
+};
 
 /**获取当前时间 */
 function formatCurrentDate() {
   const now = new Date();
-  return now.getFullYear() + '-' +
-    (now.getMonth() + 1).toString().padStart(2, '0') + '-' +
-    now.getDate().toString().padStart(2, '0');
+  return (
+    now.getFullYear() +
+    '-' +
+    (now.getMonth() + 1).toString().padStart(2, '0') +
+    '-' +
+    now.getDate().toString().padStart(2, '0')
+  );
 }
 
 /** 初始化 **/
 onMounted(() => {
   //默认赋值查询参数
-  resetQuery()
-})
+  resetQuery();
+});
 
 // 定义属性
 const props = defineProps({
@@ -133,16 +142,17 @@ const props = defineProps({
     type: Object,
     default: () => ({})
   }
-})
+});
 
 // 监听父组件category变化
-watch(() => props.currCategory,
+watch(
+  () => props.currCategory,
   () => {
     if (props.currCategory) {
-      queryParamsCountPageByBaseId.baseId = props.currCategory.id
+      queryParamsCountPageByBaseId.baseId = props.currCategory.id;
     }
-    handleQuery()
-  })
+    handleQuery();
+  }
+);
 </script>
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
--- src/views/agriculture/checkview/index.vue
+++ src/views/agriculture/checkview/index.vue
@@ -1,121 +1,122 @@
 <script setup lang="ts">
-import {ElTree} from "element-plus";
-import {ParkInfoApi, ParkInfoVO} from "@/api/agriculture/parkinfo";
-import CheckList from "@/views/agriculture/checkview/checklist.vue";
+import { ElTree } from 'element-plus';
+import { ParkInfoApi, ParkInfoVO } from '@/api/agriculture/parkinfo';
+import CheckList from '@/views/agriculture/checkview/checklist.vue';
 
 /** 设备信息 列表 */
-defineOptions({name: 'CheckView'})
+defineOptions({ name: 'CheckView' });
 
-const loading = ref(true) // 列表的加载中
+const loading = ref(true); // 列表的加载中
 
 interface Tree {
-  [key: string]: any
+  [key: string]: any;
 }
 
-const filterText = ref('')
-const treeRef = ref<InstanceType<typeof ElTree>>()
+const filterText = ref('');
+const treeRef = ref<InstanceType<typeof ElTree>>();
 const defaultProps = {
-  children:{
-    disabled:true
+  children: {
+    disabled: true
   },
   label: 'name',
-  value: 'id',
-}
+  value: 'id'
+};
+
+//定义是否展开
+const isCollapse = ref(true);
 
 watch(filterText, (val) => {
-  treeRef.value!.filter(val)
-})
+  treeRef.value!.filter(val);
+});
+
+watch(isCollapse, (val) => {
+  if (!val) {
+    outputFous.value = '';
+    filterText2.value = '';
+  }
+});
 
 /** 搜索节点过滤 */
 const filterNode = (value: string, data: Tree) => {
-  if (!value) return true
-  return data.name.includes(value)
-}
+  if (!value) return true;
+  return data.name.includes(value);
+};
 
-const categoryTree = ref<ParkInfoVO[]>([]) // 列表的数据
+//清空树节点
+const clearTree = () => {
+  clearCategory();
+};
+
+const categoryTree = ref<ParkInfoVO[]>([]); // 列表的数据
 /** 查询基地地块列表 */
 const getCategoryList = async () => {
-  loading.value = true
+  loading.value = true;
   try {
-    const queryParams = null
-    const data = await ParkInfoApi.getParkTree(queryParams)
-    categoryTree.value = data
+    const queryParams = null;
+    const data = await ParkInfoApi.getParkTree(queryParams);
+    categoryTree.value = data;
     nextTick(() => {
       if (treeRef.value) {
         treeRef.value.setCurrentKey(categoryTree.value[0].id);
       }
     });
   } finally {
-    loading.value = false
+    loading.value = false;
   }
-}
+};
 
 onMounted(() => {
   getCategoryList();
-})
+});
 
-let currCategory = ref({})
+let currCategory = ref({});
 const handleCurrentCategoryChange = (currNodeData) => {
-  console.log(currNodeData)
-  currCategory.value = currNodeData
-}
+  console.log(currNodeData);
+  currCategory.value = currNodeData;
+};
 // 取消选择
 const clearCategory = () => {
-  treeRef.value.setCurrentKey()
-}
-//定义是否展开
-let isCollapse=ref(true)
+  treeRef.value.setCurrentKey();
+};
 
+const resetTreeSelections = () => {
+  clearCategory(); // 清空选中的节点
+};
+
+//自动聚焦
+const inputFous = ref();
+const outputFous = ref('');
+const filterText2 = ref('');
+const inputFoucs = () => {};
+
+// 根据右侧高度设置左侧菜单的高度
+const treeHeight = ref(1);
+const setLeftHeight = (height: number) => {
+  treeHeight.value = height - 80;
+};
 </script>
 
 <template>
-  <el-row :gutter="24" v-loading="loading">
+  <div class="w-full flex relative">
+    <!-- 左侧 -->
     <div
-      v-if="isCollapse"
-      @click="isCollapse = false"
-      class="
-        flex items-center space-x-1
-        text-white relative top-[-.5rem]
-        box-border p-3 py-1 text-[.9rem]
-        shadow-md bg-#009688
-        transition-all cursor-pointer
-      "
-      style="border-radius: 0 1000px 1000px 0;"
+      :class="`
+        w-[240px] pl-[20px] bg-white mb-[15px] border border-solid border-[#e4e7ed]
+        ${isCollapse ? 'slide-from-right-to-left' : 'slide-from-left-to-right'}
+      `"
     >
-      <div>展开</div>
-      <el-icon><DArrowRight /></el-icon>
-    </div>
-    <el-col :span="isCollapse ? 0 : 4">
-      <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo relative"
-        :collapse="isCollapse"
-      >
-        <ContentWrap>
-          <div
-            class="flex items-center box-border p-3 justify-between rounded-2 mb-2"
-            style="border: 1px solid #e1e1e1;"
-          >
-            <el-input
-              v-model="filterText"
-              placeholder="输入关键字进行过滤"
-              clearable
-            />
-            <div
-              class="
-                flex items-center space-x-1
-                rounded-2 p-1 w-[3.5rem] text-[.7rem]
-                px-2 shadow-sm cursor-pointer
-              "
-              style="border: 1px solid #e1e1e1;"
-              @click="isCollapse = true"
-            >
-              <el-icon><DArrowLeft /></el-icon>
-              <div>收起</div>
-            </div>
-
-          </div>
-        <ContentWrap style="height: 62vh; overflow: auto;">
+      <el-menu default-active="2" class="el-menu-vertical-demo relative" :collapse="isCollapse">
+        <div class="w-[220px] h-80px flex justify-start items-center">
+          <el-input
+            class="!w-[220px] h-30px rounded"
+            v-model="filterText"
+            @focus="inputFoucs"
+            ref="inputFous"
+            placeholder="搜索基地"
+            clearable
+          />
+        </div>
+        <el-scrollbar :height="treeHeight">
           <el-tree
             ref="treeRef"
             style="max-width: 600px"
@@ -129,23 +130,120 @@ let isCollapse=ref(true)
             :expand-on-click-node="false"
             @current-change="handleCurrentCategoryChange"
           />
-        </ContentWrap>
-
-      </ContentWrap>
+        </el-scrollbar>
       </el-menu>
-    </el-col>
+    </div>
 
-    <el-col :span="isCollapse?24:20">
-      <ContentWrap style="height: 78vh; overflow: auto;">
+    <!-- 右侧 -->
+    <div
+      :class="`
+        grid relative grow w-[calc(100%-260px)]
+        ${isCollapse ? 'content-grow' : 'content-shrink'}
+      `"
+    >
+      <!-- 展开收起侧边面板按钮 -->
+      <div
+        @click="isCollapse = !isCollapse"
+        :class="`
+          h-[25px] w-[25px] rounded-full shadow-md bg-white
+          cursor-pointer flex items-center justify-center text-[14px]
+          absolute top-[30px] left-[0px] translate-x-[-12.5px]
+        `"
+        :style="{
+          color: 'var(--el-color-primary)',
+          border: '1px solid var(--el-color-primary)'
+        }"
+      >
+        <el-icon v-show="!isCollapse"><ArrowLeftBold /></el-icon>
+        <el-icon v-show="isCollapse"><ArrowRightBold /></el-icon>
+      </div>
+
+      <!-- 内容 需要想办法获取元素高度来设置左侧高度 -->
+      <ContentWrap>
         <check-list
+          :isCollapse="isCollapse"
           :currCategory="currCategory"
           @clear-category="clearCategory"
+          @reset="resetTreeSelections"
+          @clear-tree="clearTree"
+          @height-change="setLeftHeight"
         />
       </ContentWrap>
-    </el-col>
-  </el-row>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.searchTop {
+  :deep(.el-input__wrapper) {
+    box-shadow: 0 0 0 0px var(--el-input-border-color, var(--el-border-color)) inset;
+    cursor: default;
+    .el-input__inner {
+      cursor: default !important;
+    }
+  }
+}
 
+// 选中当前node的样式
+:deep(.el-tree--highlight-current .el-tree-node.is-current > .el-tree-node__content) {
+  // background: pink !important;
+  background-image: url('../../../../assets/imgs/treeActive.png') !important;
+  background-size: 100% 100% !important;
+}
+
+@keyframes slide-from-left-to-right {
+  from {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+
+.slide-from-left-to-right {
+  animation: slide-from-left-to-right 0.1s ease-out forwards;
+}
+
+@keyframes slide-from-right-to-left {
+  from {
+    transform: translateX(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+}
+
+.slide-from-right-to-left {
+  animation: slide-from-right-to-left 0.1s ease-in forwards;
+}
+
+@keyframes content-grow {
+  from {
+    margin-left: 0;
+  }
+  to {
+    margin-left: -260px;
+  }
+}
+
+.content-grow {
+  animation: content-grow 0.1s ease-out forwards;
+}
+
+@keyframes content-shrink {
+  from {
+    margin-left: -260px;
+  }
+  to {
+    margin-left: 0;
+  }
+}
+
+.content-shrink {
+  animation: content-shrink 0.1s ease-out forwards;
+}
 </style>
