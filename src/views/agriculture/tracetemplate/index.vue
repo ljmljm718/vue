98fc5750f9ce4e6c068dcd51fe1a15c83d100b59
zhoumingxiu <zhoumingxiu@inspur.com>
2024-11-14 13:46:51 +0800

    fix: 溯源模板管理查询条件

--- src/views/agriculture/tracetemplate/index.vue
+++ src/views/agriculture/tracetemplate/index.vue
@@ -1,7 +1,7 @@
 <template>
   <ContentWrap>
     <!-- 搜索工作栏 -->
-    <el-form
+    <custom-form
       class="-mb-15px"
       :model="queryParams"
       ref="queryFormRef"
@@ -43,15 +43,15 @@
       </el-form-item>
       <el-form-item>
         <el-button @click="handleQuery" type="primary">
-          <Icon icon="ep:search" class="mr-5px"/>
+          <Icon icon="ep:search" class="mr-5px" />
           搜索
         </el-button>
         <el-button @click="resetQuery">
-          <Icon icon="ep:refresh" class="mr-5px"/>
+          <Icon icon="ep:refresh" class="mr-5px" />
           重置
         </el-button>
       </el-form-item>
-    </el-form>
+    </custom-form>
   </ContentWrap>
 
   <!-- 列表 -->
@@ -64,7 +64,7 @@
           @click="openForm('create')"
           v-hasPermi="['agriculture:trace-template:create']"
         >
-          <Icon icon="ep:plus" class="mr-5px"/>
+          <Icon icon="ep:plus" class="mr-5px" />
           新增
         </el-button>
         <el-button
@@ -74,7 +74,7 @@
           :loading="exportLoading"
           v-hasPermi="['agriculture:trace-template:export']"
         >
-          <Icon icon="ep:download" class="mr-5px"/>
+          <Icon icon="ep:download" class="mr-5px" />
           导出
         </el-button>
       </div>
@@ -99,10 +99,10 @@
     </div>
     <div v-if="showType === 'list'">
       <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
-        <el-table-column label="模板名称" align="center" prop="templateName"/>
-        <el-table-column label="生产商" align="center" prop="companyName"/>
-        <el-table-column label="产地" align="center" prop="origin"/>
-        <el-table-column label="适用品牌" align="center" prop="brandName"/>
+        <el-table-column label="模板名称" align="center" prop="templateName" />
+        <el-table-column label="生产商" align="center" prop="companyName" />
+        <el-table-column label="产地" align="center" prop="origin" />
+        <el-table-column label="适用品牌" align="center" prop="brandName" />
         <el-table-column label="头部宣传图" align="center" prop="headerImg">
           <template #default="{ row }">
             <el-image
@@ -135,11 +135,7 @@
         />
         <el-table-column label="操作" align="center" width="150" fixed="right">
           <template #default="scope">
-            <el-button
-              link
-              type="primary"
-              @click="openForm('detail', scope.row.id)"
-            >
+            <el-button link type="primary" @click="openForm('detail', scope.row.id)">
               详情
             </el-button>
             <el-button
@@ -163,12 +159,14 @@
       </el-table>
     </div>
     <div v-else class="flex justify-center">
-      <div class="grid gap-3 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 container">
+      <div
+        class="grid gap-3 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 container"
+      >
         <div
           v-for="item in list"
           :key="item.id"
           class="shadow-md flex flex-col items-center"
-          style="border: 1px solid #e1e1e1;"
+          style="border: 1px solid #e1e1e1"
         >
           <img :src="item.headerImg" class="w-full aspect-1.3 object-contain bg-[#f1f1f1]" />
           <div class="w-full box-border p-3 font-bold">{{ item.templateName }}</div>
@@ -202,28 +200,28 @@
   </ContentWrap>
 
   <!-- 表单弹窗：添加/修改 -->
-  <TraceTemplateForm ref="formRef" @success="getList"/>
+  <TraceTemplateForm ref="formRef" @success="getList" />
 </template>
 
 <script setup lang="ts">
-import { dateFormatter } from '@/utils/formatTime'
-import download from '@/utils/download'
-import { TraceTemplateApi, TraceTemplateVO } from '@/api/agriculture/tracetemplate'
-import TraceTemplateForm from './TraceTemplateForm.vue'
-import { ProducerEntryApi, ProducerEntryVO } from "@/api/agriculture/producerentry";
+import { dateFormatter } from '@/utils/formatTime';
+import download from '@/utils/download';
+import { TraceTemplateApi, TraceTemplateVO } from '@/api/agriculture/tracetemplate';
+import TraceTemplateForm from './TraceTemplateForm.vue';
+import { ProducerEntryApi, ProducerEntryVO } from '@/api/agriculture/producerentry';
 // import QRCode from 'qrcode'
 
 /** 溯源模板 列表 */
-defineOptions({name: 'TraceTemplate'})
+defineOptions({ name: 'TraceTemplate' });
 
-const message = useMessage() // 消息弹窗
-const {t} = useI18n() // 国际化
+const message = useMessage(); // 消息弹窗
+const { t } = useI18n(); // 国际化
 
-const showType = ref<string>('card')
-const loading = ref(true) // 列表的加载中
-const list = ref<TraceTemplateVO[]>([]) // 列表的数据
-const producerEntryList = ref<ProducerEntryVO[]>([]) // 生产商的数据
-const total = ref(0) // 列表的总页数
+const showType = ref<string>('card');
+const loading = ref(true); // 列表的加载中
+const list = ref<TraceTemplateVO[]>([]); // 列表的数据
+const producerEntryList = ref<ProducerEntryVO[]>([]); // 生产商的数据
+const total = ref(0); // 列表的总页数
 const queryParams = reactive({
   pageNo: 1,
   pageSize: 10,
@@ -236,82 +234,81 @@ const queryParams = reactive({
   remark: undefined,
   remark1: undefined,
   brandName: undefined,
-  createTime: [],
-})
-const queryFormRef = ref() // 搜索的表单
-const exportLoading = ref(false) // 导出的加载中
+  createTime: []
+});
+const queryFormRef = ref(); // 搜索的表单
+const exportLoading = ref(false); // 导出的加载中
 
 /** 查询生产商列表 */
 const getProducerEntryList = async () => {
-  const data = await ProducerEntryApi.getProducerEntryAll({approvalStatus: 1})
-  producerEntryList.value = data
-}
+  const data = await ProducerEntryApi.getProducerEntryAll({ approvalStatus: 1 });
+  producerEntryList.value = data;
+};
 
 /** 查询列表 */
 const getList = async () => {
-  loading.value = true
+  loading.value = true;
   try {
-    const data = await TraceTemplateApi.getTraceTemplatePage(queryParams)
-    console.log("🚀 ~ getList ~ data:", data.list)
-    list.value = data.list
-    total.value = data.total
+    const data = await TraceTemplateApi.getTraceTemplatePage(queryParams);
+    console.log('🚀 ~ getList ~ data:', data.list);
+    list.value = data.list;
+    total.value = data.total;
   } finally {
-    loading.value = false
+    loading.value = false;
   }
-}
+};
 
 /** 搜索按钮操作 */
 const handleQuery = () => {
-  queryParams.pageNo = 1
-  getList()
-}
+  queryParams.pageNo = 1;
+  getList();
+};
 
 /** 重置按钮操作 */
 const resetQuery = () => {
-  queryFormRef.value.resetFields()
-  handleQuery()
-}
+  queryFormRef.value.resetFields();
+  handleQuery();
+};
 
 /** 添加/修改操作 */
-const formRef = ref()
+const formRef = ref();
 const openForm = (type: string, id?: number) => {
-  formRef.value.open(type, id)
-}
+  formRef.value.open(type, id);
+};
 
 /** 删除按钮操作 */
 const handleDelete = async (id: number) => {
   try {
     // 删除的二次确认
-    await message.delConfirm()
+    await message.delConfirm();
     // 发起删除
-    await TraceTemplateApi.deleteTraceTemplate(id)
-    message.success(t('common.delSuccess'))
+    await TraceTemplateApi.deleteTraceTemplate(id);
+    message.success(t('common.delSuccess'));
     // 刷新列表
-    await getList()
-  } catch {
-  }
-}
+    await getList();
+  } catch {}
+};
 
 /** 导出按钮操作 */
 const handleExport = async () => {
   try {
     // 导出的二次确认
-    await message.exportConfirm()
+    await message.exportConfirm();
     // 发起导出
-    exportLoading.value = true
-    const data = await TraceTemplateApi.exportTraceTemplate(queryParams)
-    download.excel(data, '溯源模板.xls')
+    exportLoading.value = true;
+    const data = await TraceTemplateApi.exportTraceTemplate(queryParams);
+    download.excel(data, '溯源模板.xls');
   } catch {
   } finally {
-    exportLoading.value = false
+    exportLoading.value = false;
   }
-}
+};
 
 /** 初始化 **/
 onMounted(() => {
-  getProducerEntryList()
-  getList()
-})
+  getProducerEntryList();
+  getList();
+});
 </script>
 <style scoped lang="scss">
 .tab-btn,
