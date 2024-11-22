4a5bfc747e3ceca23a1ad30df6350cea030a2be0
changxu02 <changxu02@inspur.com>
2024-11-22 16:48:28 +0800

    feat:智能物联-设备管理-设备状态 表单界面6.1列表demo

---
+++ src/config/colorTheme/colorConfig.ts
@@ -0,0 +1,11 @@
+/**
+ * 存储不同主题的配色方案
+ * key    是主题色的HEX值
+ * value  是个对象 包括
+ *   primaryBtnActiveBg  主按钮激活时的背景色 以及 链接按钮激活时的文字颜色
+ */
+export const colorOpt = {
+  '#009688': {
+    primaryBtnActiveBg: '#00877A'
+  }
+};
