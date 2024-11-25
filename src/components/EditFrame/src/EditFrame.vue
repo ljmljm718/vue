2b7eaafd113b8a2bd8e08579b0c18e1ad7be23a9
changxu02 <changxu02@inspur.com>
2024-11-25 17:26:10 +0800

    feat:智能物联-设备管理-设备信息-设备信息新增 6.2.1单表录入demo

--- src/components/EditFrame/src/EditFrame.vue
+++ src/components/EditFrame/src/EditFrame.vue
@@ -1,17 +1,42 @@
 <script lang="ts" setup>
-defineOptions({ name: 'EditFrame' })
+defineOptions({ name: 'EditFrame' });
 </script>
+
 <template>
-    <ContentWrap>
-        <div class="flex justify-between w-full">
-            <slot name="header">
-                <div></div>
-            </slot>
-        </div>
-    </ContentWrap>
-    <ContentWrap>
-        <el-scrollbar height="700px">
-            <slot name="content"></slot>
-        </el-scrollbar>
-    </ContentWrap>
+  <div class="bg-[#ECEFF7] rounded-[6px] w-full main-height">
+    <div class="flex justify-between w-full p-[16px] box-border">
+      <slot name="header">
+        <div></div>
+      </slot>
+    </div>
+    <el-scrollbar class="w-full p-[16px] box-border bg-white rounded-[6px] content-height">
+      <slot name="content"></slot>
+    </el-scrollbar>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+/*
+  topMenuHeight     顶部菜单高度
+  contentPadding    内容外边距
+  modulePadding     模块内边距
+  min-btn-height    el-button的最小高度
+  max-btn-height    el-button的最大高度
+*/
+$topMenuHeight: 85;
+$contentPadding: 8;
+$modulePadding: 16;
+$min-btn-height: 24;
+$max-btn-height: 40;
+
+// 内容高度
+.main-height {
+  height: calc(100vh - #{$topMenuHeight + 2 * $contentPadding + 1}px);
+}
+
+// 新增表单的高度
+.content-height {
+  max-height: calc(100% - #{2 * $modulePadding + $min-btn-height}px);
+  min-height: calc(100% - #{2 * $modulePadding + $max-btn-height}px);
+}
+</style>
