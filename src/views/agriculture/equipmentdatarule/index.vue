6a294caf1696be8215b4fde48cb2105c714613a1
zhoumingxiu <zhoumingxiu@inspur.com>
2024-11-25 14:58:06 +0800

    fix: 主题订阅，数据同步规则修改

--- src/views/agriculture/equipmentdatarule/index.vue
+++ src/views/agriculture/equipmentdatarule/index.vue
@@ -1,53 +1,32 @@
 <template>
-  <ContentWrap>
-    <!-- 搜索工作栏 -->
-    <custom-form :model="queryParams" ref="queryFormRef" :inline="true">
-      <el-form-item label="终端" prop="dtuId">
-        <el-input
-          v-model="queryParams.dtuId"
-          placeholder="请输入终端"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="通道" prop="channelId">
-        <el-input
-          v-model="queryParams.channelId"
-          placeholder="请输入通道"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item label="监测类型" prop="monitoringType">
-        <el-input
-          v-model="queryParams.monitoringType"
-          placeholder="请输入监测类型"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <!--      <el-form-item label="规则类型" prop="ruleType">
-                <el-input
-                  v-model="queryParams.ruleType"
-                  placeholder="请输入规则类型"
-                  clearable
-                  @keyup.enter="handleQuery"
-                  class="!w-240px"
-                />
-              </el-form-item> -->
-      <el-form-item label="设备名称" prop="deviceName">
-        <el-input
-          v-model="queryParams.deviceName"
-          placeholder="请输入设备名称"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <el-form-item>
+  <el-scrollbar
+    class="w-full bg-white rounded-[6px] text-[#666] text-[14px] p-[16px] box-border"
+    :style="{ height: 'calc(100vh - ' + (topMenuHeight + 2 * contentPadding) + 'px)' }"
+  >
+    <div class="w-full flex justify-between items-center">
+      <div class="flex items-center">
+        <!-- 一级标题名字 -->
+        <h1 class="m-0 text-[#333] font-bold text-[18px]">数据同步规则</h1>
+
+        <Icon icon="ep:question-filled" :size="14" class="ml-[8px] cursor-pointer text-[#F08000]" />
+        <div class="w-[1px] h-[32px] mx-[16px] bg-[#ebebeb]"></div>
+
+        <!--
+          一级标题旁边的按钮
+          主按钮 type="primary" 次按钮不设置 type 属性 不设置 plain 属性
+        -->
+        <el-button
+          type="primary"
+          @click="openForm('create')"
+          v-hasPermi="['agriculture:equipment-data-rule:create']"
+        >
+          <Icon icon="ep:plus" class="mr-5px" />
+          新增
+        </el-button>
+      </div>
+
+      <div class="flex items-center">
+        <!-- 一级标题这行右侧的按钮写在下面 修改点击事件函数 -->
         <el-button @click="handleQuery" type="primary">
           <Icon icon="ep:search" class="mr-5px" />
           搜索
@@ -56,74 +35,129 @@
           <Icon icon="ep:refresh" class="mr-5px" />
           重置
         </el-button>
-      </el-form-item>
-    </custom-form>
-  </ContentWrap>
+        <button
+          class="circle-arrow-up ml-[16px]"
+          :class="showSearch ? 'rotate180andthemeBg' : 'rotate180andwhiteBg'"
+          @click="handleClickShowSearch"
+        >
+          <Icon :size="14" icon="ep:arrow-up" />
+        </button>
+      </div>
+    </div>
 
-  <!-- 列表 -->
-  <ContentWrap>
-    <el-form-item>
-      <el-button
-        type="primary"
-        plain
-        @click="openForm('create')"
-        v-hasPermi="['agriculture:equipment-data-rule:create']"
+    <!-- 搜索栏 注意 :model 和 ref 的名称 -->
+    <el-form
+      :model="queryParams"
+      ref="queryFormRef"
+      class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-[8px] mt-[8px] w-full form"
+      :class="showSearch ? 'opacity-100' : 'h-0 opacity-0'"
+      label-width="95px"
+      :inline="true"
+    >
+      <!--
+        原来的表单里的内容复制过来 不要操作按钮
+        去掉每项的固定宽度 比如 !w-240px !w-220px
+        如果 输入框内 有按钮 则只保留图标 不要文字
+      -->
+      <el-form-item label="终端" prop="dtuId">
+        <el-input
+          v-model="queryParams.dtuId"
+          placeholder="请输入终端"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="通道" prop="channelId">
+        <el-input
+          v-model="queryParams.channelId"
+          placeholder="请输入通道"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="监测类型" prop="monitoringType">
+        <el-input
+          v-model="queryParams.monitoringType"
+          placeholder="请输入监测类型"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="设备名称" prop="deviceName">
+        <el-input
+          v-model="queryParams.deviceName"
+          placeholder="请输入设备名称"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+    </el-form>
+
+    <div class="w-full mt-[16px]">
+      <!-- 原来的表格复制过来 操作按钮按照 el-table操作按钮.md 里的例子 -->
+      <el-table
+        v-loading="loading"
+        :data="list"
+        :stripe="true"
+        :show-overflow-tooltip="true"
+        border
       >
-        <Icon icon="ep:plus" class="mr-5px" />
-        新增
-      </el-button>
-    </el-form-item>
-    <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true" border>
-      <el-table-column label="设备名称" align="center" prop="deviceName" width="180" />
-      <el-table-column label="终端" align="center" prop="dtuId" width="150" />
-      <el-table-column label="通道" align="center" prop="channelId" width="150" />
-      <el-table-column label="参数" align="center" prop="param" />
-      <el-table-column label="监测类型" align="center" prop="monitoringType" width="100" />
-      <el-table-column label="单位" align="center" prop="ruleUnit" />
-      <el-table-column label="规则类型" align="center" prop="ruleType" width="150">
-        <template #default="scope">
-          <dict-tag :type="DICT_TYPE.DATA_SYNC_RULE_TYPE" :value="scope.row.ruleType" />
-        </template>
-      </el-table-column>
-      <el-table-column label="系数" align="center" prop="rule" />
-      <el-table-column label="字典" align="center" prop="dictionary" />
-      <el-table-column label="备注" align="center" prop="remark" />
-      <el-table-column
-        label="创建时间"
-        align="center"
-        prop="createTime"
-        :formatter="dateFormatter"
-        width="180px"
-      />
-      <el-table-column label="操作" align="center" width="150" fixed="right">
-        <template #default="scope">
-          <el-button
-            link
-            type="primary"
-            @click="openForm('update', scope.row.id)"
-            v-hasPermi="['agriculture:equipment-data-rule:update']"
-          >
-            编辑
-          </el-button>
-          <el-button
-            link
-            type="danger"
-            @click="handleDelete(scope.row.id)"
-            v-hasPermi="['agriculture:equipment-data-rule:delete']"
-          >
-            删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <!-- 分页 -->
+        <el-table-column label="设备名称" align="center" prop="deviceName" width="180" />
+        <el-table-column label="终端" align="center" prop="dtuId" width="150" />
+        <el-table-column label="通道" align="center" prop="channelId" width="150" />
+        <el-table-column label="参数" align="center" prop="param" />
+        <el-table-column label="监测类型" align="center" prop="monitoringType" width="100" />
+        <el-table-column label="单位" align="center" prop="ruleUnit" />
+        <el-table-column label="规则类型" align="center" prop="ruleType" width="150">
+          <template #default="scope">
+            <dict-tag :type="DICT_TYPE.DATA_SYNC_RULE_TYPE" :value="scope.row.ruleType" />
+          </template>
+        </el-table-column>
+        <el-table-column label="系数" align="center" prop="rule" />
+        <el-table-column label="字典" align="center" prop="dictionary" />
+        <el-table-column label="备注" align="center" prop="remark" />
+        <el-table-column
+          label="创建时间"
+          align="center"
+          prop="createTime"
+          :formatter="dateFormatter"
+          width="180px"
+        />
+        <el-table-column label="操作" align="center" fixed="right" min-width="154px">
+          <template #default="scope">
+            <div class="flex items-center justify-center">
+              <el-button
+                link
+                type="primary"
+                @click="openForm('update', scope.row.id)"
+                v-hasPermi="['agriculture:equipment-data-rule:update']"
+              >
+                编辑
+              </el-button>
+              <div class="mx-[12px] w-[1px] h-[24px] bg-[#e6e6e6]"></div>
+              <el-button
+                link
+                type="danger"
+                @click="handleDelete(scope.row.id)"
+                v-hasPermi="['agriculture:equipment-data-rule:delete']"
+              >
+                删除
+              </el-button>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <!-- 页码组件 注意绑定的值和事件函数 -->
     <Pagination
+      style="margin-bottom: 0; margin-top: 8px"
       :total="total"
       v-model:page="queryParams.pageNo"
       v-model:limit="queryParams.pageSize"
       @pagination="getList"
     />
-  </ContentWrap>
+  </el-scrollbar>
 
   <!-- 表单弹窗：添加/修改 -->
   <EquipmentDataRuleForm ref="formRef" @success="getList" />
@@ -229,4 +263,92 @@ onMounted(() => {
   if (route.query.dtuId) queryParams.dtuId = route.query.dtuId as string;
   getList();
 });
+
+/**
+ * topMenuHeight      顶部菜单和标签页高度
+ * contentPadding     页面内容外边距
+ */
+const topMenuHeight = 85;
+const contentPadding = 8;
+
+// 展开或收起搜索栏
+const showSearch = ref(false);
+const handleClickShowSearch = () => {
+  showSearch.value = !showSearch.value;
+};
 </script>
+<style lang="scss" scoped>
+// 鼠标移在按钮上时显示主题色边框
+:deep(.el-button:hover) {
+  border-color: var(--el-color-primary);
+}
+
+// 去掉表单的边距
+:deep(.form > *) {
+  margin: 0;
+}
+
+// 调整表单标签和输入框之间的距离
+:deep(.form .el-form-item__label) {
+  padding: 0 4px 0 0;
+}
+
+// 收起
+.circle-arrow-up {
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  border: 1px solid #ebebeb;
+  color: #333;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    color: white;
+    border-width: 0;
+    background-color: var(--el-color-primary);
+  }
+}
+
+// 向上箭头展开收起的动画
+@keyframes rotate180andwhiteBg {
+  from {
+    transform: rotate(0deg);
+    color: #333;
+    background-color: white;
+  }
+  to {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+}
+
+.rotate180andwhiteBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andwhiteBg;
+  animation-fill-mode: forwards;
+}
+
+@keyframes rotate180andthemeBg {
+  from {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+  to {
+    transform: rotate(360deg);
+    color: #333;
+    background-color: white;
+  }
+}
+
+.rotate180andthemeBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andthemeBg;
+  animation-fill-mode: forwards;
+}
+</style>
--- src/views/agriculture/topicdata/index.vue
+++ src/views/agriculture/topicdata/index.vue
@@ -1,27 +1,115 @@
 <template>
-  <ContentWrap>
-    <!-- 搜索工作栏 -->
-    <custom-form :model="queryParams" ref="queryFormRef" :inline="true">
+  <el-scrollbar
+    class="w-full bg-white rounded-[6px] text-[#666] text-[14px] p-[16px] box-border"
+    :style="{ height: 'calc(100vh - ' + (topMenuHeight + 2 * contentPadding) + 'px)' }"
+  >
+    <div class="w-full flex justify-between items-center">
+      <div class="flex items-center">
+        <!-- 一级标题名字 -->
+        <h1 class="m-0 text-[#333] font-bold text-[18px]">主题订阅</h1>
+
+        <Icon icon="ep:question-filled" :size="14" class="ml-[8px] cursor-pointer text-[#F08000]" />
+        <div class="w-[1px] h-[32px] mx-[16px] bg-[#ebebeb]"></div>
+
+        <!--
+          一级标题旁边的按钮
+          主按钮 type="primary" 次按钮不设置 type 属性 不设置 plain 属性
+        -->
+        <el-button
+          type="primary"
+          @click="openForm('create')"
+          v-hasPermi="['agriculture:topic-data:create']"
+        >
+          <Icon icon="ep:plus" class="mr-5px" />
+          新增
+        </el-button>
+        <el-button
+          :disabled="multiple || sub"
+          @click="handleSubscribe"
+          v-hasPermi="['agriculture:topic-data:subscribe']"
+        >
+          订阅主题
+        </el-button>
+        <el-button
+          :disabled="multiple || unsub"
+          @click="handleUnsubscribe"
+          v-hasPermi="['agriculture:topic-data:unsubscribe']"
+        >
+          退订主题
+        </el-button>
+        <el-button @click="handleSubscribeAll" v-hasPermi="['agriculture:topic-data:subscribeall']">
+          全部订阅
+        </el-button>
+        <el-button
+          @click="handleUnSubscribeAll"
+          v-hasPermi="['agriculture:topic-data:unsubscribeAll']"
+        >
+          全部退订
+        </el-button>
+      </div>
+
+      <div class="flex items-center">
+        <!-- 一级标题这行右侧的按钮写在下面 修改点击事件函数 -->
+        <el-button @click="handleQuery" type="primary">
+          <Icon icon="ep:search" class="mr-5px" />
+          搜索
+        </el-button>
+        <el-button @click="resetQuery">
+          <Icon icon="ep:refresh" class="mr-5px" />
+          重置
+        </el-button>
+        <el-button
+          @click="handleExport"
+          :loading="exportLoading"
+          v-hasPermi="['agriculture:topic-data:export']"
+        >
+          <Icon icon="ep:download" class="mr-5px" />
+          导出
+        </el-button>
+
+        <button
+          class="circle-arrow-up ml-[16px]"
+          :class="showSearch ? 'rotate180andthemeBg' : 'rotate180andwhiteBg'"
+          @click="handleClickShowSearch"
+        >
+          <Icon :size="14" icon="ep:arrow-up" />
+        </button>
+      </div>
+    </div>
+
+    <!-- 搜索栏 注意 :model 和 ref 的名称 -->
+    <el-form
+      :model="queryParams"
+      ref="queryFormRef"
+      class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-[8px] mt-[8px] w-full form"
+      :class="showSearch ? 'opacity-100' : 'h-0 opacity-0'"
+      label-width="95px"
+      :inline="true"
+    >
+      <!--
+        原来的表单里的内容复制过来 不要操作按钮
+        去掉每项的固定宽度 比如 !w-240px !w-220px
+        如果 输入框内 有按钮 则只保留图标 不要文字
+      -->
       <el-form-item label="主题名称" prop="topicName">
         <el-input
           v-model="queryParams.topicName"
           placeholder="请输入主题名称"
           clearable
           @keyup.enter="handleQuery"
-          class="!w-240px"
         />
       </el-form-item>
       <!--      <el-form-item label="主题关键字" prop="topicKey">
-        <el-input
-          v-model="queryParams.topicKey"
-          placeholder="请输入主题关键字"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>-->
+          <el-input
+            v-model="queryParams.topicKey"
+            placeholder="请输入主题关键字"
+            clearable
+            @keyup.enter="handleQuery"
+            class="!w-240px"
+          />
+        </el-form-item>-->
       <el-form-item label="是否订阅" prop="topicIsorder">
-        <el-select v-model="queryParams.topicIsorder" clearable class="!w-240px">
+        <el-select v-model="queryParams.topicIsorder" clearable>
           <el-option
             v-for="dict in getStrDictOptions(DICT_TYPE.AGRI_MQTT_SUBSCRIBE_STATUS)"
             :key="dict.value"
@@ -31,7 +119,7 @@
         </el-select>
       </el-form-item>
       <el-form-item label="主题类型" prop="topicType">
-        <el-select v-model="queryParams.topicType" clearable class="!w-240px">
+        <el-select v-model="queryParams.topicType" clearable>
           <el-option
             v-for="dict in getStrDictOptions(DICT_TYPE.AGRI_MQTT_TOPIC_TYPE)"
             :key="dict.value"
@@ -40,140 +128,82 @@
           />
         </el-select>
       </el-form-item>
-      <el-form-item>
-        <el-button @click="handleQuery" type="primary">
-          <Icon icon="ep:search" class="mr-5px" />
-          搜索
-        </el-button>
-        <el-button @click="resetQuery">
-          <Icon icon="ep:refresh" class="mr-5px" />
-          重置
-        </el-button>
-      </el-form-item>
-    </custom-form>
-  </ContentWrap>
+    </el-form>
 
-  <!-- 列表 -->
-  <ContentWrap>
-    <el-form-item>
-      <el-button
-        type="primary"
-        plain
-        @click="openForm('create')"
-        v-hasPermi="['agriculture:topic-data:create']"
+    <div class="w-full mt-[16px]">
+      <el-table
+        v-loading="loading"
+        :data="list"
+        :stripe="true"
+        ref="multipleTableRef"
+        :show-overflow-tooltip="true"
+        border
+        @select="select"
+        @row-click="selectClick"
+        @selection-change="handleSelectionChange"
       >
-        <Icon icon="ep:plus" class="mr-5px" />
-        新增
-      </el-button>
-      <el-button
-        type="success"
-        plain
-        @click="handleExport"
-        :loading="exportLoading"
-        v-hasPermi="['agriculture:topic-data:export']"
-      >
-        <Icon icon="ep:download" class="mr-5px" />
-        导出
-      </el-button>
-      <el-button
-        type="primary"
-        plain
-        :disabled="multiple || sub"
-        @click="handleSubscribe"
-        v-hasPermi="['agriculture:topic-data:subscribe']"
-      >
-        订阅主题
-      </el-button>
-      <el-button
-        type="warning"
-        plain
-        :disabled="multiple || unsub"
-        @click="handleUnsubscribe"
-        v-hasPermi="['agriculture:topic-data:unsubscribe']"
-      >
-        退订主题
-      </el-button>
-      <el-button
-        type="success"
-        plain
-        @click="handleSubscribeAll"
-        v-hasPermi="['agriculture:topic-data:subscribeall']"
-      >
-        全部订阅
-      </el-button>
-      <el-button
-        type="danger"
-        plain
-        @click="handleUnSubscribeAll"
-        v-hasPermi="['agriculture:topic-data:unsubscribeAll']"
-      >
-        全部退订
-      </el-button>
-    </el-form-item>
-    <el-table
-      v-loading="loading"
-      :data="list"
-      :stripe="true"
-      ref="multipleTableRef"
-      :show-overflow-tooltip="true"
-      border
-      @select="select"
-      @row-click="selectClick"
-      @selection-change="handleSelectionChange"
-    >
-      <el-table-column type="selection" width="55px" align="left" />
-      <el-table-column label="主题名称" align="center" prop="topicName" />
-      <el-table-column label="主题类型" align="center" prop="topicType">
-        <template #default="scope">
-          <dict-tag :type="DICT_TYPE.AGRI_MQTT_TOPIC_TYPE" :value="scope.row.topicType" />
-        </template>
-      </el-table-column>
-      <!--      <el-table-column label="主题关键字" align="center" prop="topicKey"/>-->
-      <el-table-column label="主题" align="center" prop="topicPath" />
-      <!--      <el-table-column label="单位" align="center" prop="topicUnit"/>-->
-      <el-table-column label="是否订阅" align="center" prop="topicIsorder">
-        <template #default="scope">
-          <dict-tag :type="DICT_TYPE.AGRI_MQTT_SUBSCRIBE_STATUS" :value="scope.row.topicIsorder" />
-        </template>
-      </el-table-column>
-      <el-table-column label="处理类" align="center" prop="topicClass" />
-      <el-table-column label="备注" align="center" prop="topicNote" />
-      <el-table-column
-        label="创建时间"
-        align="center"
-        prop="createTime"
-        :formatter="dateFormatter"
-        width="180px"
-      />
-      <el-table-column label="操作" align="center" width="100">
-        <template #default="scope">
-          <el-button
-            link
-            type="primary"
-            @click="openForm('update', scope.row.id)"
-            v-hasPermi="['agriculture:topic-data:update']"
-          >
-            编辑
-          </el-button>
-          <el-button
-            link
-            type="danger"
-            @click="handleDelete(scope.row.id)"
-            v-hasPermi="['agriculture:topic-data:delete']"
-          >
-            删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <!-- 分页 -->
+        <el-table-column type="selection" width="55px" align="left" />
+        <el-table-column label="主题名称" align="center" prop="topicName" />
+        <el-table-column label="主题类型" align="center" prop="topicType">
+          <template #default="scope">
+            <dict-tag :type="DICT_TYPE.AGRI_MQTT_TOPIC_TYPE" :value="scope.row.topicType" />
+          </template>
+        </el-table-column>
+        <!--      <el-table-column label="主题关键字" align="center" prop="topicKey"/>-->
+        <el-table-column label="主题" align="center" prop="topicPath" />
+        <!--      <el-table-column label="单位" align="center" prop="topicUnit"/>-->
+        <el-table-column label="是否订阅" align="center" prop="topicIsorder">
+          <template #default="scope">
+            <dict-tag
+              :type="DICT_TYPE.AGRI_MQTT_SUBSCRIBE_STATUS"
+              :value="scope.row.topicIsorder"
+            />
+          </template>
+        </el-table-column>
+        <el-table-column label="处理类" align="center" prop="topicClass" />
+        <el-table-column label="备注" align="center" prop="topicNote" />
+        <el-table-column
+          label="创建时间"
+          align="center"
+          prop="createTime"
+          :formatter="dateFormatter"
+          width="180px"
+        />
+        <el-table-column label="操作" align="center" fixed="right" min-width="154px">
+          <template #default="scope">
+            <div class="flex items-center justify-center">
+              <el-button
+                link
+                type="primary"
+                @click="openForm('update', scope.row.id)"
+                v-hasPermi="['agriculture:topic-data:update']"
+              >
+                编辑
+              </el-button>
+              <div class="mx-[12px] w-[1px] h-[24px] bg-[#e6e6e6]"></div>
+              <el-button
+                link
+                type="danger"
+                @click="handleDelete(scope.row.id)"
+                v-hasPermi="['agriculture:topic-data:delete']"
+              >
+                删除
+              </el-button>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <!-- 页码组件 注意绑定的值和事件函数 -->
     <Pagination
+      style="margin-bottom: 0; margin-top: 8px"
       :total="total"
       v-model:page="queryParams.pageNo"
       v-model:limit="queryParams.pageSize"
       @pagination="getList"
     />
-  </ContentWrap>
+  </el-scrollbar>
 
   <!-- 表单弹窗：添加/修改 -->
   <TopicDataForm ref="formRef" @success="getList" />
@@ -361,10 +391,97 @@ const selectClick = (row) => {
 onMounted(() => {
   getList();
 });
+
+/**
+ * topMenuHeight      顶部菜单和标签页高度
+ * contentPadding     页面内容外边距
+ */
+const topMenuHeight = 85;
+const contentPadding = 8;
+
+// 展开或收起搜索栏
+const showSearch = ref(false);
+const handleClickShowSearch = () => {
+  showSearch.value = !showSearch.value;
+};
 </script>
 <style scoped lang="scss">
 // 隐藏全选按钮
 :deep(.el-table th.el-table__cell:nth-child(1) .cell) {
   visibility: hidden;
+}
+
+// 鼠标移在按钮上时显示主题色边框
+:deep(.el-button:hover) {
+  border-color: var(--el-color-primary);
+}
+
+// 去掉表单的边距
+:deep(.form > *) {
+  margin: 0;
+}
+
+// 调整表单标签和输入框之间的距离
+:deep(.form .el-form-item__label) {
+  padding: 0 4px 0 0;
+}
+
+// 收起
+.circle-arrow-up {
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  border: 1px solid #ebebeb;
+  color: #333;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    color: white;
+    border-width: 0;
+    background-color: var(--el-color-primary);
+  }
+}
+
+// 向上箭头展开收起的动画
+@keyframes rotate180andwhiteBg {
+  from {
+    transform: rotate(0deg);
+    color: #333;
+    background-color: white;
+  }
+  to {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+}
+
+.rotate180andwhiteBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andwhiteBg;
+  animation-fill-mode: forwards;
+}
+
+@keyframes rotate180andthemeBg {
+  from {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+  to {
+    transform: rotate(360deg);
+    color: #333;
+    background-color: white;
+  }
+}
+
+.rotate180andthemeBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andthemeBg;
+  animation-fill-mode: forwards;
 }
 </style>
