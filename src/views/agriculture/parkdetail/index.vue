347cbb09c2b0af20ece96e41e53ff6cfc7c09e6f
zhoumingxiu <zhoumingxiu@inspur.com>
2024-11-26 17:45:14 +0800

    fix: 基地分类，地块信息修改

--- src/views/agriculture/parkdetail/index.vue
+++ src/views/agriculture/parkdetail/index.vue
@@ -1,18 +1,80 @@
 <template>
-  <ContentWrap>
-    <!-- 搜索工作栏 -->
-    <custom-form class="-mb-15px" :model="queryParams" ref="queryFormRef" :inline="true" label-width="68px">
-      <!--      <el-form-item label="编号" prop="code">
+  <el-scrollbar
+    class="w-full bg-white rounded-[6px] text-[#666] text-[14px] p-[16px] box-border"
+    :style="{ height: 'calc(100vh - ' + (topMenuHeight + 2 * contentPadding) + 'px)' }"
+  >
+    <div class="w-full flex justify-between items-center">
+      <div class="flex items-center">
+        <!-- 一级标题名字 -->
+        <h1 class="m-0 text-[#333] font-bold text-[18px]">地块信息</h1>
+
+        <Icon icon="ep:question-filled" :size="14" class="ml-[8px] cursor-pointer text-[#F08000]" />
+        <div class="w-[1px] h-[32px] mx-[16px] bg-[#ebebeb]"></div>
+
+        <!--
+          一级标题旁边的按钮
+          主按钮 type="primary" 次按钮不设置 type 属性 不设置 plain 属性
+        -->
+        <!--        <el-button
+          type="primary"
+          @click="openForm('create')"
+          v-hasPermi="['agriculture:park-detail:create']"
+        >
+          <Icon icon="ep:plus" class="mr-5px" />
+          新增
+        </el-button>-->
+      </div>
+
+      <div class="flex items-center">
+        <!-- 一级标题这行右侧的按钮写在下面 修改点击事件函数 -->
+        <el-button @click="handleQuery" type="primary">
+          <Icon icon="ep:search" class="mr-5px" />
+          搜索
+        </el-button>
+        <el-button @click="resetQuery">
+          <Icon icon="ep:refresh" class="mr-5px" />
+          重置
+        </el-button>
+        <el-button
+          @click="handleExport"
+          :loading="exportLoading"
+          v-hasPermi="['agriculture:park-detail:export']"
+        >
+          <Icon icon="ep:download" class="mr-5px" />
+          导出
+        </el-button>
+
+        <button
+          class="circle-arrow-up ml-[16px]"
+          :class="showSearch ? 'rotate180andthemeBg' : 'rotate180andwhiteBg'"
+          @click="handleClickShowSearch"
+        >
+          <Icon :size="14" icon="ep:arrow-up" />
+        </button>
+      </div>
+    </div>
+
+    <!-- 搜索栏 注意 :model 和 ref 的名称 -->
+    <el-form
+      :model="queryParams"
+      ref="queryFormRef"
+      class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-[8px] mt-[8px] w-full form"
+      :class="showSearch ? 'opacity-100' : 'h-0 opacity-0'"
+      label-width="95px"
+      :inline="true"
+    >
+      <!--
+        原来的表单里的内容复制过来 不要操作按钮
+        去掉每项的固定宽度 比如 !w-240px !w-220px
+        如果 输入框内 有按钮 则只保留图标 不要文字
+      -->
+      <el-form-item label="名称" prop="name">
         <el-input
-          v-model="queryParams.code"
-          placeholder="请输入编号"
+          v-model="queryParams.name"
+          placeholder="请输入名称"
           clearable
           @keyup.enter="handleQuery"
-          class="!w-240px"
         />
-      </el-form-item>-->
-      <el-form-item label="名称" prop="name">
-        <el-input v-model="queryParams.name" placeholder="请输入名称" clearable @keyup.enter="handleQuery" class="!w-240px" />
       </el-form-item>
       <el-form-item label="创建时间" prop="createTime">
         <el-date-picker
@@ -22,107 +84,102 @@
           start-placeholder="开始日期"
           end-placeholder="结束日期"
           :default-time="[new Date('1 00:00:00'), new Date('1 23:59:59')]"
-          class="!w-240px"
         />
       </el-form-item>
-      <el-form-item>
-        <el-button @click="handleQuery" type="primary">
-          <Icon icon="ep:search" class="mr-5px" />
-          搜索
-        </el-button>
-        <el-button @click="resetQuery">
-          <Icon icon="ep:refresh" class="mr-5px" />
-          重置
-        </el-button>
-        <el-button type="primary" plain @click="openForm('create')" v-hasPermi="['agriculture:park-detail:create']">
-          <Icon icon="ep:plus" class="mr-5px" />
-          新增
-        </el-button>
-        <el-button
-          type="success"
-          plain
-          @click="handleExport"
-          :loading="exportLoading"
-          v-hasPermi="['agriculture:park-detail:export']"
-        >
-          <Icon icon="ep:download" class="mr-5px" />
-          导出
-        </el-button>
-      </el-form-item>
-    </custom-form>
-  </ContentWrap>
+    </el-form>
 
-  <!-- 列表 -->
-  <ContentWrap>
-    <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
-      <!--      <el-table-column label="所属基地" align="center" prop="parkId" width="200"/>-->
-      <el-table-column label="基地名称" align="center" prop="parkName" width="200" />
-      <el-table-column label="基地类型" align="center" prop="parkType" width="200" />
-      <!--      <el-table-column label="编号" align="center" prop="code" width="200"/>-->
-      <el-table-column label="名称" align="center" prop="name" width="200" />
-      <el-table-column label="地块类型" align="center" prop="type" width="150">
-        <template #default="scope">
-          <dict-tag :type="DICT_TYPE.AGRI_PLOT_TYPE" :value="scope.row.type" />
-        </template>
-      </el-table-column>
-      <el-table-column label="海拔" align="center" prop="altitude">
-        <template #default="scope">
-          {{ scope.row.altitude != undefined ? scope.row.altitude + '米' : '-' }}
-        </template>
-      </el-table-column>
-      <el-table-column label="纬度" align="center" prop="latitude" width="120" />
-      <el-table-column label="经度" align="center" prop="longitude" width="120" />
-      <el-table-column label="图片" align="center" prop="img">
-        <template #default="{ row }">
-          <el-image class="h-50px w-50px" lazy :src="row.img" :preview-src-list="[row.img]" preview-teleported fit="cover" />
-        </template>
-      </el-table-column>
-      <el-table-column label="通讯地址" align="center" prop="address" width="200" />
-      <el-table-column label="联系人" align="center" prop="contact" />
-      <el-table-column label="联系电话" align="center" prop="tel" width="120" />
-      <el-table-column label="面积" align="center" prop="area" width="100">
-        <template #default="scope">
-          {{ scope.row.area != undefined ? scope.row.area + '亩' : '-' }}
-        </template>
-      </el-table-column>
-      <el-table-column label="数量" align="center" prop="quantity" width="100" />
-      <el-table-column label="模式" align="center" prop="adoptionType" width="100" />
-      <el-table-column label="备注" align="center" prop="remark" />
+    <div class="w-full mt-[16px]">
+      <!-- 原来的表格复制过来 操作按钮按照 el-table操作按钮.md 里的例子 -->
+      <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
+        <!--      <el-table-column label="所属基地" align="center" prop="parkId" width="200"/>-->
+        <el-table-column label="基地名称" align="center" prop="parkName" width="200" />
+        <el-table-column label="基地类型" align="center" prop="parkType" width="200" />
+        <!--      <el-table-column label="编号" align="center" prop="code" width="200"/>-->
+        <el-table-column label="名称" align="center" prop="name" width="200" />
+        <el-table-column label="地块类型" align="center" prop="type" width="150">
+          <template #default="scope">
+            <dict-tag :type="DICT_TYPE.AGRI_PLOT_TYPE" :value="scope.row.type" />
+          </template>
+        </el-table-column>
+        <el-table-column label="海拔" align="center" prop="altitude">
+          <template #default="scope">
+            {{ scope.row.altitude != undefined ? scope.row.altitude + '米' : '-' }}
+          </template>
+        </el-table-column>
+        <el-table-column label="纬度" align="center" prop="latitude" width="120" />
+        <el-table-column label="经度" align="center" prop="longitude" width="120" />
+        <el-table-column label="图片" align="center" prop="img">
+          <template #default="{ row }">
+            <el-image
+              class="h-50px w-50px"
+              lazy
+              :src="row.img"
+              :preview-src-list="[row.img]"
+              preview-teleported
+              fit="cover"
+            />
+          </template>
+        </el-table-column>
+        <el-table-column label="通讯地址" align="center" prop="address" width="200" />
+        <el-table-column label="联系人" align="center" prop="contact" />
+        <el-table-column label="联系电话" align="center" prop="tel" width="120" />
+        <el-table-column label="面积" align="center" prop="area" width="100">
+          <template #default="scope">
+            {{ scope.row.area != undefined ? scope.row.area + '亩' : '-' }}
+          </template>
+        </el-table-column>
+        <el-table-column label="数量" align="center" prop="quantity" width="100" />
+        <el-table-column label="模式" align="center" prop="adoptionType" width="100" />
+        <el-table-column label="备注" align="center" prop="remark" />
+        <el-table-column
+          label="创建时间"
+          align="center"
+          prop="createTime"
+          :formatter="dateFormatter"
+          width="180px"
+        />
+        <el-table-column label="二维码图片" fixed="right" align="center" prop="qrImg" width="200px">
+          <template #default="scope">
+            <el-image
+              :src="`data:image/png;base64,${scope.row.qrImg}`"
+              style="object-fit: cover; width: 2rem; height: 2rem"
+              preview-teleported
+              :preview-src-list="[`data:image/png;base64,${scope.row.qrImg}`]"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
 
-      <el-table-column label="创建时间" align="center" prop="createTime" :formatter="dateFormatter" width="180px" />
-      <el-table-column label="二维码图片" fixed="right" align="center" prop="qrImg" width="200px">
-        <template #default="scope">
-          <el-image
-            :src="`data:image/png;base64,${scope.row.qrImg}`"
-            style="object-fit: cover; width: 2rem; height: 2rem"
-            preview-teleported
-            :preview-src-list="[`data:image/png;base64,${scope.row.qrImg}`]"
-          />
-        </template>
-      </el-table-column>
-      <!--      <el-table-column label="操作" fixed="right" align="center">-->
-      <!--&lt;!&ndash;        <template #default="scope">&ndash;&gt;-->
-      <!--&lt;!&ndash;          <el-button&ndash;&gt;-->
-      <!--&lt;!&ndash;            link&ndash;&gt;-->
-      <!--&lt;!&ndash;            type="primary"&ndash;&gt;-->
-      <!--&lt;!&ndash;            @click="goCheck(scope.row)"&ndash;&gt;-->
-      <!--&lt;!&ndash;          >&ndash;&gt;-->
-      <!--&lt;!&ndash;            溯源&ndash;&gt;-->
-      <!--&lt;!&ndash;          </el-button>&ndash;&gt;-->
-      <!--&lt;!&ndash;          &lt;!&ndash;          <el-button&ndash;&gt;&ndash;&gt;-->
-      <!--&lt;!&ndash;          &lt;!&ndash;            link&ndash;&gt;&ndash;&gt;-->
-      <!--&lt;!&ndash;          &lt;!&ndash;            type="danger"&ndash;&gt;&ndash;&gt;-->
-      <!--&lt;!&ndash;          &lt;!&ndash;            @click="handleDelete(scope.row.id)"&ndash;&gt;&ndash;&gt;-->
-      <!--&lt;!&ndash;          &lt;!&ndash;            v-hasPermi="['agriculture:park-detail:delete']"&ndash;&gt;&ndash;&gt;-->
-      <!--&lt;!&ndash;          &lt;!&ndash;          >&ndash;&gt;&ndash;&gt;-->
-      <!--&lt;!&ndash;          &lt;!&ndash;            删除&ndash;&gt;&ndash;&gt;-->
-      <!--&lt;!&ndash;          &lt;!&ndash;          </el-button>&ndash;&gt;&ndash;&gt;-->
-      <!--&lt;!&ndash;        </template>&ndash;&gt;-->
-      <!--      </el-table-column>-->
-    </el-table>
-    <!-- 分页 -->
-    <Pagination :total="total" v-model:page="queryParams.pageNo" v-model:limit="queryParams.pageSize" @pagination="getList" />
-  </ContentWrap>
+    <!-- 页码组件 注意绑定的值和事件函数 -->
+    <Pagination
+      style="margin-bottom: 0; margin-top: 8px"
+      :total="total"
+      v-model:page="queryParams.pageNo"
+      v-model:limit="queryParams.pageSize"
+      @pagination="getList"
+    />
+  </el-scrollbar>
+
+  <!--      <el-table-column label="操作" fixed="right" align="center">-->
+  <!--&lt;!&ndash;        <template #default="scope">&ndash;&gt;-->
+  <!--&lt;!&ndash;          <el-button&ndash;&gt;-->
+  <!--&lt;!&ndash;            link&ndash;&gt;-->
+  <!--&lt;!&ndash;            type="primary"&ndash;&gt;-->
+  <!--&lt;!&ndash;            @click="goCheck(scope.row)"&ndash;&gt;-->
+  <!--&lt;!&ndash;          >&ndash;&gt;-->
+  <!--&lt;!&ndash;            溯源&ndash;&gt;-->
+  <!--&lt;!&ndash;          </el-button>&ndash;&gt;-->
+  <!--&lt;!&ndash;          &lt;!&ndash;          <el-button&ndash;&gt;&ndash;&gt;-->
+  <!--&lt;!&ndash;          &lt;!&ndash;            link&ndash;&gt;&ndash;&gt;-->
+  <!--&lt;!&ndash;          &lt;!&ndash;            type="danger"&ndash;&gt;&ndash;&gt;-->
+  <!--&lt;!&ndash;          &lt;!&ndash;            @click="handleDelete(scope.row.id)"&ndash;&gt;&ndash;&gt;-->
+  <!--&lt;!&ndash;          &lt;!&ndash;            v-hasPermi="['agriculture:park-detail:delete']"&ndash;&gt;&ndash;&gt;-->
+  <!--&lt;!&ndash;          &lt;!&ndash;          >&ndash;&gt;&ndash;&gt;-->
+  <!--&lt;!&ndash;          &lt;!&ndash;            删除&ndash;&gt;&ndash;&gt;-->
+  <!--&lt;!&ndash;          &lt;!&ndash;          </el-button>&ndash;&gt;&ndash;&gt;-->
+  <!--&lt;!&ndash;        </template>&ndash;&gt;-->
+  <!--      </el-table-column>-->
 
   <!-- 表单弹窗：添加/修改 -->
   <ParkDetailForm ref="formRef" @success="getList" />
@@ -245,4 +302,97 @@ const goCheck = (row) => {
 onMounted(() => {
   getList();
 });
+
+/**
+ * topMenuHeight      顶部菜单和标签页高度
+ * contentPadding     页面内容外边距
+ */
+const topMenuHeight = 85;
+const contentPadding = 8;
+
+// 展开或收起搜索栏
+const showSearch = ref(false);
+const handleClickShowSearch = () => {
+  showSearch.value = !showSearch.value;
+};
 </script>
+
+<style lang="scss" scoped>
+// 原页面样式复制在下面
+
+// 原页面样式复制在上面
+
+// 鼠标移在按钮上时显示主题色边框
+:deep(.el-button:hover) {
+  border-color: var(--el-color-primary);
+}
+
+// 去掉表单的边距
+:deep(.form > *) {
+  margin: 0;
+}
+
+// 调整表单标签和输入框之间的距离
+:deep(.form .el-form-item__label) {
+  padding: 0 4px 0 0;
+}
+
+// 收起
+.circle-arrow-up {
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  border: 1px solid #ebebeb;
+  color: #333;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    color: white;
+    border-width: 0;
+    background-color: var(--el-color-primary);
+  }
+}
+
+// 向上箭头展开收起的动画
+@keyframes rotate180andwhiteBg {
+  from {
+    transform: rotate(0deg);
+    color: #333;
+    background-color: white;
+  }
+  to {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+}
+
+.rotate180andwhiteBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andwhiteBg;
+  animation-fill-mode: forwards;
+}
+
+@keyframes rotate180andthemeBg {
+  from {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+  to {
+    transform: rotate(360deg);
+    color: #333;
+    background-color: white;
+  }
+}
+
+.rotate180andthemeBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andthemeBg;
+  animation-fill-mode: forwards;
+}
+</style>
