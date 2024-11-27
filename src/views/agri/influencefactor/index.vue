219d405eee1dbe2c3a57122f31aba4dfe6e95572
wangyan21 <wangyan21@inspur.com>
2024-11-27 10:05:29 +0800

    控制模型-影响因子管理，表单列表样式修改

--- src/views/agri/influencefactor/index.vue
+++ src/views/agri/influencefactor/index.vue
@@ -1,29 +1,80 @@
 <template>
-  <ContentWrap>
-    <!-- 搜索工作栏 -->
-    <custom-form
-      class="-mb-15px"
+  <el-scrollbar
+    class="w-full bg-white rounded-[6px] text-[#666] text-[14px] p-[16px] box-border"
+    :style="{ height: 'calc(100vh - ' + (topMenuHeight + 2 * contentPadding) + 'px)' }"
+  >
+    <div class="w-full flex justify-between items-center">
+      <div class="flex items-center">
+        <!-- 一级标题名字 todo替换成菜单名称-->
+        <h1 class="m-0 text-[#333] font-bold text-[18px]">影响因子管理</h1>
+        <Icon icon="ep:question-filled" :size="14" class="ml-[8px] cursor-pointer text-[#F08000]" />
+        <div class="w-[1px] h-[32px] mx-[16px] bg-[#ebebeb]"></div>
+        <!-- 一级标题旁边的按钮 -->
+        <!-- todo原新增按钮 -->
+        <!-- todo需要包含type="primary"&&不能有plain属性 -->
+        <el-button
+          type="primary"
+          @click="openForm('create')"
+          v-hasPermi="['agriculture:feeding-rule:create']"
+        >
+          <Icon icon="ep:plus" class="mr-5px" />
+          新增
+        </el-button>
+      </div>
+
+      <div class="flex items-center">
+        <!-- 一级标题这行右侧的按钮写在下面 修改点击事件函数 -->
+        <!-- todo复制原页面【搜索、重置、导出】 -->
+        <!-- todo【搜索】按钮需要包含type="primary"&&不能有plain属性 -->
+        <!-- todo删除导出按钮的type和plain属性 -->
+        <el-button @click="handleQuery" type="primary">
+          <Icon icon="ep:search" class="mr-5px" />
+          搜索
+        </el-button>
+        <el-button @click="resetQuery">
+          <Icon icon="ep:refresh" class="mr-5px" />
+          重置
+        </el-button>
+        <el-button
+          @click="handleExport"
+          :loading="exportLoading"
+          v-hasPermi="['agriculture:feeding-rule:export']"
+        >
+          <Icon icon="ep:download" class="mr-5px" />
+          导出
+        </el-button>
+        <button
+          class="circle-arrow-up ml-[16px]"
+          :class="showSearch ? 'rotate180andthemeBg' : 'rotate180andwhiteBg'"
+          @click="handleClickShowSearch"
+        >
+          <Icon :size="14" icon="ep:arrow-up" />
+        </button>
+      </div>
+    </div>
+
+    <!-- 搜索栏 注意 :model 和 ref 的名称 -->
+    <el-form
       :model="queryParams"
       ref="queryFormRef"
+      class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-[8px] mt-[8px] w-full form"
+      :class="showSearch ? 'opacity-100' : 'h-0 opacity-0'"
+      label-width="95px"
       :inline="true"
-      label-width="68px"
     >
+      <!-- 原来的表单里的内容复制过来 不要操作按钮 -->
+      <!-- todo复制原来的搜索列表 -->
+      <!-- todo 所有的都需要删除class=“!w-240” 这一类的属性 -->
       <el-form-item label="因子名称" prop="factorName">
         <el-input
           v-model="queryParams.factorName"
           placeholder="请输入因子名称"
           clearable
           @keyup.enter="handleQuery"
-          class="!w-240px"
         />
       </el-form-item>
       <el-form-item label="因子类别" prop="factorType">
-        <el-select
-          v-model="queryParams.factorType"
-          placeholder="请选择因子类别"
-          clearable
-          class="!w-240px"
-        >
+        <el-select v-model="queryParams.factorType" placeholder="请选择因子类别" clearable>
           <el-option
             v-for="dict in getIntDictOptions(DICT_TYPE.FACTOR_TYPE)"
             :key="dict.label"
@@ -33,12 +84,7 @@
         </el-select>
       </el-form-item>
       <el-form-item label="监测类型" prop="monitorType">
-        <el-select
-          v-model="queryParams.monitorType"
-          placeholder="请选择传感器监测类型"
-          clearable
-          class="!w-240px"
-        >
+        <el-select v-model="queryParams.monitorType" placeholder="请选择传感器监测类型" clearable>
           <el-option
             v-for="dict in getIntDictOptions(DICT_TYPE.MONITOR_TYPE)"
             :key="dict.value"
@@ -53,119 +99,99 @@
           placeholder="请输入影响类别"
           clearable
           @keyup.enter="handleQuery"
-          class="!w-240px"
         />
       </el-form-item>
-      <el-form-item>
-        <el-button @click="handleQuery" type="primary">
-          <Icon icon="ep:search" class="mr-5px"/>
-          搜索
-        </el-button>
-        <el-button @click="resetQuery">
-          <Icon icon="ep:refresh" class="mr-5px"/>
-          重置
-        </el-button>
-      </el-form-item>
-      <el-row>
-        <el-form-item>
-          <el-button
-            type="primary"
-            plain
-            @click="openForm('create')"
-            v-hasPermi="['agriculture:feeding-rule:create']"
-          >
-            <Icon icon="ep:plus" class="mr-5px" /> 新增
-          </el-button>
-          <el-button
-            type="success"
-            plain
-            @click="handleExport"
-            :loading="exportLoading"
-            v-hasPermi="['agriculture:feeding-rule:export']"
-          >
-            <Icon icon="ep:download" class="mr-5px" /> 导出
-          </el-button>
-        </el-form-item>
-      </el-row>
-    </custom-form>
-  </ContentWrap>
+    </el-form>
 
-  <!-- 列表 -->
-  <ContentWrap>
-    <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
-      <!--      <el-table-column label="主键" align="center" prop="id"/>-->
-      <el-table-column label="因子名称" align="center" prop="factorName"/>
-      <el-table-column label="传感器监测类型" align="center" prop="monitorType">
-        <template #default="scope">
-          <dict-tag :type="DICT_TYPE.MONITOR_TYPE" :value="scope.row.monitorType"/>
-        </template>
-      </el-table-column>
-      <el-table-column label="因子类别" align="center" prop="factorType">
-        <template #default="scope">
-          <dict-tag :type="DICT_TYPE.FACTOR_TYPE" :value="scope.row.factorType"/>
-        </template>
-      </el-table-column>
-      <el-table-column label="影响类别" align="center" prop="remark"/>
-      <el-table-column label="影响结果" align="center" prop="influence"/>
-      <el-table-column label="影响信息" align="center" prop="influenceInfo"/>
-      <el-table-column label="建议" align="center" prop="proposal"/>
-      <!--      <el-table-column-->
-      <!--        label="创建时间"-->
-      <!--        align="center"-->
-      <!--        prop="createTime"-->
-      <!--        :formatter="dateFormatter"-->
-      <!--        width="180px"-->
-      <!--      />-->
-      <el-table-column label="操作" align="center">
-        <template #default="scope">
-          <el-button
-            link
-            type="primary"
-            @click="openForm('update', scope.row.id)"
-            v-hasPermi="['agri:influence-factor:update']"
-          >
-            编辑
-          </el-button>
-          <el-button
-            link
-            type="danger"
-            @click="handleDelete(scope.row.id)"
-            v-hasPermi="['agri:influence-factor:delete']"
-          >
-            删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <!-- 分页 -->
+    <div class="w-full mt-[8px]">
+      <!-- 原来的表格复制过来 操作按钮按照 el-table操作按钮.md 里的例子 -->
+      <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
+        <!-- todo复制列表 -->
+        <el-table-column label="因子名称" align="center" prop="factorName" />
+        <el-table-column label="传感器监测类型" align="center" prop="monitorType">
+          <template #default="scope">
+            <dict-tag :type="DICT_TYPE.MONITOR_TYPE" :value="scope.row.monitorType" />
+          </template>
+        </el-table-column>
+        <el-table-column label="因子类别" align="center" prop="factorType">
+          <template #default="scope">
+            <dict-tag :type="DICT_TYPE.FACTOR_TYPE" :value="scope.row.factorType" />
+          </template>
+        </el-table-column>
+        <el-table-column label="影响类别" align="center" prop="remark" />
+        <el-table-column label="影响结果" align="center" prop="influence" />
+        <el-table-column label="影响信息" align="center" prop="influenceInfo" />
+        <el-table-column label="建议" align="center" prop="proposal" />
+
+        <el-table-column label="操作" align="center" fixed="right" min-width="154px">
+          <template #default="scope">
+            <!-- todo操作按钮 -->
+            <!-- 1.  <template #default="scope"> 中，加入
+                <div class="flex items-center justify-center">
+                  其中放入编辑，删除"按钮"等，每一个按钮中完成后加入
+                    <div class="mx-[12px] w-[1px] h-[24px] bg-[#e6e6e6]"></div>
+                  这段代码的含义是“竖杠”分隔符
+                </div>
+                2.请注意“方案一”和“方案二”只采用一种，请根据自身按钮数量选择性删除或保留
+              -->
+            <!-- todo方案一 -->
+            <div class="flex items-center justify-center">
+              <el-button
+                link
+                type="primary"
+                @click="openForm('update', scope.row.id)"
+                v-hasPermi="['agri:influence-factor:update']"
+              >
+                编辑
+              </el-button>
+              <div class="mx-[12px] w-[1px] h-[24px] bg-[#e6e6e6]"></div>
+              <el-button
+                link
+                type="danger"
+                @click="handleDelete(scope.row.id)"
+                v-hasPermi="['agri:influence-factor:delete']"
+              >
+                删除
+              </el-button>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <!-- 页码组件 注意绑定的值和事件函数 -->
+    <!-- 不用改 -->
     <Pagination
+      style="margin-bottom: 0; margin-top: 8px"
       :total="total"
       v-model:page="queryParams.pageNo"
       v-model:limit="queryParams.pageSize"
       @pagination="getList"
     />
-  </ContentWrap>
+  </el-scrollbar>
+  <!-- todo页面组件复制在下面 -->
+  <!-- 表单弹窗-->
 
   <!-- 表单弹窗：添加/修改 -->
-  <InfluenceFactorForm ref="formRef" @success="getList"/>
+  <InfluenceFactorForm ref="formRef" @success="getList" />
 </template>
 
 <script setup lang="ts">
-import {dateFormatter} from '@/utils/formatTime'
-import download from '@/utils/download'
-import {InfluenceFactorApi, InfluenceFactorVO} from '@/api/agri/influencefactor'
-import InfluenceFactorForm from './InfluenceFactorForm.vue'
-import {getIntDictOptions, DICT_TYPE} from "@/utils/dict";
+import { dateFormatter } from '@/utils/formatTime';
+import download from '@/utils/download';
+import { InfluenceFactorApi, InfluenceFactorVO } from '@/api/agri/influencefactor';
+import InfluenceFactorForm from './InfluenceFactorForm.vue';
+import { getIntDictOptions, DICT_TYPE } from '@/utils/dict';
 
 /** 影响因子 列表 */
-defineOptions({name: 'InfluenceFactor'})
+defineOptions({ name: 'InfluenceFactor' });
 
-const message = useMessage() // 消息弹窗
-const {t} = useI18n() // 国际化
+const message = useMessage(); // 消息弹窗
+const { t } = useI18n(); // 国际化
 
-const loading = ref(true) // 列表的加载中
-const list = ref<InfluenceFactorVO[]>([]) // 列表的数据
-const total = ref(0) // 列表的总页数
+const loading = ref(true); // 列表的加载中
+const list = ref<InfluenceFactorVO[]>([]); // 列表的数据
+const total = ref(0); // 列表的总页数
 const queryParams = reactive({
   pageNo: 1,
   pageSize: 10,
@@ -176,72 +202,162 @@ const queryParams = reactive({
   proposal: undefined,
   remark: undefined,
   influenceInfo: undefined,
-  createTime: [],
-})
-const queryFormRef = ref() // 搜索的表单
-const exportLoading = ref(false) // 导出的加载中
+  createTime: []
+});
+const queryFormRef = ref(); // 搜索的表单
+const exportLoading = ref(false); // 导出的加载中
 
 /** 查询列表 */
 const getList = async () => {
-  loading.value = true
+  loading.value = true;
   try {
-    const data = await InfluenceFactorApi.getInfluenceFactorPage(queryParams)
-    list.value = data.list
-    total.value = data.total
+    const data = await InfluenceFactorApi.getInfluenceFactorPage(queryParams);
+    list.value = data.list;
+    total.value = data.total;
   } finally {
-    loading.value = false
+    loading.value = false;
   }
-}
+};
 
 /** 搜索按钮操作 */
 const handleQuery = () => {
-  queryParams.pageNo = 1
-  getList()
-}
+  queryParams.pageNo = 1;
+  getList();
+};
 
 /** 重置按钮操作 */
 const resetQuery = () => {
-  queryFormRef.value.resetFields()
-  handleQuery()
-}
+  queryFormRef.value.resetFields();
+  handleQuery();
+};
 
 /** 添加/修改操作 */
-const formRef = ref()
+const formRef = ref();
 const openForm = (type: string, id?: number) => {
-  formRef.value.open(type, id)
-}
+  formRef.value.open(type, id);
+};
 
 /** 删除按钮操作 */
 const handleDelete = async (id: number) => {
   try {
     // 删除的二次确认
-    await message.delConfirm()
+    await message.delConfirm();
     // 发起删除
-    await InfluenceFactorApi.deleteInfluenceFactor(id)
-    message.success(t('common.delSuccess'))
+    await InfluenceFactorApi.deleteInfluenceFactor(id);
+    message.success(t('common.delSuccess'));
     // 刷新列表
-    await getList()
-  } catch {
-  }
-}
+    await getList();
+  } catch {}
+};
 
 /** 导出按钮操作 */
 const handleExport = async () => {
   try {
     // 导出的二次确认
-    await message.exportConfirm()
+    await message.exportConfirm();
     // 发起导出
-    exportLoading.value = true
-    const data = await InfluenceFactorApi.exportInfluenceFactor(queryParams)
-    download.excel(data, '影响因子.xls')
+    exportLoading.value = true;
+    const data = await InfluenceFactorApi.exportInfluenceFactor(queryParams);
+    download.excel(data, '影响因子.xls');
   } catch {
   } finally {
-    exportLoading.value = false
+    exportLoading.value = false;
   }
-}
+};
 
 /** 初始化 **/
 onMounted(() => {
-  getList()
-})
+  getList();
+});
+
+/**
+ * topMenuHeight      顶部菜单和标签页高度
+ * contentPadding     页面内容外边距
+ */
+const topMenuHeight = 85;
+const contentPadding = 8;
+
+// 展开或收起搜索栏
+const showSearch = ref(false);
+const handleClickShowSearch = () => {
+  showSearch.value = !showSearch.value;
+};
 </script>
+
+<style lang="scss" scoped>
+// 原页面样式复制在下面
+
+// 鼠标移在按钮上时显示主题色边框
+:deep(.el-button:hover) {
+  border-color: var(--el-color-primary);
+}
+
+// 去掉表单的边距
+:deep(.form > *) {
+  margin: 0;
+}
+
+// 调整表单标签和输入框之间的距离
+:deep(.form .el-form-item__label) {
+  padding: 0 4px 0 0;
+}
+
+// 收起
+.circle-arrow-up {
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  border: 1px solid #ebebeb;
+  color: #333;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    color: white;
+    border-width: 0;
+    background-color: var(--el-color-primary);
+  }
+}
+
+// 向上箭头展开收起的动画
+@keyframes rotate180andwhiteBg {
+  from {
+    transform: rotate(0deg);
+    color: #333;
+    background-color: white;
+  }
+  to {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+}
+
+.rotate180andwhiteBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andwhiteBg;
+  animation-fill-mode: forwards;
+}
+
+@keyframes rotate180andthemeBg {
+  from {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+  to {
+    transform: rotate(360deg);
+    color: #333;
+    background-color: white;
+  }
+}
+
+.rotate180andthemeBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andthemeBg;
+  animation-fill-mode: forwards;
+}
+</style>
