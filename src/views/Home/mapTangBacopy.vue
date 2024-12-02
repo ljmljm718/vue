3af8dbbf1d882e0ddbf7dba812de7230a75c6399
Tony <weijialin@inspur.com>
2024-12-02 14:55:43 +0800

    feat: 紧急修复

--- src/views/Home/mapTangBacopy.vue
+++ src/views/Home/mapTangBacopy.vue
@@ -21,8 +21,64 @@
 </template>
 <script setup lang="ts">
 // TODO: 天地图调整leaflet
-import { log } from 'console';
 import { debounce } from 'lodash-es';
+// 百度坐标系转大地坐标系
+const coordinateTransformation: any = {
+  BD09II2WGS84(bdLon: number, bdLat: number) {
+    let x_pi = (3.1416 * 3000.0) / 180.0;
+    //百度坐标转成火星坐标
+    let mars_point = { lat: 0, lon: 0 };
+    let x = bdLon - 0.0065;
+    let y = bdLat - 0.006;
+    let z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * x_pi);
+    let theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * x_pi);
+    mars_point.lat = z * Math.sin(theta);
+    mars_point.lon = z * Math.cos(theta);
+
+    //把火星坐标GCJ02转地球坐标系WGS84
+    let gcjLat = mars_point.lat;
+    let gcjLon = mars_point.lon;
+    let d = this.delta(gcjLat, gcjLon);
+    return [gcjLon - d.lon, gcjLat - d.lat];
+    return {
+      lat: gcjLat - d.lat,
+      lon: gcjLon - d.lon
+    };
+  },
+  delta(lat: number, lon: number) {
+    let PI = 3.1416;
+    let a = 6378245;
+    let ee = 0.0067;
+    let dLat = this.transformLat(lon - 105.0, lat - 35.0);
+    let dLon = this.transformLon(lon - 105.0, lat - 35.0);
+    let radLat = (lat / 180.0) * PI;
+    let magic = Math.sin(radLat);
+    magic = 1 - ee * magic * magic;
+    let sqrtMagic = Math.sqrt(magic);
+    dLat = (dLat * 180.0) / (((a * (1 - ee)) / (magic * sqrtMagic)) * PI);
+    dLon = (dLon * 180.0) / ((a / sqrtMagic) * Math.cos(radLat) * PI);
+    return {
+      lat: dLat,
+      lon: dLon
+    };
+  },
+  transformLat(x: number, y: number) {
+    let PI = 3.1416;
+    let ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.sqrt(Math.abs(x));
+    ret += ((20.0 * Math.sin(6.0 * x * PI) + 20.0 * Math.sin(2.0 * x * PI)) * 2.0) / 3.0;
+    ret += ((20.0 * Math.sin(y * PI) + 40.0 * Math.sin((y / 3.0) * PI)) * 2.0) / 3.0;
+    ret += ((160.0 * Math.sin((y / 12.0) * PI) + 320 * Math.sin((y * PI) / 30.0)) * 2.0) / 3.0;
+    return ret;
+  },
+  transformLon(x: number, y: number) {
+    let PI = 3.1416;
+    let ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(Math.abs(x));
+    ret += ((20.0 * Math.sin(6.0 * x * PI) + 20.0 * Math.sin(2.0 * x * PI)) * 2.0) / 3.0;
+    ret += ((20.0 * Math.sin(x * PI) + 40.0 * Math.sin((x / 3.0) * PI)) * 2.0) / 3.0;
+    ret += ((150.0 * Math.sin((x / 12.0) * PI) + 300.0 * Math.sin((x / 30.0) * PI)) * 2.0) / 3.0;
+    return ret;
+  }
+};
 
 defineOptions({ name: 'MapTangBa' });
 
@@ -193,6 +249,63 @@ const removeSatellite = () => {
   map.setMapType(map.TMAP_TERRAIN_MAP);
 };
 
+const outOfChina = (lng, lat) => {
+  return lng < 72.004 || lng > 137.8347 || lat < 0.8293 || lat > 55.8271 || false;
+};
+/**
+ * 经度转换
+ * @param { Number } lng
+ * @param { Number } lat
+ */
+function transformlat(lng, lat) {
+  var ret =
+    -100.0 +
+    2.0 * lng +
+    3.0 * lat +
+    0.2 * lat * lat +
+    0.1 * lng * lat +
+    0.2 * Math.sqrt(Math.abs(lng));
+  ret += ((20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0) / 3.0;
+  ret += ((20.0 * Math.sin(lat * PI) + 40.0 * Math.sin((lat / 3.0) * PI)) * 2.0) / 3.0;
+  ret += ((160.0 * Math.sin((lat / 12.0) * PI) + 320 * Math.sin((lat * PI) / 30.0)) * 2.0) / 3.0;
+  return ret;
+}
+/**
+ * 纬度转换
+ * @param { Number } lng
+ * @param { Number } lat
+ */
+function transformlng(lng, lat) {
+  var ret =
+    300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * Math.sqrt(Math.abs(lng));
+  ret += ((20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0) / 3.0;
+  ret += ((20.0 * Math.sin(lng * PI) + 40.0 * Math.sin((lng / 3.0) * PI)) * 2.0) / 3.0;
+  ret += ((150.0 * Math.sin((lng / 12.0) * PI) + 300.0 * Math.sin((lng / 30.0) * PI)) * 2.0) / 3.0;
+  return ret;
+}
+
+// const x_PI = 3.14159265358979324 * 3000.0 / 180.0
+var PI = 3.141;
+var a = 6378245.0;
+var ee = 0.00669;
+
+const gcj02towgs84 = (lng, lat) => {
+  if (outOfChina(lng, lat)) {
+    return [lng, lat];
+  } else {
+    var dlat = transformlat(lng - 105.0, lat - 35.0);
+    var dlng = transformlng(lng - 105.0, lat - 35.0);
+    var radlat = (lat / 180.0) * PI;
+    var magic = Math.sin(radlat);
+    magic = 1 - ee * magic * magic;
+    var sqrtmagic = Math.sqrt(magic);
+    dlat = (dlat * 180.0) / (((a * (1 - ee)) / (magic * sqrtmagic)) * PI);
+    dlng = (dlng * 180.0) / ((a / sqrtmagic) * Math.cos(radlat) * PI);
+    const mglat = parseFloat(lat) + dlat;
+    const mglng = parseFloat(lng) + dlng;
+    return [lng * 2 - mglng, lat * 2 - mglat];
+  }
+};
 const markerList: Map<string, any> = new Map();
 const addMarkerToMap = (
   longitude: number,
@@ -201,11 +314,11 @@ const addMarkerToMap = (
   icon = '/tangba/offlineMonitor.png'
 ) => {
   if (!longitude || !latitude) return;
-  // const { lon, lat } = transformGCJ2WGS(longitude, latitude)
+  const [lng, lat] = coordinateTransformation.BD09II2WGS84(longitude, latitude);
 
   // @ts-ignore
   const marker = new T.Marker(
-    new T.LngLat(longitude, latitude),
+    new T.LngLat(lng, lat),
     // title,
     // @ts-ignore
     {
