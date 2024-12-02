99885f9d2be5a701d3f4959574fdafb3d8e3138f
Tony <weijialin@inspur.com>
2024-12-02 12:49:46 +0800

    feat: 水肥一体化示意图

--- src/views/waterAndFertilizer/diagram/index.vue
+++ src/views/waterAndFertilizer/diagram/index.vue
@@ -2,6 +2,18 @@
 // 示意图
 </script>
 <template>
-  <div>示意图</div>
+  <div class="w-full bg-white">
+    <div class="irrigate-img w-full aspect-1.9"></div>
+  </div>
 </template>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.irrigate-dash-container {
+  height: calc(100vh - 105px);
+}
+.irrigate-img {
+  background-image: url(./irrigate.png);
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+</style>
