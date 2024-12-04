26cc29e37cfc895b861a4e523fb78d7f5c39aa98
Tony <weijialin@inspur.com>
2024-12-04 11:25:44 +0800

    feat: 灌区分布

--- src/components/CustomSelector/src/CustomSelector.vue
+++ src/components/CustomSelector/src/CustomSelector.vue
@@ -1,15 +1,131 @@
 <script setup lang="ts">
+import { ParkInfoApi, ParkInfoVO } from '@/api/agriculture/parkinfo/index';
+
 defineOptions({ name: 'CustomSelector' });
+const collapsed = ref<boolean>(false);
+const props = defineProps({
+  menuTitle: {
+    type: String,
+    default: () => ''
+  },
+  menuType: {
+    type: String, // "default"使用menuList， "plot"使用地块
+    default: () => 'default'
+  },
+  menuList: {
+    type: Array<any>,
+    default: () => []
+  }
+});
+
+const basePlotDataList = ref<ParkInfoVO[]>([]);
+const getBasePlotDataList = async () => {
+  const data = await ParkInfoApi.getParkTree({});
+  console.log('🚀 ~ getBasePlotDataList ~ data:', data);
+  if (Array.isArray(data))
+    basePlotDataList.value = data.map((item) => ({ ...item, children: item.child }));
+};
+if (props.menuType === 'plot') getBasePlotDataList();
+
+const showedMenuList = computed(() => {
+  if (props.menuType === 'default') return props.menuList;
+  if (props.menuType === 'plot') return basePlotDataList.value;
+});
+
+const emit = defineEmits(['change', 'join']);
+const handleSelect = (e) => {
+  emit('change', e);
+};
+const handleJoinArea = (id) => {
+  emit('join', id);
+};
 </script>
 <template>
   <div
-    class="custom-selector-wrapper w-full bg-white dark:bg-#1d1e1f text-[#666] dark:text-#ccc rounded-[6px] text-[14px]"
+    class="custom-selector-wrapper w-full bg-white dark:bg-#1d1e1f text-[#666] dark:text-#ccc rounded-[6px] text-[14px] flex relative"
   >
-    sss
+    <div
+      class="collapse-btn absolute left-[267px] top-[16px] z-999"
+      :style="`left: ${collapsed ? '-8px' : '267px'};transform: rotate(${collapsed ? '180deg' : '0'});`"
+      @click="collapsed = !collapsed"
+    ></div>
+    <div
+      class="w-280px flex flex-col p-18px pr-8px box-border relative z-20"
+      :style="`display: ${collapsed ? 'none' : 'block'}`"
+    >
+      <div class="pb-14px h-18px mr-10px font-bold" style="border-bottom: 1px solid #ccc">
+        {{ props.menuTitle }}
+      </div>
+      <div style="height: calc(100% - 19px)">
+        <el-scrollbar style="height: 100%">
+          <div class="pr-10px py-6px">
+            <el-menu @select="handleSelect">
+              <el-sub-menu
+                :index="firstItem.id"
+                :key="firstItem.id"
+                v-for="firstItem in showedMenuList"
+              >
+                <template #title>
+                  <div class="flex space-x-2 items-center">
+                    <el-icon><Folder /></el-icon>
+                    <div class="text-[14px] font-bold">{{ firstItem.name }}</div>
+                  </div>
+                </template>
+                <el-menu-item
+                  :index="secondItem.id"
+                  :key="secondItem.id"
+                  v-for="secondItem in firstItem.children"
+                >
+                  <template #title>
+                    <div class="flex space-x-2 items-center">
+                      <el-icon><DocumentCopy /></el-icon>
+                      <div class="text-[14px]">{{ secondItem.name }}</div>
+                      <el-button type="primary" plain @click="handleJoinArea(secondItem.id)">
+                        加入灌区
+                      </el-button>
+                    </div>
+                  </template>
+                </el-menu-item>
+              </el-sub-menu>
+            </el-menu>
+          </div>
+        </el-scrollbar>
+      </div>
+    </div>
+
+    <div
+      :style="`
+        width: ${collapsed ? '100%' : 'calc(100% - 280px)'};
+        border-radius: ${collapsed ? '6px 6px 6px 6px' : '0 6px 6px 0'};
+      `"
+      class="bg-[#275021] relative z-0 overflow-hidden"
+    >
+      <slot></slot>
+    </div>
   </div>
 </template>
 <style scoped lang="scss">
 .custom-selector-wrapper {
   height: calc(100vh - 102px);
+}
+
+:deep(.el-menu) {
+  border-right: none;
+}
+
+:deep(.el-menu-item.is-active) {
+  background-color: #00968830;
+  border-radius: 6px;
+}
+
+:deep(.el-sub-menu.is-active .el-sub-menu__title) {
+  color: #009688;
+}
+
+.collapse-btn {
+  background-image: url(./collapse.png);
+  width: 26px;
+  height: 26px;
+  background-size: contain;
 }
 </style>
--- src/views/waterAndFertilizer/irrigateManagement/distribution/index.vue
+++ src/views/waterAndFertilizer/irrigateManagement/distribution/index.vue
@@ -1,8 +1,120 @@
 <script setup lang="ts">
 // 灌区分布
 import CustomSelector from '@/components/CustomSelector/src/CustomSelector.vue';
+import { ParkInfoApi } from '@/api/agriculture/parkinfo/index';
+import { dateFormatter } from '@/utils/formatTime';
+
+const basePlotDataList = ref<any[]>([]);
+const getBasePlotDataList = async () => {
+  const data = await ParkInfoApi.getParkTree({});
+  console.log('🚀 ~ getBasePlotDataList ~ data:', data);
+  if (!Array.isArray(data)) return;
+  basePlotDataList.value = data;
+  nextTick(() => drawBasePlot(data));
+};
+const findPlotItem = (id) => {
+  let selectedItem = null;
+  basePlotDataList.value.map((baseItem) => {
+    const plotArr = baseItem.child;
+    if (!Array.isArray(plotArr)) return;
+    selectedItem = plotArr.find((plotItem) => plotItem.id === id);
+  });
+  if (selectedItem) return selectedItem;
+  return;
+};
+onMounted(() => getBasePlotDataList());
+
+const mapIns = ref();
+
+const handleMenuChange = (id) => {
+  console.log('🚀 ~ handleMenuChange ~ id:', id);
+  const seletedPlot = findPlotItem(id);
+  if (seletedPlot) {
+    console.log('🚀 ~ handleMenuChange ~ seletedPlot:', seletedPlot);
+    const { longitude, latitude } = seletedPlot;
+    if (!longitude || !latitude) return ElMessage.warning('所选地块无经纬度数据!');
+    mapIns.value.setCenterZoom([parseFloat(latitude), parseFloat(longitude)], 16);
+  }
+};
+
+// 绘制地块
+const drawBasePlot = (arr: any[]) => {
+  arr.forEach((baseItem) => {
+    const plotArr = baseItem.child;
+    if (!Array.isArray(plotArr)) return;
+    plotArr.forEach((plotItem) => {
+      const geofencing = JSON.parse(plotItem.geofencing);
+      if (Array.isArray(geofencing)) return ElMessage.warning('数据异常');
+      const { corrdinates, option } = geofencing;
+      if (!Array.isArray(corrdinates)) return ElMessage.warning('数据异常');
+      mapIns.value.createPolygon(corrdinates, option);
+    });
+  });
+};
+
+const showJoinAreaDialog = ref<boolean>(false);
+const handleJoinArea = (id) => {
+  console.log('🚀 ~ handleJoinArea ~ id:', id);
+  showJoinAreaDialog.value = true;
+};
+
+const dialogDataList = ref<any[]>([]);
+const dialogLoading = ref<boolean>(false);
+const handleCurrentChange = () => {};
+const handleJoinConfirm = () => {
+  showJoinAreaDialog.value = false;
+};
+const handleJoinCancel = () => {
+  showJoinAreaDialog.value = false;
+};
 </script>
 <template>
-  <CustomSelector />
+  <CustomSelector
+    menuTitle="灌区列表"
+    menuType="default"
+    @change="handleMenuChange"
+    @join="handleJoinArea"
+    :menuList="basePlotDataList.map((item) => ({ ...item, children: item.child }))"
+  >
+    <map-custom ref="mapIns" :enableEdit="false" />
+  </CustomSelector>
+  <Dialog title="加入灌区" v-model="showJoinAreaDialog">
+    <el-table
+      v-loading="dialogLoading"
+      :data="dialogDataList"
+      :stripe="true"
+      :show-overflow-tooltip="true"
+      @current-change="handleCurrentChange"
+      highlight-current-row
+    >
+      <el-table-column type="selection" width="30" label="选择" :reserve-selection="true" />
+      <el-table-column label="灌区编号" align="center" prop="id" />
+      <el-table-column label="灌区名称" align="center" prop="id" />
+      <el-table-column label="喷灌类型" align="center" prop="id" />
+      <el-table-column label="阀门状态" align="center" prop="id" />
+      <el-table-column label="图片" align="center" prop="imgId">
+        <template #default="{ row }">
+          <el-image
+            class="h-50px w-50px"
+            :src="row.imgId"
+            :preview-src-list="[row.imgId]"
+            preview-teleported
+            fit="cover"
+          />
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="创建时间"
+        align="center"
+        prop="id"
+        :formatter="dateFormatter"
+        width="180"
+      />
+    </el-table>
+    <template #footer>
+      <el-button @click="handleJoinConfirm()" type="primary">确 定</el-button>
+      <el-button @click="handleJoinCancel()">取 消</el-button>
+    </template>
+  </Dialog>
 </template>
 <style scoped lang="scss"></style>
