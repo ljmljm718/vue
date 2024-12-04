fe67eb0229676edcfc65fb9da1159ad2d7d3a4b0
Tony <weijialin@inspur.com>
2024-12-04 17:55:02 +0800

    fix: 溯源模板管理 基础信息字段修改

--- src/views/agriculture/tracetemplate/mobile.vue
+++ src/views/agriculture/tracetemplate/mobile.vue
@@ -1,56 +1,51 @@
 <script setup lang="ts">
-import {
-  ProducerEntryApi,
-  ProducerEntryVO
-} from "@/api/agriculture/producerentry";
-import {
-  TraceTemplateApi
-} from '@/api/agriculture/tracetemplate'
-import dayjs from 'dayjs'
+import { ProducerEntryApi, ProducerEntryVO } from '@/api/agriculture/producerentry';
+import { TraceTemplateApi } from '@/api/agriculture/tracetemplate';
+import dayjs from 'dayjs';
 
 /** 查询生产商列表 */
-const producerEntryList = ref<ProducerEntryVO[]>([]) // 生产商的数据
+const producerEntryList = ref<ProducerEntryVO[]>([]); // 生产商的数据
 const getProducerEntryList = async () => {
-  const data = await ProducerEntryApi.getProducerEntryAll({ approvalStatus: 1 })
-  producerEntryList.value = data
-}
+  const data = await ProducerEntryApi.getProducerEntryAll({ approvalStatus: 1 });
+  producerEntryList.value = data;
+};
 const getLabelById = (id: string) => {
-  const selectedItem = producerEntryList.value.find(item => item.id === id);
+  const selectedItem = producerEntryList.value.find((item) => item.id === id);
   if (!selectedItem) return null;
-  return selectedItem.companyName
-}
+  return selectedItem.companyName;
+};
 
 const props = defineProps({
   data: {
     type: Object,
     default: () => ({})
   }
-})
+});
 
-const route = useRoute()
-const routeData = ref<any>({})
+const route = useRoute();
+const routeData = ref<any>({});
 const getDataById = async () => {
   const { id } = route.query;
   if (!id) {
-    getProducerEntryList()
-    return {}
+    getProducerEntryList();
+    return {};
   }
-  const data = await TraceTemplateApi.getCodeSendingInfoAndCreatTraceRecord({ id })
-  console.log("🚀 ~ getDataById ~ data:", data)
-  if (data) routeData.value = data
-}
-getDataById()
+  const data = await TraceTemplateApi.getCodeSendingInfoAndCreatTraceRecord({ id });
+  console.log('🚀 ~ getDataById ~ data:', data);
+  if (data) routeData.value = data;
+};
+getDataById();
 const dataInfo = computed(() => {
   if (Object.keys(props.data).length !== 0) return props.data;
   return routeData.value;
-})
+});
 
 const resourceInfoList = computed(() => {
-  const iconArr = ['苗', '秧', '地', '虫', '除草', '肥', '地', '药']
+  const iconArr = ['苗', '秧', '地', '虫', '除草', '肥', '地', '药'];
   if (routeData.value && Array.isArray(routeData.value.farmRecordList)) {
-    return routeData.value.farmRecordList.map(item => {
-      const index = iconArr.findIndex(ele => item.defineName.indexOf(ele) !== -1);
-      return { ...item, icon: `icon-${index === -1 ? 7 : index + 1}` }
+    return routeData.value.farmRecordList.map((item) => {
+      const index = iconArr.findIndex((ele) => item.defineName.indexOf(ele) !== -1);
+      return { ...item, icon: `icon-${index === -1 ? 7 : index + 1}` };
     });
   } else {
     // return [
@@ -58,37 +53,34 @@ const resourceInfoList = computed(() => {
     //   { recordTime: new Date().valueOf(), defineName: '除草', icon: 'icon-5' },
     //   { recordTime: new Date().valueOf(), defineName: '打药', icon: 'icon-8' },
     // ]
-    return []
+    return [];
   }
-})
+});
 
 const productList = computed(() => {
   if (routeData.value && routeData.value.productImg) {
-    const arr = routeData.value.productImg.split(',')
+    const arr = routeData.value.productImg.split(',');
     return arr.map((img, i) => {
-      return { id: i + 1, img}
-    })
+      return { id: i + 1, img };
+    });
   } else {
     if (Object.keys(props.data).length !== 0) {
       const { productImgs } = props.data;
       if (Array.isArray(productImgs)) {
         return productImgs.map((img, i) => {
-          return { id: i + 1, img}
-        })
+          return { id: i + 1, img };
+        });
       }
     }
-    return []
+    return [];
   }
-})
+});
 </script>
 <template>
   <div class="w-full h-full overflow-auto text-[#000]">
     <!-- <div class="header-bg w-full max-h-[8rem]">
     </div> -->
-    <img
-      :src="dataInfo?.headerImg"
-      class="w-full h-18rem object-cover"
-    />
+    <img :src="dataInfo?.headerImg" class="w-full h-18rem object-cover" />
     <div class="linear-bg py-[1.6rem] flex flex-col items-center">
       <div class="relative font-bold text-[1.1rem]">
         <span>产品概况</span>
@@ -98,12 +90,14 @@ const productList = computed(() => {
       <div class="relative mt-2rem w-[90%]">
         <div class="rect-bg aspect-3 box-border p-2rem space-y-.4rem">
           <div class="flex items-start justify-between">
-            <div class="w-4.5rem">模板名称:</div>
-            <div class="value-right">{{ dataInfo?.templateName }}</div>
+            <div class="w-4.5rem">产品:</div>
+            <div class="value-right">{{ dataInfo?.brandName }}</div>
           </div>
           <div class="flex items-start justify-between">
             <div class="w-4.5rem">生产商:</div>
-            <div class="value-right">{{ getLabelById(dataInfo?.mfrsId) ?? dataInfo?.producer }}</div>
+            <div class="value-right">
+              {{ getLabelById(dataInfo?.mfrsId) ?? dataInfo?.producer }}
+            </div>
           </div>
           <div class="flex items-start justify-between">
             <div class="w-4.5rem">原产地:</div>
@@ -111,25 +105,32 @@ const productList = computed(() => {
           </div>
           <div class="flex items-start justify-between">
             <div class="w-4.5rem">生产时间:</div>
-            <div class="value-right">{{ dataInfo?.productionTime ? dayjs(dataInfo.productionTime).format('YYYY-MM-DD HH:mm:ss') : '' }}</div>
+            <div class="value-right">
+              {{
+                dataInfo?.productionTime
+                  ? dayjs(dataInfo.productionTime).format('YYYY-MM-DD HH:mm:ss')
+                  : ''
+              }}
+            </div>
           </div>
         </div>
         <div
           class="title-bg w-7rem h-2rem absolute text-center text-white"
-          style="top: -1rem;left: calc(50% - 3.5rem);line-height: 2rem;"
-        >基础信息</div>
+          style="top: -1rem; left: calc(50% - 3.5rem); line-height: 2rem"
+        >
+          基础信息
+        </div>
       </div>
       <div class="relative mt-1.8rem w-[90%]">
-        <div
-          class="rect-bg aspect-3 pt-1.4rem px-1.3rem pb-.6rem"
-          style="text-indent: 2rem;"
-        >
+        <div class="rect-bg aspect-3 pt-1.4rem px-1.3rem pb-.6rem" style="text-indent: 2rem">
           {{ dataInfo?.brandDetail }}
         </div>
         <div
           class="title-bg w-7rem h-2rem absolute text-center text-white"
-          style="top: -1rem;left: calc(50% - 3.5rem);line-height: 2rem;"
-        >品牌介绍</div>
+          style="top: -1rem; left: calc(50% - 3.5rem); line-height: 2rem"
+        >
+          品牌介绍
+        </div>
       </div>
     </div>
     <div class="py-[1.6rem] flex flex-col items-center">
@@ -140,27 +141,31 @@ const productList = computed(() => {
       <div class="w-2.3rem h-4px bg-[#009688] mt-[.2rem] rounded-full"></div>
       <div v-if="resourceInfoList.length > 0" class="w-90% flex flex-col space-y-3 mt-1.6rem">
         <div
-          v-for="item, index in resourceInfoList"
+          v-for="(item, index) in resourceInfoList"
           :key="item.id"
           class="flex items-start space-x-2"
         >
           <div class="flex flex-col items-center w-2rem h-full relative top-1rem">
             <div class="text-#009688 art-font">{{ index + 1 }}</div>
-            <div
-              class="w-1px h-2rem mt-.4rem"
-              style="border-right: 1px dashed #009688;"
-            ></div>
+            <div class="w-1px h-2rem mt-.4rem" style="border-right: 1px dashed #009688"></div>
           </div>
-          <div class="grow shadow-md h-3.6rem flex space-x-1rem items-center box-border px-1rem" style="border: 1px solid #e1e1e1;">
+          <div
+            class="grow shadow-md h-3.6rem flex space-x-1rem items-center box-border px-1rem"
+            style="border: 1px solid #e1e1e1"
+          >
             <div :class="`w-2rem h-2rem rounded-full ${item.icon}`"></div>
             <div>
               <div>{{ item.defineName }}</div>
-              <div class="text-#999 text-.8rem">{{ dayjs(item.recordTime).format('YYYY-MM-DD HH:mm:ss') }}</div>
+              <div class="text-#999 text-.8rem">
+                {{ dayjs(item.recordTime).format('YYYY-MM-DD HH:mm:ss') }}
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div v-else class="w-90% h-[10rem] mt-1.6rem no-data text-center tracking-widest">暂无数据</div>
+      <div v-else class="w-90% h-[10rem] mt-1.6rem no-data text-center tracking-widest">
+        暂无数据
+      </div>
     </div>
     <div class="py-[1.6rem] flex flex-col items-center">
       <div class="relative font-bold text-[1.1rem]">
@@ -173,7 +178,7 @@ const productList = computed(() => {
           class="w-full p-1 box-border rounded-2"
           v-for="item in productList"
           :key="item.id"
-          style="border: 1px solid #009688;"
+          style="border: 1px solid #009688"
         >
           <img :src="item.img" class="w-full h-full object-contain" />
         </div>
@@ -188,7 +193,7 @@ const productList = computed(() => {
 }
 
 .linear-bg {
-  background: linear-gradient(to top, #FFFFFF, #EAF5F4, #FFFFFF);
+  background: linear-gradient(to top, #ffffff, #eaf5f4, #ffffff);
 }
 
 .rect-bg {
