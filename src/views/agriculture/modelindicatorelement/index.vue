4d8bfb05e8a95fc6b47936cd707245b5a7b260e5
李沐阳 <limuyang@inspur.com>
2024-12-05 16:07:04 +0800

    fix: 模型管理指标要素和要素范围表单整改

--- src/views/agriculture/modelindicatorelement/index.vue
+++ src/views/agriculture/modelindicatorelement/index.vue
@@ -1,75 +1,28 @@
 <template>
-  <ContentWrap>
-    <!-- 搜索工作栏 -->
-    <custom-form
-      class="-mb-15px"
-      :model="queryParams"
-      ref="queryFormRef"
-      :inline="true"
-      label-width="68px"
-    >
-      <!--      <el-form-item label="指标id" prop="indicatorId">-->
-      <!--        <el-input-->
-      <!--          v-model="queryParams.indicatorId"-->
-      <!--          placeholder="请输入指标id"-->
-      <!--          clearable-->
-      <!--          @keyup.enter="handleQuery"-->
-      <!--          class="!w-240px"-->
-      <!--        />-->
-      <!--      </el-form-item>-->
-      <el-form-item label="要素名称" prop="elementName">
-        <el-input
-          v-model="queryParams.elementName"
-          placeholder="请输入要素名称"
-          clearable
-          @keyup.enter="handleQuery"
-          class="!w-240px"
-        />
-      </el-form-item>
-      <!--      <el-form-item label="权重" prop="weight">-->
-      <!--        <el-input-->
-      <!--          v-model="queryParams.weight"-->
-      <!--          placeholder="请输入权重"-->
-      <!--          clearable-->
-      <!--          @keyup.enter="handleQuery"-->
-      <!--          class="!w-240px"-->
-      <!--        />-->
-      <!--      </el-form-item>-->
-      <!--      <el-form-item label="绑定设备" prop="bindDevice">-->
-      <!--        <el-input-->
-      <!--          v-model="queryParams.bindDevice"-->
-      <!--          placeholder="请输入绑定设备"-->
-      <!--          clearable-->
-      <!--          @keyup.enter="handleQuery"-->
-      <!--          class="!w-240px"-->
-      <!--        />-->
-      <!--      </el-form-item>-->
-      <!--      <el-form-item label="状态" prop="status">-->
-      <!--        <el-select-->
-      <!--          v-model="queryParams.status"-->
-      <!--          placeholder="请选择状态"-->
-      <!--          clearable-->
-      <!--          class="!w-240px"-->
-      <!--        >-->
-      <!--          <el-option-->
-      <!--            v-for="dict in getIntDictOptions(DICT_TYPE.COMMON_STATUS)"-->
-      <!--            :key="dict.value"-->
-      <!--            :label="dict.label"-->
-      <!--            :value="dict.value"-->
-      <!--          />-->
-      <!--        </el-select>-->
-      <!--      </el-form-item>-->
-      <!--      <el-form-item label="实现类" prop="implementationClass">-->
-      <!--        <el-input-->
-      <!--          v-model="queryParams.implementationClass"-->
-      <!--          placeholder="请输入实现类"-->
-      <!--          clearable-->
-      <!--          @keyup.enter="handleQuery"-->
-      <!--          class="!w-240px"-->
-      <!--        />-->
-      <!--      </el-form-item>-->
-      <el-form-item>
-        <el-button @click="handleQuery">
+  <el-scrollbar
+    class="w-full bg-white rounded-[6px] text-[#666] text-[14px] p-[16px] box-border"
+    :style="{ height: 'calc(100vh - ' + (topMenuHeight + 2 * contentPadding) + 'px)' }"
+  >
+    <div class="w-full flex justify-between items-center">
+      <div class="flex items-center">
+        <!-- 一级标题名字 -->
+        <h1 class="m-0 text-[#333] font-bold text-[18px]">指标要素</h1>
+
+        <Icon icon="ep:question-filled" :size="14" class="ml-[8px] cursor-pointer text-[#F08000]" />
+        <div class="w-[1px] h-[32px] mx-[16px] bg-[#ebebeb]"></div>
+
+        <el-button
+          type="primary"
+          @click="openForm('create')"
+          v-hasPermi="['agriculture:model-indicator-element:create']"
+        >
+          <Icon icon="ep:plus" class="mr-5px" />
+          新增
+        </el-button>
+      </div>
+
+      <div class="flex items-center">
+        <el-button @click="handleQuery" type="primary">
           <Icon icon="ep:search" class="mr-5px" />
           搜索
         </el-button>
@@ -77,97 +30,89 @@
           <Icon icon="ep:refresh" class="mr-5px" />
           重置
         </el-button>
-        <!--        <el-button-->
-        <!--          type="success"-->
-        <!--          plain-->
-        <!--          @click="handleExport"-->
-        <!--          :loading="exportLoading"-->
-        <!--          v-hasPermi="['agriculture:model-indicator-element:export']"-->
-        <!--        >-->
-        <!--          <Icon icon="ep:download" class="mr-5px" /> 导出-->
-        <!--        </el-button>-->
-      </el-form-item>
-    </custom-form>
-  </ContentWrap>
 
-  <!-- 列表 -->
-  <ContentWrap>
-    <el-form-item>
-      <el-button
-        type="primary"
-        plain
-        @click="openForm('create')"
-        v-hasPermi="['agriculture:model-indicator-element:create']"
-      >
-        <Icon icon="ep:plus" class="mr-5px" />
-        新增
-      </el-button>
-    </el-form-item>
-    <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
-      <el-table-column label="要素编号" align="center" prop="id" />
-      <!--      <el-table-column label="监测指标" align="center" prop="indicatorId" />-->
-      <el-table-column label="要素名称" align="center" prop="elementName" width="110" />
-      <el-table-column label="权重(%)" align="center" prop="weight" width="90" />
-      <el-table-column label="指标说明" align="center" prop="indicatorDescription" />
-      <el-table-column label="绑定设备" align="center" prop="deviceName" />
-      <!--      <el-table-column label="状态" align="center" prop="status" width="110">-->
-      <!--        <template #default="scope">-->
-      <!--          <el-switch-->
-      <!--            v-model="scope.row.status"-->
-      <!--            :active-value="0"-->
-      <!--            :inactive-value="1"-->
-      <!--            @change="handleStatusChange(scope.row)"-->
-      <!--          />-->
-      <!--        </template>-->
-      <!--      </el-table-column>-->
-      <el-table-column label="实现类" align="center" prop="implementationClass" />
-      <!--      <el-table-column-->
-      <!--        label="创建时间"-->
-      <!--        align="center"-->
-      <!--        prop="createTime"-->
-      <!--        :formatter="dateFormatter"-->
-      <!--        width="180px"-->
-      <!--      />-->
-      <el-table-column label="操作" align="center">
-        <template #default="scope">
-          <el-button
-            link
-            type="primary"
-            @click="
-              router.push(
-                `/growth_monitor/model-indicator-element-range?indicatorElementId=${scope.row.id}`
-              )
-            "
-          >
-            指标范围
-          </el-button>
-          <el-button
-            link
-            type="primary"
-            @click="openForm('update', scope.row)"
-            v-hasPermi="['agriculture:model-indicator-element:update']"
-          >
-            编辑
-          </el-button>
-          <el-button
-            link
-            type="danger"
-            @click="handleDelete(scope.row.id)"
-            v-hasPermi="['agriculture:model-indicator-element:delete']"
-          >
-            删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <!-- 分页 -->
+        <button
+          class="circle-arrow-up ml-[16px]"
+          :class="showSearch ? 'rotate180andthemeBg' : 'rotate180andwhiteBg'"
+          @click="handleClickShowSearch"
+        >
+          <Icon :size="14" icon="ep:arrow-up" />
+        </button>
+      </div>
+    </div>
+
+    <el-form
+      :model="queryParams"
+      ref="queryFormRef"
+      class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-[8px] mt-[8px] w-full form"
+      :class="showSearch ? 'opacity-100' : 'h-0 opacity-0'"
+      label-width="95px"
+      :inline="true"
+    >
+      <el-form-item label="要素名称" prop="elementName">
+        <el-input
+          v-model="queryParams.elementName"
+          placeholder="请输入要素名称"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+    </el-form>
+
+    <div class="w-full mt-[16px]">
+      <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
+        <el-table-column label="要素编号" align="center" prop="id" />
+        <el-table-column label="要素名称" align="center" prop="elementName" width="110" />
+        <el-table-column label="权重(%)" align="center" prop="weight" width="90" />
+        <el-table-column label="指标说明" align="center" prop="indicatorDescription" />
+        <el-table-column label="绑定设备" align="center" prop="deviceName" />
+        <el-table-column label="实现类" align="center" prop="implementationClass" />
+        <el-table-column label="操作" align="center" fixed="right" min-width="184px">
+          <template #default="scope">
+            <div class="flex items-center justify-center">
+              <el-button
+                link
+                type="primary"
+                @click="
+                  router.push(
+                    `/growth_monitor/model-indicator-element-range?indicatorElementId=${scope.row.id}`
+                  )
+                "
+              >
+                指标范围
+              </el-button>
+              <div class="mx-[12px] w-[1px] h-[24px] bg-[#e6e6e6]"></div>
+              <el-button
+                link
+                type="primary"
+                @click="openForm('update', scope.row)"
+                v-hasPermi="['agriculture:model-indicator-element:update']"
+              >
+                编辑
+              </el-button>
+              <div class="mx-[12px] w-[1px] h-[24px] bg-[#e6e6e6]"></div>
+              <el-button
+                link
+                type="danger"
+                @click="handleDelete(scope.row.id)"
+                v-hasPermi="['agriculture:model-indicator-element:delete']"
+              >
+                删除
+              </el-button>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
     <Pagination
+      style="margin-bottom: 0; margin-top: 8px"
       :total="total"
       v-model:page="queryParams.pageNo"
       v-model:limit="queryParams.pageSize"
       @pagination="getList"
     />
-  </ContentWrap>
+  </el-scrollbar>
 
   <!-- 表单弹窗：添加/修改 -->
   <ModelIndicatorElementForm ref="formRef" @success="getList" />
@@ -180,11 +125,9 @@ import {
   ModelIndicatorElementVO
 } from '@/api/agriculture/modelindicatorelement';
 import ModelIndicatorElementForm from './ModelIndicatorElementForm.vue';
-import { DICT_TYPE, getIntDictOptions } from '@/utils/dict';
 import { CommonStatusEnum } from '@/utils/constants';
-import { ModelManagementApi, ModelManagementVO } from '@/api/agriculture/modelmanagement';
 import { DeviceInfoApi, DeviceInfoVO } from '@/api/agriculture/deviceinfo';
-import { CropGrowthNewApi } from '@/api/agri/cropgrowthnew';
+import { ElTable } from 'element-plus';
 
 /** 指标要素 列表 */
 defineOptions({ name: 'ModelIndicatorElement' });
@@ -330,4 +273,94 @@ const init = async () => {
 onMounted(() => {
   init();
 });
+/* 原页面的js代码复制在上面 包括import */
+
+/**
+ * topMenuHeight      顶部菜单和标签页高度
+ * contentPadding     页面内容外边距
+ */
+const topMenuHeight = 85;
+const contentPadding = 8;
+
+// 展开或收起搜索栏
+const showSearch = ref(false);
+const handleClickShowSearch = () => {
+  showSearch.value = !showSearch.value;
+};
 </script>
+
+<style lang="scss" scoped>
+// 鼠标移在按钮上时显示主题色边框
+:deep(.el-button:hover) {
+  border-color: var(--el-color-primary);
+}
+
+// 去掉表单的边距
+:deep(.form > *) {
+  margin: 0;
+}
+
+// 调整表单标签和输入框之间的距离
+:deep(.form .el-form-item__label) {
+  padding: 0 4px 0 0;
+}
+
+// 收起
+.circle-arrow-up {
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  border: 1px solid #ebebeb;
+  color: #333;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    color: white;
+    border-width: 0;
+    background-color: var(--el-color-primary);
+  }
+}
+
+// 向上箭头展开收起的动画
+@keyframes rotate180andwhiteBg {
+  from {
+    transform: rotate(0deg);
+    color: #333;
+    background-color: white;
+  }
+  to {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+}
+
+.rotate180andwhiteBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andwhiteBg;
+  animation-fill-mode: forwards;
+}
+
+@keyframes rotate180andthemeBg {
+  from {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+  to {
+    transform: rotate(360deg);
+    color: #333;
+    background-color: white;
+  }
+}
+
+.rotate180andthemeBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andthemeBg;
+  animation-fill-mode: forwards;
+}
+</style>
--- src/views/agriculture/modelindicatorelementrange/index.vue
+++ src/views/agriculture/modelindicatorelementrange/index.vue
@@ -1,59 +1,45 @@
 <template>
-  <ContentWrap>
-    <!-- 搜索工作栏 -->
-    <custom-form
-      class="-mb-15px"
+  <el-scrollbar
+    class="w-full bg-white rounded-[6px] text-[#666] text-[14px] p-[16px] box-border"
+    :style="{ height: 'calc(100vh - ' + (topMenuHeight + 2 * contentPadding) + 'px)' }"
+  >
+    <div class="w-full flex justify-between items-center">
+      <div class="flex items-center">
+        <!-- 一级标题名字 -->
+        <h1 class="m-0 text-[#333] font-bold text-[18px]">指标要素范围</h1>
+
+        <Icon icon="ep:question-filled" :size="14" class="ml-[8px] cursor-pointer text-[#F08000]" />
+        <div class="w-[1px] h-[32px] mx-[16px] bg-[#ebebeb]"></div>
+      </div>
+
+      <div class="flex items-center">
+        <el-button @click="handleQuery" type="primary">
+          <Icon icon="ep:search" class="mr-5px" />
+          搜索
+        </el-button>
+        <el-button @click="resetQuery">
+          <Icon icon="ep:refresh" class="mr-5px" />
+          重置
+        </el-button>
+
+        <button
+          class="circle-arrow-up ml-[16px]"
+          :class="showSearch ? 'rotate180andthemeBg' : 'rotate180andwhiteBg'"
+          @click="handleClickShowSearch"
+        >
+          <Icon :size="14" icon="ep:arrow-up" />
+        </button>
+      </div>
+    </div>
+
+    <el-form
       :model="queryParams"
       ref="queryFormRef"
+      class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-[8px] mt-[8px] w-full form"
+      :class="showSearch ? 'opacity-100' : 'h-0 opacity-0'"
+      label-width="95px"
       :inline="true"
-      label-width="68px"
     >
-      <!--      <el-form-item label="指标要素" prop="indicatorElementId">-->
-      <!--        <el-input v-model="queryParams.indicatorElementId" placeholder="请选择指标要素" disabled>-->
-      <!--          <template #append>-->
-      <!--            <el-button @click="openModelIndicatorElementSelectPopup('0')">-->
-      <!--              <Icon icon="ep:search"/>-->
-      <!--              选择-->
-      <!--            </el-button>-->
-      <!--          </template>-->
-      <!--        </el-input>-->
-      <!--      </el-form-item>-->
-      <!--      <el-form-item label="下限" prop="lowLimit">-->
-      <!--        <el-input-->
-      <!--          v-model="queryParams.lowLimit"-->
-      <!--          placeholder="请输入下限"-->
-      <!--          clearable-->
-      <!--          @keyup.enter="handleQuery"-->
-      <!--          class="!w-240px"-->
-      <!--        />-->
-      <!--      </el-form-item>-->
-      <!--      <el-form-item label="上限" prop="highLimit">-->
-      <!--        <el-input-->
-      <!--          v-model="queryParams.highLimit"-->
-      <!--          placeholder="请输入上限"-->
-      <!--          clearable-->
-      <!--          @keyup.enter="handleQuery"-->
-      <!--          class="!w-240px"-->
-      <!--        />-->
-      <!--      </el-form-item>-->
-      <!--      <el-form-item label="单位" prop="unit">-->
-      <!--        <el-input-->
-      <!--          v-model="queryParams.unit"-->
-      <!--          placeholder="请输入单位"-->
-      <!--          clearable-->
-      <!--          @keyup.enter="handleQuery"-->
-      <!--          class="!w-240px"-->
-      <!--        />-->
-      <!--      </el-form-item>-->
-      <!--      <el-form-item label="健康比例" prop="healthRatio">-->
-      <!--        <el-input-->
-      <!--          v-model="queryParams.healthRatio"-->
-      <!--          placeholder="请输入健康比例"-->
-      <!--          clearable-->
-      <!--          @keyup.enter="handleQuery"-->
-      <!--          class="!w-240px"-->
-      <!--        />-->
-      <!--      </el-form-item>-->
       <el-form-item label="健康等级" prop="healthLevel">
         <el-select
           v-model="queryParams.healthLevel"
@@ -69,110 +55,64 @@
           />
         </el-select>
       </el-form-item>
-      <!--      <el-form-item label="指标结果" prop="indicatorResult">-->
-      <!--        <el-input-->
-      <!--          v-model="queryParams.indicatorResult"-->
-      <!--          placeholder="请输入指标结果"-->
-      <!--          clearable-->
-      <!--          @keyup.enter="handleQuery"-->
-      <!--          class="!w-240px"-->
-      <!--        />-->
-      <!--      </el-form-item>-->
-      <!--      <el-form-item label="排序" prop="sortBy">-->
-      <!--        <el-input-->
-      <!--          v-model="queryParams.sortBy"-->
-      <!--          placeholder="请输入排序"-->
-      <!--          clearable-->
-      <!--          @keyup.enter="handleQuery"-->
-      <!--          class="!w-240px"-->
-      <!--        />-->
-      <!--      </el-form-item>-->
-      <el-form-item>
-        <el-button @click="handleQuery">
-          <Icon icon="ep:search" class="mr-5px" />
-          搜索
-        </el-button>
-        <el-button @click="resetQuery">
-          <Icon icon="ep:refresh" class="mr-5px" />
-          重置
-        </el-button>
-        <!--        <el-button-->
-        <!--          type="primary"-->
-        <!--          plain-->
-        <!--          @click="openForm('create')"-->
-        <!--          v-hasPermi="['agriculture:model-indicator-element-range:create']"-->
-        <!--        >-->
-        <!--          <Icon icon="ep:plus" class="mr-5px" /> 新增-->
-        <!--        </el-button>-->
-        <!--        <el-button-->
-        <!--          type="success"-->
-        <!--          plain-->
-        <!--          @click="handleExport"-->
-        <!--          :loading="exportLoading"-->
-        <!--          v-hasPermi="['agriculture:model-indicator-element-range:export']"-->
-        <!--        >-->
-        <!--          <Icon icon="ep:download" class="mr-5px" /> 导出-->
-        <!--        </el-button>-->
-      </el-form-item>
-    </custom-form>
-  </ContentWrap>
+    </el-form>
 
-  <!-- 列表 -->
-  <ContentWrap>
-    <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
-      <!--      <el-table-column label="主键" align="center" prop="id" />-->
-      <el-table-column label="指标要素" align="center" prop="indicatorElementId" width="240" />
-      <el-table-column label="指标范围" align="center" width="240">
-        <template #default="scope">
-          {{
-            scope.row.lowLimit +
-            '  ~  ' +
-            scope.row.highLimit +
-            '  ' +
-            (!scope.row.unit ? '' : scope.row.unit)
-          }}
-        </template>
-      </el-table-column>
-      <!--      <el-table-column label="下限" align="center" prop="lowLimit" />-->
-      <!--      <el-table-column label="上限" align="center" prop="highLimit" />-->
-      <!--      <el-table-column label="单位" align="center" prop="unit" />-->
-      <el-table-column label="健康值" align="center" prop="healthRatio" />
-      <el-table-column label="健康等级" align="center" prop="healthLevel">
-        <template #default="scope">
-          <dict-tag :type="DICT_TYPE.AGRI_HEALTH_LEVEL" :value="scope.row.healthLevel" />
-        </template>
-      </el-table-column>
-      <el-table-column label="指标结果" align="center" prop="indicatorResult" width="240" />
-      <el-table-column label="排序" align="center" prop="sortBy" />
-      <el-table-column label="操作" align="center">
-        <template #default="scope">
-          <el-button
-            link
-            type="primary"
-            @click="openForm('update', scope.row.id)"
-            v-hasPermi="['agriculture:model-indicator-element-range:update']"
-          >
-            编辑
-          </el-button>
-          <el-button
-            link
-            type="danger"
-            @click="handleDelete(scope.row.id)"
-            v-hasPermi="['agriculture:model-indicator-element-range:delete']"
-          >
-            删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <!-- 分页 -->
+    <div class="w-full mt-[16px]">
+      <el-table v-loading="loading" :data="list" :stripe="true" :show-overflow-tooltip="true">
+        <el-table-column label="指标要素" align="center" prop="indicatorElementId" width="240" />
+        <el-table-column label="指标范围" align="center" width="240">
+          <template #default="scope">
+            {{
+              scope.row.lowLimit +
+              '  ~  ' +
+              scope.row.highLimit +
+              '  ' +
+              (!scope.row.unit ? '' : scope.row.unit)
+            }}
+          </template>
+        </el-table-column>
+        <el-table-column label="健康值" align="center" prop="healthRatio" />
+        <el-table-column label="健康等级" align="center" prop="healthLevel">
+          <template #default="scope">
+            <dict-tag :type="DICT_TYPE.AGRI_HEALTH_LEVEL" :value="scope.row.healthLevel" />
+          </template>
+        </el-table-column>
+        <el-table-column label="指标结果" align="center" prop="indicatorResult" width="240" />
+        <el-table-column label="排序" align="center" prop="sortBy" />
+        <el-table-column label="操作" align="center" fixed="right" min-width="154px">
+          <template #default="scope">
+            <div class="flex items-center justify-center">
+              <el-button
+                link
+                type="primary"
+                @click="openForm('update', scope.row.id)"
+                v-hasPermi="['agriculture:model-indicator-element-range:update']"
+              >
+                编辑
+              </el-button>
+              <div class="mx-[12px] w-[1px] h-[24px] bg-[#e6e6e6]"></div>
+              <el-button
+                link
+                type="danger"
+                @click="handleDelete(scope.row.id)"
+                v-hasPermi="['agriculture:model-indicator-element-range:delete']"
+              >
+                删除
+              </el-button>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
     <Pagination
+      style="margin-bottom: 0; margin-top: 8px"
       :total="total"
       v-model:page="queryParams.pageNo"
       v-model:limit="queryParams.pageSize"
       @pagination="getList"
     />
-  </ContentWrap>
+  </el-scrollbar>
 
   <!-- 表单弹窗：添加/修改 -->
   <ModelIndicatorElementRangeForm ref="formRef" @success="getList" />
@@ -194,6 +134,7 @@ import ModelIndicatorElementRangeForm from './ModelIndicatorElementRangeForm.vue
 import { ModelIndicatorElementVO } from '@/api/agriculture/modelindicatorelement';
 import ModelIndicatorElementSelectPopup from '@/views/agriculture/modelindicatorelement/components/ModelIndicatorElementSelectPopup.vue';
 import { DICT_TYPE, getStrDictOptions } from '@/utils/dict';
+import { ElTable } from 'element-plus';
 
 /** 指标要素范围 列表 */
 defineOptions({ name: 'ModelIndicatorElementRange' });
@@ -297,4 +238,95 @@ const handleModelIndicatorElementSelectPopupChange = (order: ModelIndicatorEleme
 onMounted(() => {
   getList();
 });
+
+/* 原页面的js代码复制在上面 包括import */
+
+/**
+ * topMenuHeight      顶部菜单和标签页高度
+ * contentPadding     页面内容外边距
+ */
+const topMenuHeight = 85;
+const contentPadding = 8;
+
+// 展开或收起搜索栏
+const showSearch = ref(false);
+const handleClickShowSearch = () => {
+  showSearch.value = !showSearch.value;
+};
 </script>
+
+<style lang="scss" scoped>
+// 鼠标移在按钮上时显示主题色边框
+:deep(.el-button:hover) {
+  border-color: var(--el-color-primary);
+}
+
+// 去掉表单的边距
+:deep(.form > *) {
+  margin: 0;
+}
+
+// 调整表单标签和输入框之间的距离
+:deep(.form .el-form-item__label) {
+  padding: 0 4px 0 0;
+}
+
+// 收起
+.circle-arrow-up {
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  border: 1px solid #ebebeb;
+  color: #333;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    color: white;
+    border-width: 0;
+    background-color: var(--el-color-primary);
+  }
+}
+
+// 向上箭头展开收起的动画
+@keyframes rotate180andwhiteBg {
+  from {
+    transform: rotate(0deg);
+    color: #333;
+    background-color: white;
+  }
+  to {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+}
+
+.rotate180andwhiteBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andwhiteBg;
+  animation-fill-mode: forwards;
+}
+
+@keyframes rotate180andthemeBg {
+  from {
+    transform: rotate(180deg);
+    color: white;
+    background-color: var(--el-color-primary);
+  }
+  to {
+    transform: rotate(360deg);
+    color: #333;
+    background-color: white;
+  }
+}
+
+.rotate180andthemeBg {
+  animation-duration: 0.5s;
+  animation-name: rotate180andthemeBg;
+  animation-fill-mode: forwards;
+}
+</style>
