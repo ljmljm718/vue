93a7d0a1b307b921550afb4bf0a9691012f1808e
李沐阳 <limuyang@inspur.com>
2024-12-20 11:44:09 +0800

    fix: 生产商入库更多按钮样式修改

--- src/views/agriculture/producerentry/index.vue
+++ src/views/agriculture/producerentry/index.vue
@@ -137,7 +137,7 @@
               <div class="mx-[12px] w-[1px] h-[24px] bg-[#e6e6e6]"></div>
               <el-popover :width="104" trigger="hover" popper-style="min-width: 0">
                 <template #reference>
-                  <button class="link-btn flex items-center">
+                  <div class="flex items-center">
                     <div
                       class="w-[2px] h-[2px] mx-[1px] rounded-full"
                       style="background-color: var(--el-color-primary)"
@@ -150,7 +150,7 @@
                       class="w-[2px] h-[2px] mx-[1px] rounded-full"
                       style="background-color: var(--el-color-primary)"
                     ></div>
-                  </button>
+                  </div>
                 </template>
 
                 <div class="flex flex-col items-start space-y-[8px] space-x-0">
